a58eeb0b8548b40579b08c8cd7fcab471a7b78d2
Taiji Uchida <uchi825@gmail.com>
2016-04-10 18:30:11 +0900

    セットアップファイルの追加。

---
+++ Setup.bat
@@ -0,0 +1,6 @@
+cd /d %~dp0
+"%ProgramFiles(x86)%\MSBuild\14.0\Bin\msbuild.exe" Implem.CodeDefiner.sln /t:rebuild /p:Configuration=Release
+.\Implem.CodeDefiner\bin\Release\Implem.CodeDefiner.exe _rds
+"%ProgramFiles(x86)%\MSBuild\14.0\Bin\msbuild.exe" Implem.Pleasanter.sln /t:rebuild /p:Configuration=Release /P:DeployOnBuild=true;CreatePackageOnPublish=true;DeployIisAppPath="Default Web Site/pleasanter"
+.\Implem.Pleasanter\obj\Release\Package\Implem.Pleasanter.deploy.cmd /T /Y
+pause
