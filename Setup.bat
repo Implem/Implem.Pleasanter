b00ca81f6fdb35b4a61eb4914ead1c5c6d65aa78
Taiji Uchida <uchi825@gmail.com>
2016-04-10 20:05:58 +0900

    Bug fixed: セットアップ時にNuGetパッケージ群が存在しない問題を解消。

--- Setup.bat
+++ Setup.bat
@@ -1,4 +1,6 @@
 cd /d %~dp0
+nuget.exe restore Implem.Pleasanter.sln
+nuget.exe restore Implem.CodeDefiner.sln
 "%ProgramFiles(x86)%\MSBuild\14.0\Bin\msbuild.exe" Implem.CodeDefiner.sln /t:rebuild /p:Configuration=Release
 .\Implem.CodeDefiner\bin\Release\Implem.CodeDefiner.exe _rds
 "%ProgramFiles(x86)%\MSBuild\14.0\Bin\msbuild.exe" Implem.Pleasanter.sln /t:rebuild /p:Configuration=Release /P:DeployOnBuild=true;CreatePackageOnPublish=true;DeployIisAppPath="Default Web Site/pleasanter"
