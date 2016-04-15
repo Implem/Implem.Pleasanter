eb3ea169fc6e65423e282207eb3f99474e92e463
Taiji Uchida <uchi825@gmail.com>
2016-04-16 08:23:43 +0900

    配布方法の変更に伴いセットアップ関連ファイルを削除。

--- Setup.bat
+++
@@ -1,8 +0,0 @@
-cd /d %~dp0
-nuget.exe restore Implem.Pleasanter.sln
-nuget.exe restore Implem.CodeDefiner.sln
-"%ProgramFiles(x86)%\MSBuild\14.0\Bin\msbuild.exe" Implem.CodeDefiner.sln /t:rebuild /p:Configuration=Release
-.\Implem.CodeDefiner\bin\Release\Implem.CodeDefiner.exe _rds
-"%ProgramFiles(x86)%\MSBuild\14.0\Bin\msbuild.exe" Implem.Pleasanter.sln /t:rebuild /p:Configuration=Release /P:DeployOnBuild=true;CreatePackageOnPublish=true;DeployIisAppPath="Default Web Site/pleasanter"
-.\Implem.Pleasanter\obj\Release\Package\Implem.Pleasanter.deploy.cmd /T /Y
-pause
