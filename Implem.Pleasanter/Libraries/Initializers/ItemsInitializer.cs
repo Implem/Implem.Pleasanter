976d5b2918b073d9697f34a6daec6c929625679b
Taiji Uchida <uchi825@gmail.com>
2016-04-23 19:26:42 +0900

    Bug fixed: 使用していないItems_historyテーブルに初期データが投入される問題を解消。

--- Implem.Pleasanter/Libraries/Initializers/ItemsInitializer.cs
+++ Implem.Pleasanter/Libraries/Initializers/ItemsInitializer.cs
@@ -150,42 +150,6 @@ namespace Implem.Pleasanter.Libraries.Initializers
                                 });
                         }
                     });
-                Rds.ExecuteTable(statements: Rds.SelectIssues(
-                    tableType: Sqls.TableTypes.History,
-                    column: Rds.IssuesColumn()
-                        .SiteId()
-                        .IssueId()
-                        .Ver())).AsEnumerable().ForEach(dataRow =>
-                    {
-                        var siteSettings = new SiteModel().Get(where:
-                            Rds.SitesWhere().SiteId(dataRow["SiteId"].ToLong()))?
-                                .IssuesSiteSettings();
-                        var issueModel = new IssueModel(siteSettings).Get(
-                            tableType: Sqls.TableTypes.History,
-                            where: Rds.IssuesWhere()
-                                .SiteId(dataRow["SiteId"].ToLong())
-                                .IssueId(dataRow["IssueId"].ToLong())
-                                .Ver(dataRow["Ver"].ToInt()));
-                        if (siteSettings != null &&
-                            issueModel.AccessStatus == Databases.AccessStatuses.Selected)
-                        {
-                            Rds.ExecuteNonQuery(
-                                statements: new SqlStatement[]
-                                {
-                                    Rds.InsertItems(
-                                        tableType: Sqls.TableTypes.History,
-                                        param: Rds.ItemsParam()
-                                            .ReferenceId(issueModel.IssueId)
-                                            .Ver(issueModel.Ver)
-                                            .ReferenceType("Issues")
-                                            .SiteId(issueModel.SiteId)
-                                            .Title(IssuesUtility.TitleDisplayValue(
-                                                siteSettings, issueModel))
-                                            .Subset(Jsons.ToJson(new IssueSubset(
-                                                issueModel, siteSettings))))
-                                });
-                        }
-                    });
                 Rds.ExecuteTable(statements: Rds.SelectResults(
                     tableType: Sqls.TableTypes.Normal,
                     column: Rds.ResultsColumn()
@@ -259,42 +223,6 @@ namespace Implem.Pleasanter.Libraries.Initializers
                                 });
                         }
                     });
-                Rds.ExecuteTable(statements: Rds.SelectResults(
-                    tableType: Sqls.TableTypes.History,
-                    column: Rds.ResultsColumn()
-                        .SiteId()
-                        .ResultId()
-                        .Ver())).AsEnumerable().ForEach(dataRow =>
-                    {
-                        var siteSettings = new SiteModel().Get(where:
-                            Rds.SitesWhere().SiteId(dataRow["SiteId"].ToLong()))?
-                                .ResultsSiteSettings();
-                        var resultModel = new ResultModel(siteSettings).Get(
-                            tableType: Sqls.TableTypes.History,
-                            where: Rds.ResultsWhere()
-                                .SiteId(dataRow["SiteId"].ToLong())
-                                .ResultId(dataRow["ResultId"].ToLong())
-                                .Ver(dataRow["Ver"].ToInt()));
-                        if (siteSettings != null &&
-                            resultModel.AccessStatus == Databases.AccessStatuses.Selected)
-                        {
-                            Rds.ExecuteNonQuery(
-                                statements: new SqlStatement[]
-                                {
-                                    Rds.InsertItems(
-                                        tableType: Sqls.TableTypes.History,
-                                        param: Rds.ItemsParam()
-                                            .ReferenceId(resultModel.ResultId)
-                                            .Ver(resultModel.Ver)
-                                            .ReferenceType("Results")
-                                            .SiteId(resultModel.SiteId)
-                                            .Title(ResultsUtility.TitleDisplayValue(
-                                                siteSettings, resultModel))
-                                            .Subset(Jsons.ToJson(new ResultSubset(
-                                                resultModel, siteSettings))))
-                                });
-                        }
-                    });
                 Rds.ExecuteTable(statements: Rds.SelectWikis(
                     tableType: Sqls.TableTypes.Normal,
                     column: Rds.WikisColumn()
@@ -356,42 +284,6 @@ namespace Implem.Pleasanter.Libraries.Initializers
                                 {
                                     Rds.InsertItems(
                                         tableType: Sqls.TableTypes.Deleted,
-                                        param: Rds.ItemsParam()
-                                            .ReferenceId(wikiModel.WikiId)
-                                            .Ver(wikiModel.Ver)
-                                            .ReferenceType("Wikis")
-                                            .SiteId(wikiModel.SiteId)
-                                            .Title(WikisUtility.TitleDisplayValue(
-                                                siteSettings, wikiModel))
-                                            .Subset(Jsons.ToJson(new WikiSubset(
-                                                wikiModel, siteSettings))))
-                                });
-                        }
-                    });
-                Rds.ExecuteTable(statements: Rds.SelectWikis(
-                    tableType: Sqls.TableTypes.History,
-                    column: Rds.WikisColumn()
-                        .SiteId()
-                        .WikiId()
-                        .Ver())).AsEnumerable().ForEach(dataRow =>
-                    {
-                        var siteSettings = new SiteModel().Get(where:
-                            Rds.SitesWhere().SiteId(dataRow["SiteId"].ToLong()))?
-                                .WikisSiteSettings();
-                        var wikiModel = new WikiModel(siteSettings).Get(
-                            tableType: Sqls.TableTypes.History,
-                            where: Rds.WikisWhere()
-                                .SiteId(dataRow["SiteId"].ToLong())
-                                .WikiId(dataRow["WikiId"].ToLong())
-                                .Ver(dataRow["Ver"].ToInt()));
-                        if (siteSettings != null &&
-                            wikiModel.AccessStatus == Databases.AccessStatuses.Selected)
-                        {
-                            Rds.ExecuteNonQuery(
-                                statements: new SqlStatement[]
-                                {
-                                    Rds.InsertItems(
-                                        tableType: Sqls.TableTypes.History,
                                         param: Rds.ItemsParam()
                                             .ReferenceId(wikiModel.WikiId)
                                             .Ver(wikiModel.Ver)
