018beb7f1e84c63f3169221336c149762fab2b0f
Taiji Uchida <uchi825@gmail.com>
2016-05-15 11:51:37 +0900

    Bug fixed: 余白をクリックしてダイアログを閉じる動作が2回目以降に動作しない問題を解消。

--- Implem.Pleasanter/Scripts/Dialogs.js
+++ Implem.Pleasanter/Scripts/Dialogs.js
@@ -1,6 +1,6 @@
 ﻿$(function () {
     $(document).on('click', '.ui-widget-overlay', function () {
-        $(this).prev().find('.ui-dialog-content').dialog('close');
+        $('.ui-dialog-content:visible').dialog('close');
     });
     func.clearDialogs = function () {
         $('body > .ui-dialog').remove();
