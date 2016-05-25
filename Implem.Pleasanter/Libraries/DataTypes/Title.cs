e675f16e2dab9e91001a78dcc1f84e4c11048f01
Taiji Uchida <uchi825@gmail.com>
2016-05-26 07:13:51 +0900

    Refactored: TitleBodyクラスをファイルに切り出し。

--- Implem.Pleasanter/Libraries/DataTypes/Title.cs
+++ Implem.Pleasanter/Libraries/DataTypes/Title.cs
@@ -79,42 +79,6 @@ namespace Implem.Pleasanter.Libraries.DataTypes
         }
     }
 
-    public class TitleBody : Title, IConvertable
-    {
-        public string Body = string.Empty;
-
-        public TitleBody()
-        {
-        }
-
-        public TitleBody(long id, string title, string displayValue, string body)
-        {
-            Id = id;
-            Value = title;
-            DisplayValue = displayValue;
-            Body = body;
-        }
-
-        public override string ToString()
-        {
-            return Value + "\r\n" + Body;
-        }
-
-        public override HtmlBuilder Td(HtmlBuilder hb, Column column)
-        {
-            return hb.Td(action: () => hb
-                .Div(css: "grid-title-body", action: () => hb
-                    .P(css: "title", action: () => TdTitle(hb, column))
-                    .P(css: "body markup", action: () => hb
-                         .Text(text: Body))));
-        }
-
-        public override string ToExport(Column column)
-        {
-            return ToString();
-        }
-    }
-
     public static class TitleUtility
     {
         public static string DisplayValue(SiteSettings siteSettings, DataRow dataRow)
