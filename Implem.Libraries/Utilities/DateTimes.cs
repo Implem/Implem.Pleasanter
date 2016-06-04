13cd76aeb930bde2d7e24618d7fd3f4a090bad16
Taiji Uchida <uchi825@gmail.com>
2016-06-05 05:18:57 +0900

    Refactored: 使用していないメソッドを削除。

--- Implem.Libraries/Utilities/DateTimes.cs
+++ Implem.Libraries/Utilities/DateTimes.cs
@@ -17,40 +17,5 @@ namespace Implem.Libraries.Utilities
         {
             return dateTime.ToString("yyyyMMddHHmmss");
         }
-
-        public static string Full_()
-        {
-            return Full_(DateTime.Now);
-        }
-
-        public static string Full_(DateTime dateTime)
-        {
-            return dateTime.ToString("yyyy/MM/dd HH:mm:ss");
-        }
-
-        public static string YearMonthDay()
-        {
-            return YearMonthDay(DateTime.Now);
-        }
-
-        public static string YearMonthDay(DateTime dateTime)
-        {
-            return dateTime.ToString("yyyyMMdd");
-        }
-
-        public static string YearMonth()
-        {
-            return YearMonth(DateTime.Now);
-        }
-
-        public static string YearMonth(DateTime dateTime)
-        {
-            return dateTime.ToString("yyyyMM");
-        }
-
-        public static string Year(DateTime dateTime)
-        {
-            return dateTime.ToString("yyyy");
-        }
     }
 }
