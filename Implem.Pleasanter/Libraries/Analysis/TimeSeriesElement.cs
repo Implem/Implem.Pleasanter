6887299255a8c5fff8c8a34196b042613498a744
Taiji Uchida <uchi825@gmail.com>
2016-06-06 05:00:10 +0900

    時系列チャートの要素データ格納用クラス作成。

---
+++ Implem.Pleasanter/Libraries/Analysis/TimeSeriesElement.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Implem.Pleasanter.Libraries.Analysis
+{
+    public class TimeSeriesElement
+    {
+        public DateTime Time;
+        public int Number;
+        public string Name;
+        public decimal Value;
+
+        public TimeSeriesElement(DateTime time, int number, string name, decimal value)
+        {
+            Time = time;
+            Number = number;
+            Name = name;
+            Value = value;
+        }
+    }
+}
