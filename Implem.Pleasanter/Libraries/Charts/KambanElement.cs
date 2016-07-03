a650aee1e97c5de37e82ff1c2955d1c0c93ba09a
Taiji Uchida <uchi825@gmail.com>
2016-07-04 07:14:01 +0900

    カンバン用クラスの作成。

---
+++ Implem.Pleasanter/Libraries/Charts/KambanElement.cs
@@ -0,0 +1,24 @@
+﻿using Implem.Pleasanter.Libraries.DataTypes;
+using System;
+namespace Implem.Pleasanter.Libraries.Charts
+{
+    public class KambanElement
+    {
+        public long Id;
+        public string Title;
+        public DateTime StartTime;
+        public CompletionTime CompletionTime;
+        public WorkValue WorkValue;
+        public ProgressRate ProgressRate;
+        public decimal RemainingWorkValue;
+        public Status Status;
+        public User Manager;
+        public User Owner;
+        public string Group;
+        public decimal Value;
+
+        public KambanElement()
+        {
+        }
+    }
+}
