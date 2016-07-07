34f20a309e84ef1e91cd963492cc71e82da76ff3
Taiji Uchida <uchi825@gmail.com>
2016-07-08 05:49:48 +0900

    Bug fixed: イベントハンドラが二重登録される問題を解消。

--- Implem.Pleasanter/Scripts/Kamban.js
+++ Implem.Pleasanter/Scripts/Kamban.js
@@ -18,10 +18,12 @@
             requestByForm(getForm($(this)), $('#KambanGrid'));
         }
     });
+};
+$(function () {
     $(document).on('dblclick', '.kamban-item', function () {
         location.href = $('#BaseUrl').val() + $(this).attr('data-id');
     });
     $(document).on('click', '.kamban-item .ui-icon-pencil', function () {
         location.href = $('#BaseUrl').val() + $(this).parent().attr('data-id');
     });
-};
+});
