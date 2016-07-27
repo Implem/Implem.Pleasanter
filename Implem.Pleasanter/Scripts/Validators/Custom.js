ec1f61fd7492ed4731a36060a536e0b239f84106
Taiji Uchida <uchi825@gmail.com>
2016-07-27 19:59:21 +0900

    Bug fixed: 数値カスタムフィールドで小数点以下の数値が登録できない問題を解消。

--- Implem.Pleasanter/Scripts/Validators/Custom.js
+++ Implem.Pleasanter/Scripts/Validators/Custom.js
@@ -1,6 +1,6 @@
 ﻿$.validator.addMethod(
     'c_num',
     function (value, element) {
-        return this.optional(element) || /^(-)?(¥|\\|\$)?[\d,]+$/.test(value);
+        return this.optional(element) || /^(-)?(¥|\\|\$)?[\d,.]+$/.test(value);
     }
 );
