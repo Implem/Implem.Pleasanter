3cf846a711dc512460f8fd08844e27d5db67c733
Taiji Uchida <uchi825@gmail.com>
2016-08-20 07:19:48 +0900

    Refactored: 不要なusingの削除。

--- Implem.Pleasanter/Libraries/Scripts/JavaScripts.cs
+++ Implem.Pleasanter/Libraries/Scripts/JavaScripts.cs
@@ -1,5 +1,4 @@
-﻿using Implem.DefinitionAccessor;
-using Implem.Pleasanter.Libraries.Requests;
+﻿using Implem.Pleasanter.Libraries.Requests;
 using Implem.Pleasanter.Libraries.Security;
 using Implem.Pleasanter.Libraries.Settings;
 namespace Implem.Pleasanter.Libraries.Scripts
