c3ca68d669f2e4ae391ccd4c2be223c4426b1ffc
Taiji Uchida <uchi825@gmail.com>
2016-09-01 21:26:45 +0900

    Refactored: 使用していないメソッドを削除。

--- Implem.Libraries/Utilities/Bools.cs
+++ Implem.Libraries/Utilities/Bools.cs
@@ -2,11 +2,6 @@
 {
     public static class Bools
     {
-        public static int ToOneOrZero(this bool self)
-        {
-            return self ? 1 : 0;
-        }
-
         public static string ToOneOrZeroString(this bool self)
         {
             return self ? "1" : "0";
