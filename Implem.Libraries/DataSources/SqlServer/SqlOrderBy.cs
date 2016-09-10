7e6014b164336ad02fc58d4f4d2a37b92408cec3
Taiji Uchida <uchi825@gmail.com>
2016-09-11 08:02:21 +0900

    Refactored: SqlOrderByCollectionクラスを別ファイルに切り出し。

--- Implem.Libraries/DataSources/SqlServer/SqlOrderBy.cs
+++ Implem.Libraries/DataSources/SqlServer/SqlOrderBy.cs
@@ -1,9 +1,4 @@
-﻿using Implem.Libraries.Classes;
-using Implem.Libraries.Utilities;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-namespace Implem.Libraries.DataSources.SqlServer
+﻿namespace Implem.Libraries.DataSources.SqlServer
 {
     public class SqlOrderBy
     {
@@ -21,48 +16,6 @@ namespace Implem.Libraries.DataSources.SqlServer
         {
             ColumnBracket = columnBracket;
             OrderType = orderType;
-        }
-    }
-
-    public class SqlOrderByCollection : ListEx<SqlOrderBy>
-    {
-        public SqlOrderByCollection(params SqlOrderBy[] sqlOrderByCollection)
-        {
-            this.AddRange(sqlOrderByCollection.Where(o =>
-                o.OrderType != SqlOrderBy.Types.release));
-        }
-
-        public SqlOrderByCollection Add(SqlOrderBy.Types type, params string[] columnBrackets)
-        {
-            if (type != SqlOrderBy.Types.release)
-            {
-                columnBrackets.ForEach(columnBracket =>
-                    base.Add(new SqlOrderBy(columnBracket, type)));
-            }
-            return this;
-        }
-
-        public void BuildCommandText(StringBuilder commandText, int pageSize, int? commandCount)
-        {
-            if (this.Count > 0)
-            {
-                var orderBy = new Dictionary<string, string>();
-                this.ForEach(o =>
-                {
-                    if (!orderBy.ContainsKey(o.ColumnBracket))
-                    {
-                        orderBy.Add(o.ColumnBracket, o.ColumnBracket + " " + o.OrderType);
-                    }
-                });
-                commandText.Append("order by ", orderBy.Values.Join(), " ");
-                if (pageSize != 0)
-                {
-                    commandText.Append(
-                        "offset @_Offset", commandCount.ToString(),
-                        " rows fetch next @_PageSize", commandCount.ToString(),
-                        " rows only ");
-                }
-            }
         }
     }
 }
