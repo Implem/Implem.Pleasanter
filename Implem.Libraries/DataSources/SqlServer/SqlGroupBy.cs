9498060484fc5fd5a66296375227e8b278e52a9b
Taiji Uchida <uchi825@gmail.com>
2016-09-13 07:21:16 +0900

    Refactored: SqlGroupByCollectionクラスを別ファイルに切り出し。

--- Implem.Libraries/DataSources/SqlServer/SqlGroupBy.cs
+++ Implem.Libraries/DataSources/SqlServer/SqlGroupBy.cs
@@ -1,8 +1,4 @@
-﻿using Implem.Libraries.Classes;
-using Implem.Libraries.Utilities;
-using System.Linq;
-using System.Text;
-namespace Implem.Libraries.DataSources.SqlServer
+﻿namespace Implem.Libraries.DataSources.SqlServer
 {
     public class SqlGroupBy
     {
@@ -11,28 +7,6 @@ namespace Implem.Libraries.DataSources.SqlServer
         public SqlGroupBy(string columnBracket)
         {
             ColumnBracket = columnBracket;
-        }
-    }
-
-    public class SqlGroupByCollection : ListEx<SqlGroupBy>
-    {
-        public SqlGroupByCollection(params SqlGroupBy[] sqlGroupByCollection)
-        {
-            this.AddRange(sqlGroupByCollection);
-        }
-
-        public SqlGroupByCollection Add(params string[] columnBrackets)
-        {
-            columnBrackets.ForEach(columnBracket => base.Add(new SqlGroupBy(columnBracket)));
-            return this;
-        }
-
-        public void BuildCommandText(StringBuilder commandText)
-        {
-            if (this.Count > 0)
-            {
-                commandText.Append("group by ", this.Select(o => o.ColumnBracket).Join(), " ");
-            }
         }
     }
 }
---
+++ Implem.Libraries/DataSources/SqlServer/SqlGroupByCollection.cs
@@ -0,0 +1,28 @@
+﻿using Implem.Libraries.Classes;
+using Implem.Libraries.Utilities;
+using System.Linq;
+using System.Text;
+namespace Implem.Libraries.DataSources.SqlServer
+{
+    public class SqlGroupByCollection : ListEx<SqlGroupBy>
+    {
+        public SqlGroupByCollection(params SqlGroupBy[] sqlGroupByCollection)
+        {
+            this.AddRange(sqlGroupByCollection);
+        }
+
+        public SqlGroupByCollection Add(params string[] columnBrackets)
+        {
+            columnBrackets.ForEach(columnBracket => base.Add(new SqlGroupBy(columnBracket)));
+            return this;
+        }
+
+        public void BuildCommandText(StringBuilder commandText)
+        {
+            if (this.Count > 0)
+            {
+                commandText.Append("group by ", this.Select(o => o.ColumnBracket).Join(), " ");
+            }
+        }
+    }
+}
