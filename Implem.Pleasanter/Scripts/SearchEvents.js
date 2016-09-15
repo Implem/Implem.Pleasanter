1495c27fcb914a8a4ed6ba37c21dffba0ac8b451
Taiji Uchida <uchi825@gmail.com>
2016-09-15 19:37:19 +0900

    Bug fixed: 検索画面以外でもスクロール等の不要なイベントが登録される問題を解消。

--- Implem.Pleasanter/Scripts/SearchEvents.js
+++ Implem.Pleasanter/Scripts/SearchEvents.js
@@ -5,17 +5,19 @@
     $(document).on('keydown', '#Search', function (e) {
         if (e.which === 13) $p.search($(this).val(), $(this).hasClass('redirect'));
     });
-    $(window).on('scroll resize', function () {
-        var $control = $('#SearchResults')
-        if ($control.length) {
-            if ($(window).scrollTop() + $(window).height() >= $control.offset().top + $control.height()) {
-                if ($('#SearchOffset').val() !== '-1') {
-                    $p.search($('#Search').val(), false, $('#SearchOffset').val());
+    if ($('#SearchResults').length === 1) {
+        $(window).on('scroll resize', function () {
+            var $control = $('#SearchResults')
+            if ($control.length) {
+                if ($(window).scrollTop() + $(window).height() >= $control.offset().top + $control.height()) {
+                    if ($('#SearchOffset').val() !== '-1') {
+                        $p.search($('#Search').val(), false, $('#SearchOffset').val());
+                    }
                 }
             }
-        }
-    });
-    $(document).on('click', '#SearchResults .result', function () {
-        location.href = $(this).attr('data-href');
-    });
+        });
+        $(document).on('click', '#SearchResults .result', function () {
+            location.href = $(this).attr('data-href');
+        });
+    }
 });
