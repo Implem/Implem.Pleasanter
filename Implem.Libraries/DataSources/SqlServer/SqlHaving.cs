4102e9bd2a6ffada1887aa4ee5b73cd972a8de31
Taiji Uchida <uchi825@gmail.com>
2016-09-19 09:21:41 +0900

    Refactored: SqlHavingCollectionクラスを別ファイルに切り出し。

--- Implem.Libraries/DataSources/SqlServer/SqlHaving.cs
+++ Implem.Libraries/DataSources/SqlServer/SqlHaving.cs
@@ -1,5 +1,4 @@
-﻿using Implem.Libraries.Classes;
-using Implem.Libraries.Utilities;
+﻿using Implem.Libraries.Utilities;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
@@ -151,67 +150,6 @@ namespace Implem.Libraries.DataSources.SqlServer
             Or.MultiClauseOperator = " or ";
             Or.BuildCommandText(sqlContainer, sqlCommand, commandText, commandCount);
             return "(" + commandText + ")";
-        }
-    }
-
-    public class SqlHavingCollection : ListEx<SqlHaving>
-    {
-        public string Clause = "having ";
-        public string MultiClauseOperator = " and ";
-
-        public SqlHavingCollection(params SqlHaving[] sqlHavingCollection)
-        {
-            this.AddRange(sqlHavingCollection);
-        }
-
-        public SqlHavingCollection Add(
-            string columnBracket = null,
-            string name = "",
-            object value = null,
-            string _operator = "=",
-            string multiParamOperator = " and ",
-            SqlStatement sub = null,
-            string raw = "",
-            SqlHavingCollection or = null,
-            bool _using = true)
-        {
-            base.Add(new SqlHaving(
-                columnBracket: columnBracket,
-                name: name,
-                value: value,
-                _operator: _operator,
-                multiParamOperator: multiParamOperator,
-                sub: sub,
-                raw: raw,
-                _using: _using));
-            return this;
-        }
-
-        public void BuildCommandText(
-            SqlContainer sqlContainer, 
-            SqlCommand sqlCommand, 
-            StringBuilder commandText, 
-            int? commandCount)
-        {
-            commandText.Append(Sql(sqlContainer, sqlCommand, commandCount));
-        }
-
-        public string Sql(
-            SqlContainer sqlContainer,
-            SqlCommand sqlCommand,
-            int? commandCount)
-        {
-            return this.Any(o => o.Using)
-                ? Clause + this
-                    .Where(o => o.Using)
-                    .Select(o => o.Sql(sqlContainer, sqlCommand, commandCount))
-                    .Join(MultiClauseOperator) + " "
-                : string.Empty;
-        }
-
-        public void Prefix(string prefix)
-        {
-            this.ForEach(o => o.Name += prefix);
         }
     }
 }
---
+++ Implem.Libraries/DataSources/SqlServer/SqlHavingCollection.cs
@@ -0,0 +1,68 @@
+﻿using Implem.Libraries.Classes;
+using Implem.Libraries.Utilities;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+namespace Implem.Libraries.DataSources.SqlServer
+{
+    public class SqlHavingCollection : ListEx<SqlHaving>
+    {
+        public string Clause = "having ";
+        public string MultiClauseOperator = " and ";
+
+        public SqlHavingCollection(params SqlHaving[] sqlHavingCollection)
+        {
+            this.AddRange(sqlHavingCollection);
+        }
+
+        public SqlHavingCollection Add(
+            string columnBracket = null,
+            string name = "",
+            object value = null,
+            string _operator = "=",
+            string multiParamOperator = " and ",
+            SqlStatement sub = null,
+            string raw = "",
+            SqlHavingCollection or = null,
+            bool _using = true)
+        {
+            base.Add(new SqlHaving(
+                columnBracket: columnBracket,
+                name: name,
+                value: value,
+                _operator: _operator,
+                multiParamOperator: multiParamOperator,
+                sub: sub,
+                raw: raw,
+                _using: _using));
+            return this;
+        }
+
+        public void BuildCommandText(
+            SqlContainer sqlContainer, 
+            SqlCommand sqlCommand, 
+            StringBuilder commandText, 
+            int? commandCount)
+        {
+            commandText.Append(Sql(sqlContainer, sqlCommand, commandCount));
+        }
+
+        public string Sql(
+            SqlContainer sqlContainer,
+            SqlCommand sqlCommand,
+            int? commandCount)
+        {
+            return this.Any(o => o.Using)
+                ? Clause + this
+                    .Where(o => o.Using)
+                    .Select(o => o.Sql(sqlContainer, sqlCommand, commandCount))
+                    .Join(MultiClauseOperator) + " "
+                : string.Empty;
+        }
+
+        public void Prefix(string prefix)
+        {
+            this.ForEach(o => o.Name += prefix);
+        }
+    }
+}
