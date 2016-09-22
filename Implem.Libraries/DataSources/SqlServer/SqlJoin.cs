1bea6c7f77e9a993978ee247093260ed0080d6a5
Taiji Uchida <uchi825@gmail.com>
2016-09-23 08:47:36 +0900

    Refactored: SqlJoinCollectionクラスを別ファイルに切り出し。

--- Implem.Libraries/DataSources/SqlServer/SqlJoin.cs
+++ Implem.Libraries/DataSources/SqlServer/SqlJoin.cs
@@ -1,8 +1,4 @@
-﻿using Implem.Libraries.Classes;
-using Implem.Libraries.Utilities;
-using System.Linq;
-using System.Text;
-namespace Implem.Libraries.DataSources.SqlServer
+﻿namespace Implem.Libraries.DataSources.SqlServer
 {
     public class SqlJoin
     {
@@ -11,25 +7,6 @@ namespace Implem.Libraries.DataSources.SqlServer
         public SqlJoin(string joinExpression)
         {
             JoinExpression = joinExpression;
-        }
-    }
-
-    public class SqlJoinCollection : ListEx<SqlJoin>
-    {
-        public SqlJoinCollection(params SqlJoin[] sqlJoinCollection)
-        {
-            this.AddRange(sqlJoinCollection);
-        }
-
-        public SqlJoinCollection Add(params string[] columnBrackets)
-        {
-            columnBrackets.ForEach(columnBracket => base.Add(new SqlJoin(columnBracket)));
-            return this;
-        }
-
-        public void BuildCommandText(StringBuilder commandText)
-        {
-            commandText.Append(this.Select(o => o.JoinExpression).Join(" "));
         }
     }
 }
---
+++ Implem.Libraries/DataSources/SqlServer/SqlJoinCollection.cs
@@ -0,0 +1,25 @@
+﻿using Implem.Libraries.Classes;
+using Implem.Libraries.Utilities;
+using System.Linq;
+using System.Text;
+namespace Implem.Libraries.DataSources.SqlServer
+{
+    public class SqlJoinCollection : ListEx<SqlJoin>
+    {
+        public SqlJoinCollection(params SqlJoin[] sqlJoinCollection)
+        {
+            this.AddRange(sqlJoinCollection);
+        }
+
+        public SqlJoinCollection Add(params string[] columnBrackets)
+        {
+            columnBrackets.ForEach(columnBracket => base.Add(new SqlJoin(columnBracket)));
+            return this;
+        }
+
+        public void BuildCommandText(StringBuilder commandText)
+        {
+            commandText.Append(this.Select(o => o.JoinExpression).Join(" "));
+        }
+    }
+}
