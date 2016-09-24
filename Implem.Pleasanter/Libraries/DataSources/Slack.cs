c191c03b339e73e15848a6cdb1dcc51dd379384e
Taiji Uchida <uchi825@gmail.com>
2016-09-25 08:50:24 +0900

    Slack通知のクラスを追加。

---
+++ Implem.Pleasanter/Libraries/DataSources/Slack.cs
@@ -0,0 +1,31 @@
+﻿using Implem.Libraries.Utilities;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+namespace Implem.Pleasanter.Libraries.DataSources
+{
+    public class Slack
+    {
+        public string text;
+        public string username;
+        public string icon_emoji;
+
+        public Slack(string _text, string _username, string _icon_emoji = null)
+        {
+            text = _text;
+            username = _username;
+            icon_emoji = _icon_emoji;
+        }
+
+        public void Send(string url)
+        {
+            Task.Run(() =>
+            {
+                var client = new WebClient();
+                client.Headers.Add(HttpRequestHeader.ContentType, "application/json;charset=UTF-8");
+                client.Encoding = Encoding.UTF8;
+                client.UploadString(url, this.ToJson());
+            });
+        }
+    }
+}
