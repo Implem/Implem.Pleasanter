61c4d6c2dd87dc43fa08e5d4fdc63527616b2885
Taiji Uchida <uchi825@gmail.com>
2016-09-25 09:40:10 +0900

    通知クラスを追加。

---
+++ Implem.Pleasanter/Libraries/Settings/Notification.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+namespace Implem.Pleasanter.Libraries.Settings
+{
+    public class Notification
+    {
+        public Types Type;
+        public string Address;
+        public List<string> Columns;
+
+        public enum Types : int
+        {
+            Mail = 1,
+            Slack = 2
+        }
+
+        public Notification(int type, string address, List<string> columns)
+        {
+            Type = (Types)type;
+            Address = address;
+            Columns = columns;
+        }
+
+        public void Update(string address, List<string> columns)
+        {
+            Address = address;
+            Columns = columns;
+        }
+    }
+}
