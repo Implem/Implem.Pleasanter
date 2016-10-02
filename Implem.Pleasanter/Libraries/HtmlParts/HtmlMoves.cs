cf25f6ce599c33056c86fec121e5a526d31bc9ad
Taiji Uchida <uchi825@gmail.com>
2016-10-02 09:27:04 +0900

    sendメソッドの引数formIdが指定されない場合、$eventSenderの上位formを使う仕様に変更。

--- Implem.Pleasanter/Libraries/HtmlParts/HtmlMoves.cs
+++ Implem.Pleasanter/Libraries/HtmlParts/HtmlMoves.cs
@@ -1,13 +1,11 @@
-﻿using Implem.DefinitionAccessor;
-using Implem.Pleasanter.Libraries.Html;
+﻿using Implem.Pleasanter.Libraries.Html;
 using Implem.Pleasanter.Libraries.Responses;
 using System.Collections.Generic;
 namespace Implem.Pleasanter.Libraries.HtmlParts
 {
     public static class HtmlMoves
     {
-        public static HtmlBuilder MoveDialog(
-            this HtmlBuilder hb, string referenceType, long id, bool bulk = false)
+        public static HtmlBuilder MoveDialog(this HtmlBuilder hb, bool bulk = false)
         {
             return hb.Div(
                 attributes: new HtmlAttributes()
@@ -15,28 +13,24 @@ namespace Implem.Pleasanter.Libraries.HtmlParts
                     .Class("dialog")
                     .Title(Displays.MoveSettings()),
                 action: () => hb
-                    .Form(
-                        attributes: new HtmlAttributes()
-                            .Action(Navigations.Action(referenceType, id)),
-                        action: () => hb
-                            .FieldDropDown(
-                                controlId: "MoveTargets",
-                                labelText: Displays.Destination(),
-                                optionCollection: new Dictionary<string, ControlData>())
-                            .P(css: "message-dialog")
-                            .Div(css: "command-center", action: () => hb
-                                .Button(
-                                    text: Displays.Move(),
-                                    controlCss: "button-icon",
-                                    onClick: "$p.move($(this));",
-                                    icon: "ui-icon-copy",
-                                    action: bulk ? "BulkMove" : "Move",
-                                    method: "put")
-                                .Button(
-                                    text: Displays.Cancel(),
-                                    controlCss: "button-icon",
-                                    onClick: "$p.closeDialog($(this));",
-                                    icon: "ui-icon-cancel"))));
+                    .FieldDropDown(
+                        controlId: "MoveTargets",
+                        labelText: Displays.Destination(),
+                        optionCollection: new Dictionary<string, ControlData>())
+                    .P(css: "message-dialog")
+                    .Div(css: "command-center", action: () => hb
+                        .Button(
+                            text: Displays.Move(),
+                            controlCss: "button-icon",
+                            onClick: "$p.move($(this));",
+                            icon: "ui-icon-copy",
+                            action: bulk ? "BulkMove" : "Move",
+                            method: "put")
+                        .Button(
+                            text: Displays.Cancel(),
+                            controlCss: "button-icon",
+                            onClick: "$p.closeDialog($(this));",
+                            icon: "ui-icon-cancel")));
         }
     }
 }
