939b75e7c700b76e95ae6b60c3151fc9278b2d17
Taiji Uchida <uchi825@gmail.com>
2016-10-03 05:16:53 +0900

    Refactored: 戻り先Url関連メソッドをHtmlBack.csに切り出し。

---
+++ Implem.Pleasanter/Libraries/HtmlParts/HtmlBack.cs
@@ -0,0 +1,65 @@
+﻿using Implem.Libraries.Utilities;
+using Implem.Pleasanter.Libraries.Html;
+using Implem.Pleasanter.Libraries.Requests;
+using Implem.Pleasanter.Libraries.Responses;
+using System.Web;
+namespace Implem.Pleasanter.Libraries.HtmlParts
+{
+    public static class HtmlBack
+    {
+        public static HtmlBuilder BackUrl(
+            this HtmlBuilder hb, long siteId, long parentId, string referenceType)
+        {
+            return !Request.IsAjax()
+                ? hb.Hidden(
+                    controlId: "BackUrl",
+                    rawValue: BackUrl(siteId, parentId, referenceType))
+                : hb;
+        }
+
+        private static string BackUrl(long siteId, long parentId, string referenceType)
+        {
+            var controller = Routes.Controller();
+            var referer = HttpUtility.UrlDecode(new Request(HttpContext.Current).UrlReferrer());
+            switch (controller)
+            {
+                case "admins":
+                    return Navigations.Top();
+                case "depts":
+                case "users":
+                    switch (Routes.Action())
+                    {
+                        case "new":
+                        case "edit":
+                            return Strings.CoalesceEmpty(
+                                referer, Navigations.Get(controller));
+                        default:
+                            return Navigations.Get("Admins");
+                    }
+                default:
+                    switch (referenceType)
+                    {
+                        case "Sites":
+                            switch (Routes.Action())
+                            {
+                                case "new":
+                                case "edit":
+                                    return Navigations.ItemIndex(siteId);
+                                default:
+                                    return Navigations.ItemIndex(parentId);
+                            }
+                        default:
+                            switch (Routes.Action())
+                            {
+                                case "new":
+                                case "edit":
+                                    return Strings.CoalesceEmpty(
+                                        referer, Navigations.ItemIndex(siteId));
+                                default:
+                                    return Navigations.ItemIndex(parentId);
+                            }
+                    }
+            }
+        }
+    }
+}
--- Implem.Pleasanter/Libraries/HtmlParts/HtmlTemplates.cs
+++ Implem.Pleasanter/Libraries/HtmlParts/HtmlTemplates.cs
@@ -3,13 +3,11 @@ using Implem.Libraries.Utilities;
 using Implem.Pleasanter.Libraries.Html;
 using Implem.Pleasanter.Libraries.Images;
 using Implem.Pleasanter.Libraries.Models;
-using Implem.Pleasanter.Libraries.Requests;
 using Implem.Pleasanter.Libraries.Responses;
 using Implem.Pleasanter.Libraries.Security;
 using Implem.Pleasanter.Libraries.Server;
 using Implem.Pleasanter.Models;
 using System;
-using System.Web;
 namespace Implem.Pleasanter.Libraries.HtmlParts
 {
     public static class HtmlTemplates
@@ -175,61 +173,6 @@ namespace Implem.Pleasanter.Libraries.HtmlParts
                 useBreadcrumb: false,
                 useTitle: false,
                 useNavigationMenu: false);
-        }
-
-        private static HtmlBuilder BackUrl(
-            this HtmlBuilder hb, long siteId, long parentId, string referenceType)
-        {
-            return !Request.IsAjax()
-                ? hb.Hidden(
-                    controlId: "BackUrl",
-                    rawValue: BackUrl(siteId, parentId, referenceType))
-                : hb;
-        }
-
-        private static string BackUrl(long siteId, long parentId, string referenceType)
-        {
-            var controller = Routes.Controller();
-            var referer = HttpUtility.UrlDecode(new Request(HttpContext.Current).UrlReferrer());
-            switch (controller)
-            {
-                case "admins":
-                    return Navigations.Top();
-                case "depts":
-                case "users":
-                    switch (Routes.Action())
-                    {
-                        case "new":
-                        case "edit":
-                            return Strings.CoalesceEmpty(
-                                referer, Navigations.Get(controller));
-                        default:
-                            return Navigations.Get("Admins");
-                    }
-                default:
-                    switch (referenceType)
-                    {
-                        case "Sites":
-                            switch (Routes.Action())
-                            {
-                                case "new":
-                                case "edit":
-                                    return Navigations.ItemIndex(siteId);
-                                default:
-                                    return Navigations.ItemIndex(parentId);
-                            }
-                        default:
-                            switch (Routes.Action())
-                            {
-                                case "new":
-                                case "edit":
-                                    return Strings.CoalesceEmpty(
-                                        referer, Navigations.ItemIndex(siteId));
-                                default:
-                                    return Navigations.ItemIndex(parentId);
-                            }
-                    }
-            }
         }
     }
 }
