32d5e9edf387808352967e05e9860a554f3ff04e
Taiji Uchida <uchi825@gmail.com>
2016-10-08 21:12:49 +0900

    Refactored: インデントの調整。

--- Implem.Pleasanter/Models/Issues/IssueCollection.cs
+++ Implem.Pleasanter/Models/Issues/IssueCollection.cs
@@ -44,7 +44,7 @@ namespace Implem.Pleasanter.Models
             if (get)
             {
                 Set(siteSettings, permissionType, Get(
-                siteSettings: siteSettings,
+                    siteSettings: siteSettings,
                     column: column,
                     join: join,
                     where: where,
