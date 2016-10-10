a63d06b72d2de91bb7581bb6d65cc334f76ef2e7
Taiji Uchida <uchi825@gmail.com>
2016-10-10 13:02:46 +0900

    エラークラスのメソッドにメッセージ用の追加データを受け取る引数を追加。

--- Implem.Pleasanter/Libraries/General/Error.cs
+++ Implem.Pleasanter/Libraries/General/Error.cs
@@ -38,41 +38,65 @@ namespace Implem.Pleasanter.Libraries.General
             return type != Types.None;
         }
 
-        public static Message Message(this Types type)
+        public static Message Message(this Types type, params string[] data)
         {
             switch (type)
             {
-                case Types.InvalidRequest: return Messages.InvalidRequest();
-                case Types.Authentication: return Messages.Authentication();
-                case Types.PasswordNotChanged: return Messages.PasswordNotChanged();
-                case Types.UpdateConflicts: return Messages.UpdateConflicts();
-                case Types.DeleteConflicts: return Messages.DeleteConflicts();
-                case Types.HasNotPermission: return Messages.HasNotPermission();
-                case Types.IncorrectCurrentPassword: return Messages.IncorrectCurrentPassword();
-                case Types.PermissionNotSelfChange: return Messages.PermissionNotSelfChange();
-                case Types.DefinitionNotFound: return Messages.DefinitionNotFound();
-                case Types.CantSetAtTopOfSite: return Messages.CantSetAtTopOfSite();
-                case Types.NotFound: return Messages.NotFound();
-                case Types.RequireMailAddresses: return Messages.RequireMailAddresses();
-                case Types.RequireColumn: return Messages.RequireColumn();
-                case Types.ExternalMailAddress: return Messages.ExternalMailAddress();
-                case Types.BadMailAddress: return Messages.BadMailAddress();
-                case Types.MailAddressHasNotSet: return Messages.MailAddressHasNotSet();
-                case Types.BadFormat: return Messages.BadFormat();
-                case Types.FileNotFound: return Messages.FileNotFound();
-                case Types.NotRequiredColumn: return Messages.NotRequiredColumn();
-                case Types.InvalidCsvData: return Messages.InvalidCsvData();
-                case Types.FailedReadFile: return Messages.FailedReadFile();
-                case Types.CanNotDisabled: return Messages.CanNotDisabled();
-                case Types.AlreadyAdded: return Messages.AlreadyAdded();
-                case Types.InvalidFormula: return Messages.InvalidFormula();
+                case Types.InvalidRequest:
+                    return Messages.InvalidRequest(data);
+                case Types.Authentication:
+                    return Messages.Authentication(data);
+                case Types.PasswordNotChanged:
+                    return Messages.PasswordNotChanged(data);
+                case Types.UpdateConflicts:
+                    return Messages.UpdateConflicts(data);
+                case Types.DeleteConflicts:
+                    return Messages.DeleteConflicts(data);
+                case Types.HasNotPermission:
+                    return Messages.HasNotPermission(data);
+                case Types.IncorrectCurrentPassword:
+                    return Messages.IncorrectCurrentPassword(data);
+                case Types.PermissionNotSelfChange:
+                    return Messages.PermissionNotSelfChange(data);
+                case Types.DefinitionNotFound:
+                    return Messages.DefinitionNotFound(data);
+                case Types.CantSetAtTopOfSite:
+                    return Messages.CantSetAtTopOfSite(data);
+                case Types.NotFound:
+                    return Messages.NotFound(data);
+                case Types.RequireMailAddresses:
+                    return Messages.RequireMailAddresses(data);
+                case Types.RequireColumn:
+                    return Messages.RequireColumn(data);
+                case Types.ExternalMailAddress:
+                    return Messages.ExternalMailAddress(data);
+                case Types.BadMailAddress:
+                    return Messages.BadMailAddress(data);
+                case Types.MailAddressHasNotSet:
+                    return Messages.MailAddressHasNotSet(data);
+                case Types.BadFormat:
+                    return Messages.BadFormat(data);
+                case Types.FileNotFound:
+                    return Messages.FileNotFound(data);
+                case Types.NotRequiredColumn:
+                    return Messages.NotRequiredColumn(data);
+                case Types.InvalidCsvData:
+                    return Messages.InvalidCsvData(data);
+                case Types.FailedReadFile:
+                    return Messages.FailedReadFile(data);
+                case Types.CanNotDisabled:
+                    return Messages.CanNotDisabled(data);
+                case Types.AlreadyAdded:
+                    return Messages.AlreadyAdded(data);
+                case Types.InvalidFormula:
+                    return Messages.InvalidFormula(data);
                 default: return null;
             }
         }
 
-        public static string MessageJson(this Types type)
+        public static string MessageJson(this Types type, params string[] data)
         {
-            return new ResponseCollection().Message(type.Message()).ToJson();
+            return new ResponseCollection().Message(type.Message(data)).ToJson();
         }
     }
 }
