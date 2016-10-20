70ef66cdd8e349981d1b954ac4287ad416ce44bc
Taiji Uchida <uchi825@gmail.com>
2016-10-20 20:10:01 +0900

    Refactored: 使用していないプロパティを削除。

--- Implem.Pleasanter/Models/Sites/SiteSubset.cs
+++ Implem.Pleasanter/Models/Sites/SiteSubset.cs
@@ -10,53 +10,29 @@ namespace Implem.Pleasanter.Models
     public class SiteSubset
     {
         public int TenantId;
-        public string TenantId_LabelText;
         public long SiteId;
-        public string SiteId_LabelText;
         public Time UpdatedTime;
-        public string UpdatedTime_LabelText;
         public int Ver;
-        public string Ver_LabelText;
         public Title Title;
-        public string Title_LabelText;
         public string Body;
-        public string Body_LabelText;
         public TitleBody TitleBody;
-        public string TitleBody_LabelText;
         public string ReferenceType;
-        public string ReferenceType_LabelText;
         public long ParentId;
-        public string ParentId_LabelText;
         public long InheritPermission;
-        public string InheritPermission_LabelText;
         public Permissions.Types PermissionType;
-        public string PermissionType_LabelText;
         public SiteSettings SiteSettings;
-        public string SiteSettings_LabelText;
         public SiteCollection Ancestors;
-        public string Ancestors_LabelText;
         public PermissionCollection PermissionSourceCollection;
-        public string PermissionSourceCollection_LabelText;
         public PermissionCollection PermissionDestinationCollection;
-        public string PermissionDestinationCollection_LabelText;
         public int SiteMenu;
-        public string SiteMenu_LabelText;
         public List<string> MonitorChangesColumns;
-        public string MonitorChangesColumns_LabelText;
         public List<string> TitleColumns;
-        public string TitleColumns_LabelText;
         public Comments Comments;
-        public string Comments_LabelText;
         public User Creator;
-        public string Creator_LabelText;
         public User Updator;
-        public string Updator_LabelText;
         public Time CreatedTime;
-        public string CreatedTime_LabelText;
         public bool VerUp;
-        public string VerUp_LabelText;
         public string Timestamp;
-        public string Timestamp_LabelText;
 
         public SiteSubset()
         {
@@ -65,53 +41,29 @@ namespace Implem.Pleasanter.Models
         public SiteSubset(SiteModel siteModel, SiteSettings siteSettings)
         {
             TenantId = siteModel.TenantId;
-            TenantId_LabelText = siteSettings.EditorColumn("TenantId")?.LabelText;
             SiteId = siteModel.SiteId;
-            SiteId_LabelText = siteSettings.EditorColumn("SiteId")?.LabelText;
             UpdatedTime = siteModel.UpdatedTime;
-            UpdatedTime_LabelText = siteSettings.EditorColumn("UpdatedTime")?.LabelText;
             Ver = siteModel.Ver;
-            Ver_LabelText = siteSettings.EditorColumn("Ver")?.LabelText;
             Title = siteModel.Title;
-            Title_LabelText = siteSettings.EditorColumn("Title")?.LabelText;
             Body = siteModel.Body;
-            Body_LabelText = siteSettings.EditorColumn("Body")?.LabelText;
             TitleBody = siteModel.TitleBody;
-            TitleBody_LabelText = siteSettings.EditorColumn("TitleBody")?.LabelText;
             ReferenceType = siteModel.ReferenceType;
-            ReferenceType_LabelText = siteSettings.EditorColumn("ReferenceType")?.LabelText;
             ParentId = siteModel.ParentId;
-            ParentId_LabelText = siteSettings.EditorColumn("ParentId")?.LabelText;
             InheritPermission = siteModel.InheritPermission;
-            InheritPermission_LabelText = siteSettings.EditorColumn("InheritPermission")?.LabelText;
             PermissionType = siteModel.PermissionType;
-            PermissionType_LabelText = siteSettings.EditorColumn("PermissionType")?.LabelText;
             SiteSettings = siteModel.SiteSettings;
-            SiteSettings_LabelText = siteSettings.EditorColumn("SiteSettings")?.LabelText;
             Ancestors = siteModel.Ancestors;
-            Ancestors_LabelText = siteSettings.EditorColumn("Ancestors")?.LabelText;
             PermissionSourceCollection = siteModel.PermissionSourceCollection;
-            PermissionSourceCollection_LabelText = siteSettings.EditorColumn("PermissionSourceCollection")?.LabelText;
             PermissionDestinationCollection = siteModel.PermissionDestinationCollection;
-            PermissionDestinationCollection_LabelText = siteSettings.EditorColumn("PermissionDestinationCollection")?.LabelText;
             SiteMenu = siteModel.SiteMenu;
-            SiteMenu_LabelText = siteSettings.EditorColumn("SiteMenu")?.LabelText;
             MonitorChangesColumns = siteModel.MonitorChangesColumns;
-            MonitorChangesColumns_LabelText = siteSettings.EditorColumn("MonitorChangesColumns")?.LabelText;
             TitleColumns = siteModel.TitleColumns;
-            TitleColumns_LabelText = siteSettings.EditorColumn("TitleColumns")?.LabelText;
             Comments = siteModel.Comments;
-            Comments_LabelText = siteSettings.EditorColumn("Comments")?.LabelText;
             Creator = siteModel.Creator;
-            Creator_LabelText = siteSettings.EditorColumn("Creator")?.LabelText;
             Updator = siteModel.Updator;
-            Updator_LabelText = siteSettings.EditorColumn("Updator")?.LabelText;
             CreatedTime = siteModel.CreatedTime;
-            CreatedTime_LabelText = siteSettings.EditorColumn("CreatedTime")?.LabelText;
             VerUp = siteModel.VerUp;
-            VerUp_LabelText = siteSettings.EditorColumn("VerUp")?.LabelText;
             Timestamp = siteModel.Timestamp;
-            Timestamp_LabelText = siteSettings.EditorColumn("Timestamp")?.LabelText;
         }
 
         public Dictionary<string, int> SearchIndexCollection()
--- Implem.Pleasanter/Models/Wikis/WikiSubset.cs
+++ Implem.Pleasanter/Models/Wikis/WikiSubset.cs
@@ -10,31 +10,18 @@ namespace Implem.Pleasanter.Models
     public class WikiSubset
     {
         public long SiteId;
-        public string SiteId_LabelText;
         public Time UpdatedTime;
-        public string UpdatedTime_LabelText;
         public long WikiId;
-        public string WikiId_LabelText;
         public int Ver;
-        public string Ver_LabelText;
         public Title Title;
-        public string Title_LabelText;
         public string Body;
-        public string Body_LabelText;
         public TitleBody TitleBody;
-        public string TitleBody_LabelText;
         public Comments Comments;
-        public string Comments_LabelText;
         public User Creator;
-        public string Creator_LabelText;
         public User Updator;
-        public string Updator_LabelText;
         public Time CreatedTime;
-        public string CreatedTime_LabelText;
         public bool VerUp;
-        public string VerUp_LabelText;
         public string Timestamp;
-        public string Timestamp_LabelText;
 
         public WikiSubset()
         {
@@ -43,31 +30,18 @@ namespace Implem.Pleasanter.Models
         public WikiSubset(WikiModel wikiModel, SiteSettings siteSettings)
         {
             SiteId = wikiModel.SiteId;
-            SiteId_LabelText = siteSettings.EditorColumn("SiteId")?.LabelText;
             UpdatedTime = wikiModel.UpdatedTime;
-            UpdatedTime_LabelText = siteSettings.EditorColumn("UpdatedTime")?.LabelText;
             WikiId = wikiModel.WikiId;
-            WikiId_LabelText = siteSettings.EditorColumn("WikiId")?.LabelText;
             Ver = wikiModel.Ver;
-            Ver_LabelText = siteSettings.EditorColumn("Ver")?.LabelText;
             Title = wikiModel.Title;
-            Title_LabelText = siteSettings.EditorColumn("Title")?.LabelText;
             Body = wikiModel.Body;
-            Body_LabelText = siteSettings.EditorColumn("Body")?.LabelText;
             TitleBody = wikiModel.TitleBody;
-            TitleBody_LabelText = siteSettings.EditorColumn("TitleBody")?.LabelText;
             Comments = wikiModel.Comments;
-            Comments_LabelText = siteSettings.EditorColumn("Comments")?.LabelText;
             Creator = wikiModel.Creator;
-            Creator_LabelText = siteSettings.EditorColumn("Creator")?.LabelText;
             Updator = wikiModel.Updator;
-            Updator_LabelText = siteSettings.EditorColumn("Updator")?.LabelText;
             CreatedTime = wikiModel.CreatedTime;
-            CreatedTime_LabelText = siteSettings.EditorColumn("CreatedTime")?.LabelText;
             VerUp = wikiModel.VerUp;
-            VerUp_LabelText = siteSettings.EditorColumn("VerUp")?.LabelText;
             Timestamp = wikiModel.Timestamp;
-            Timestamp_LabelText = siteSettings.EditorColumn("Timestamp")?.LabelText;
         }
 
         public Dictionary<string, int> SearchIndexCollection()
