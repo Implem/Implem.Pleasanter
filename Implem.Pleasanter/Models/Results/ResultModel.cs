a00a077fd8ad2c7a0fc363effb5b2d2e9b5b7e9e
Taiji Uchida <uchi825@gmail.com>
2016-10-22 05:34:32 +0900

    Refactored: 改行の調整。

--- Implem.Pleasanter/Models/Results/ResultModel.cs
+++ Implem.Pleasanter/Models/Results/ResultModel.cs
@@ -890,8 +890,7 @@ namespace Implem.Pleasanter.Models
                 });
         }
 
-        private SqlInsert InsertLinks(
-            SiteSettings ss, bool selectIdentity = false)
+        private SqlInsert InsertLinks(SiteSettings ss, bool selectIdentity = false)
         {
             var link = new Dictionary<long, long>();
             ss.ColumnCollection.Where(o => o.Link.ToBool()).ForEach(column =>
