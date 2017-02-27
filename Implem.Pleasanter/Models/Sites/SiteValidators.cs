b3da5cfe99201ec396a6cee20375a09a796ab4b9
Taiji Uchida <uchi825@gmail.com>
2017-02-27 09:13:00 +0900

    自動生成によるusingの追加。

--- Implem.Pleasanter/Models/Sites/SiteValidators.cs
+++ Implem.Pleasanter/Models/Sites/SiteValidators.cs
@@ -1,6 +1,7 @@
 ﻿using Implem.Pleasanter.Libraries.General;
 using Implem.Pleasanter.Libraries.Requests;
 using Implem.Pleasanter.Libraries.Security;
+using Implem.Pleasanter.Libraries.Server;
 using Implem.Pleasanter.Libraries.Settings;
 namespace Implem.Pleasanter.Models
 {
