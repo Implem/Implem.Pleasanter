ad473092b8d19f888ad012c4559038aa2d295e32
Taiji Uchida <uchi825@gmail.com>
2017-05-23 10:34:07 +0900

    汎用的なデータ格納クラスを追加。

---
+++ Implem.Libraries/Classes/TwoData.cs
@@ -0,0 +1,14 @@
+﻿namespace Implem.Libraries.Classes
+{
+    public class TwoData<D1, D2>
+    {
+        public D1 Data1;
+        public D2 Data2;
+
+        public TwoData(D1 data1, D2 data2)
+        {
+            Data1 = data1;
+            Data2 = data2;
+        }
+    }
+}
