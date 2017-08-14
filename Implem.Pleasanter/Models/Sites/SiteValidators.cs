21f2f245aedfea35a3d1a50e169561b4e477dab3
Taiji Uchida <uchi825@gmail.com>
2017-08-14 10:16:17 +0900

    Refactored: 改行の調整。

--- Implem.Pleasanter/Models/Sites/SiteValidators.cs
+++ Implem.Pleasanter/Models/Sites/SiteValidators.cs
@@ -5,7 +5,6 @@ using Implem.Pleasanter.Libraries.Security;
 using Implem.Pleasanter.Libraries.Server;
 using Implem.Pleasanter.Libraries.Settings;
 using System.Linq;
-
 namespace Implem.Pleasanter.Models
 {
     public static class SiteValidators
