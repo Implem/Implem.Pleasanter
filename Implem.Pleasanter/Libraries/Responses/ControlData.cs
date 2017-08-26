3f597dbb8ba1c710cfaddb47dc0514a77049e169
Taiji Uchida <uchi825@gmail.com>
2017-08-26 14:53:08 +0900

    Refactored: 引数の初期値をnullに変更。

--- Implem.Pleasanter/Libraries/Responses/ControlData.cs
+++ Implem.Pleasanter/Libraries/Responses/ControlData.cs
@@ -6,7 +6,7 @@
         public string Css;
         public string Style;
 
-        public ControlData(string text, string css = "", string style = "")
+        public ControlData(string text, string css = null, string style = null)
         {
             Text = text;
             Css = css;
