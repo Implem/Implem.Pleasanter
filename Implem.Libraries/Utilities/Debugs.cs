73f95b77e2cdd29ab7c32fbed7fc76eda366119e
Taiji Uchida <uchi825@gmail.com>
2017-09-18 12:01:10 +0900

    Refactored: 使用していないメソッドを削除。

--- Implem.Libraries/Utilities/Debugs.cs
+++ Implem.Libraries/Utilities/Debugs.cs
@@ -1,6 +1,4 @@
 ﻿using System.Diagnostics;
-using System.Linq;
-using System.Text;
 namespace Implem.Libraries.Utilities
 {
     public static class Debugs
@@ -13,24 +11,6 @@ namespace Implem.Libraries.Utilities
         public static Process CurrentProcess()
         {
             return Process.GetCurrentProcess();
-        }
-
-        public static string Trace()
-        {
-            var stringBuilder = new StringBuilder();
-            new StackTrace(1, false).GetFrames()
-                .Select(o => o.GetMethod())
-                .Where(o => o.Name != ".ctor")
-                .ForEach(method =>
-                {
-                    var callerOfClass = method.ReflectedType.Name;
-                    var callerOfMethod = method.Name;
-                    if (!callerOfClass.StartsWith("<>"))
-                    {
-                        stringBuilder.Append(callerOfClass, ".", callerOfMethod, "\r\n");
-                    }
-                });
-            return stringBuilder.ToString();
         }
     }
 }
