0a652516a4f6f1c9ee19a7515c55d84803e79f77
Taiji Uchida <uchi825@gmail.com>
2017-09-19 09:47:26 +0900

    Refactored: 使用していないメソッドを削除。

--- Implem.Libraries/Utilities/Jsons.cs
+++ Implem.Libraries/Utilities/Jsons.cs
@@ -1,15 +1,8 @@
 ﻿using Newtonsoft.Json;
-using System.Collections.Generic;
-using System.Web.Mvc;
 namespace Implem.Libraries.Utilities
 {
     public static class Jsons
     {
-        public static JsonResult Json(this Dictionary<string, object> result)
-        {
-            return Json(result);
-        }
-
         public static string ToJson(this object obj)
         {
             var settings = new JsonSerializerSettings();
