b45a7dccd98bbba0896a12ec072ae23476a4e9bf
Taiji Uchida <uchi825@gmail.com>
2017-10-06 15:54:38 +0900

    Bug fixed: リンク項目でフィルタした際にorder by句でSQLエラーが発生する問題を解消。

--- Implem.Libraries/DataSources/SqlServer/SqlOrderBy.cs
+++ Implem.Libraries/DataSources/SqlServer/SqlOrderBy.cs
@@ -28,20 +28,9 @@
 
         public string Sql(string tableBracket, Sqls.TableTypes tableType)
         {
-            string columnBracket;
-            switch (tableType)
-            {
-                case Sqls.TableTypes.Normal:
-                case Sqls.TableTypes.History:
-                case Sqls.TableTypes.Deleted:
-                    columnBracket = Sqls.TableAndColumnBracket(
-                        tableBracket: tableBracket,
-                        columnBracket: ColumnBracket);
-                    break;
-                default:
-                    columnBracket = ColumnBracket;
-                    break;
-            }
+            string columnBracket = Sqls.TableAndColumnBracket(
+                tableBracket: tableBracket,
+                columnBracket: ColumnBracket);
             var orderType = " " + OrderType.ToString().ToLower();
             switch (Function)
             {
