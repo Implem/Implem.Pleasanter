b7c487cc869f091b96aa6f3caa2c4a2161de675f
Taiji Uchida <uchi825@gmail.com>
2017-12-03 17:38:01 +0900

    Bug fixed: 組織、グループ、ユーザの一覧画面で次ページの読み込みが行なわれない問題を解消。

--- Implem.Pleasanter/Models/Groups/GroupUtilities.cs
+++ Implem.Pleasanter/Models/Groups/GroupUtilities.cs
@@ -36,11 +36,13 @@ namespace Implem.Pleasanter.Models
             var hb = new HtmlBuilder();
             var view = Views.GetBySession(ss);
             var gridData = GetGridData(ss, view);
+            var viewMode = ViewModes.GetBySession(ss.SiteId);
             return hb.Template(
                 ss: ss,
                 verType: Versions.VerTypes.Latest,
                 methodType: BaseModel.MethodTypes.Index,
                 referenceType: "Groups",
+                script: Libraries.Scripts.JavaScripts.ViewMode(viewMode),
                 title: Displays.Groups() + " - " + Displays.List(),
                 action: () =>
                 {
