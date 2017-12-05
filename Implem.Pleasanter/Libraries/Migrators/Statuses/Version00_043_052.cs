f4451401288619b1e73e97395a62b2c325ed5415
Taiji Uchida <uchi825@gmail.com>
2017-12-05 23:23:51 +0900

    不要なリンクデータを削除するためのマイグレーションを追加。

---
+++ Implem.Pleasanter/Libraries/Migrators/Statuses/Version00_043_052.cs
@@ -0,0 +1,18 @@
+﻿using Implem.Pleasanter.Libraries.DataSources;
+namespace Implem.Pleasanter.Libraries.Migrators.Statuses
+{
+    public static class Version00_043_052
+    {
+        public static void Migrate()
+        {
+            var sub = Rds.SelectItems(
+                column: Rds.ItemsColumn().ReferenceId());
+            Rds.ExecuteNonQuery(statements:
+                Rds.DeleteLinks(
+                    where: Rds.LinksWhere()
+                        .Or(or: Rds.LinksWhere()
+                            .DestinationId_In(sub: sub, negative: true)
+                            .SourceId_In(sub: sub, negative: true))));
+        }
+    }
+}
