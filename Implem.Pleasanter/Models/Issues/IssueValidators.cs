e1f65e48b0c7b50775fa17e8c014179ce984e4d4
Taiji Uchida <uchi825@gmail.com>
2018-01-10 14:47:16 +0900

    Bug fixed: 計算式の代入項目が読取り専用の場合に更新が行えない問題を解消。

--- Implem.Pleasanter/Models/Issues/IssueValidators.cs
+++ Implem.Pleasanter/Models/Issues/IssueValidators.cs
@@ -49,7 +49,9 @@ namespace Implem.Pleasanter.Models
                 return Error.Types.HasNotPermission;
             }
             ss.SetColumnAccessControls(issueModel.Mine());
-            foreach (var column in ss.Columns.Where(o => !o.CanCreate))
+            foreach (var column in ss.Columns
+                .Where(o => !o.CanCreate)
+                .Where(o => !ss.FormulaTarget(o.ColumnName)))
             {
                 switch (column.ColumnName)
                 {
@@ -563,7 +565,9 @@ namespace Implem.Pleasanter.Models
                 return Error.Types.HasNotPermission;
             }
             ss.SetColumnAccessControls(issueModel.Mine());
-            foreach (var column in ss.Columns.Where(o => !o.CanUpdate))
+            foreach (var column in ss.Columns
+                .Where(o => !o.CanUpdate)
+                .Where(o => !ss.FormulaTarget(o.ColumnName)))
             {
                 switch (column.ColumnName)
                 {
