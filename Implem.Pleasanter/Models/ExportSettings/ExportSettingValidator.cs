b366018d1d4c57decab447ea8295c2d5700ec5dc
Taiji Uchida <uchi825@gmail.com>
2018-01-25 12:48:40 +0900

    Bug fixed: プロジェクトに登録するファイルの訂正。

--- Implem.Pleasanter/Models/ExportSettings/ExportSettingValidator.cs
+++
@@ -1,9 +0,0 @@
-﻿using Implem.Pleasanter.Libraries.General;
-using Implem.Pleasanter.Libraries.Security;
-using Implem.Pleasanter.Libraries.Settings;
-namespace Implem.Pleasanter.Models
-{
-    public static class ExportSettingValidator
-    {
-    }
-}
