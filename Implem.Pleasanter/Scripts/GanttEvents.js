36aebdfed0636fd2a5f9656bd8674ebb7ec50a0f
Taiji Uchida <uchi825@gmail.com>
2018-04-18 16:23:06 +0900

    Bug fixed: イベントリスナーの設定タイミングをロード完了時に修正。

--- Implem.Pleasanter/Scripts/GanttEvents.js
+++ Implem.Pleasanter/Scripts/GanttEvents.js
@@ -1,5 +1,7 @@
-﻿$(document).on('click', '#Gantt .planned rect,#Gantt .earned rect,#Gantt .title text', function () {
-    if ($(this).filter('.summary').length === 0) {
-        location.href = $('#BaseUrl').val() + $(this).attr('data-id');
-    }
+﻿$(function () {
+    $(document).on('click', '#Gantt .planned rect,#Gantt .earned rect,#Gantt .title text', function () {
+        if ($(this).filter('.summary').length === 0) {
+            location.href = $('#BaseUrl').val() + $(this).attr('data-id');
+        }
+    });
 });
