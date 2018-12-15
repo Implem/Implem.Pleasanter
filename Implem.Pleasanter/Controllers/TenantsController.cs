58284b65411124e3afdd7ce1a6f2752184d3abe5
Taiji Uchida <support@pleasanter.org>
2018-12-15 20:44:23 +0900

    未使用のメソッドを削除。

--- Implem.Pleasanter/Controllers/TenantsController.cs
+++ Implem.Pleasanter/Controllers/TenantsController.cs
@@ -1,7 +1,4 @@
 ﻿using Implem.Pleasanter.Libraries.Requests;
-using Implem.Pleasanter.Libraries.Responses;
-using Implem.Pleasanter.Libraries.Security;
-using Implem.Pleasanter.Libraries.Server;
 using Implem.Pleasanter.Libraries.Settings;
 using Implem.Pleasanter.Models;
 using System.Web.Mvc;
@@ -11,29 +8,6 @@ namespace Implem.Pleasanter.Controllers
     [ValidateInput(false)]
     public class TenantsController : Controller
     {
-        /// <summary>
-        /// Fixed:
-        /// </summary>
-        [AcceptVerbs(HttpVerbs.Get | HttpVerbs.Post)]
-        public ActionResult Index()
-        {
-            var context = new Context();
-            return Redirect(Locations.Edit(context, "Tenants", context.TenantId));
-        }
-
-        [HttpGet]
-        public ActionResult New(long id = 0)
-        {
-            var context = new Context();
-            var log = new SysLogModel(context: context);
-            var html = TenantUtilities.EditorNew(
-                context: context,
-                ss: SiteSettingsUtilities.TenantsSiteSettings(context: context));
-            ViewBag.HtmlBody = html;
-            log.Finish(context: context, responseSize: html.Length);
-            return View();
-        }
-
         [AcceptVerbs(HttpVerbs.Get | HttpVerbs.Post)]
         public ActionResult Edit(int id)
         {
@@ -62,107 +36,12 @@ namespace Implem.Pleasanter.Controllers
             }
         }
 
-        [HttpGet]
-        public ActionResult Export(long id)
-        {
-            var context = new Context();
-            var log = new SysLogModel(context: context);
-            var responseFile = new ItemModel(
-                context: context,
-                referenceId: id)
-                    .Export(context: context);
-            if (responseFile != null)
-            {
-                log.Finish(context: context, responseSize: responseFile.Length);
-                return responseFile.ToFile();
-            }
-            else
-            {
-                log.Finish(context: context, responseSize: 0);
-                return null;
-            }
-        }
-
-        [HttpPost]
-        public string GridRows()
-        {
-            var context = new Context();
-            var log = new SysLogModel(context: context);
-            var json = TenantUtilities.GridRows(context: context);
-            log.Finish(context: context, responseSize: json.Length);
-            return json;
-        }
-
-        [HttpPost]
-        public string Create()
-        {
-            var context = new Context();
-            var log = new SysLogModel(context: context);
-            var json = TenantUtilities.Create(
-                context: context,
-                ss: SiteSettingsUtilities.TenantsSiteSettings(context: context));
-            log.Finish(context: context, responseSize: json.Length);
-            return json;
-        }
-
         [HttpPut]
         public string Update(int id)
         {
             var context = new Context();
             var log = new SysLogModel(context: context);
             var json = TenantUtilities.Update(
-                context: context,
-                ss: SiteSettingsUtilities.TenantsSiteSettings(context: context),
-                tenantId: id);
-            log.Finish(context: context, responseSize: json.Length);
-            return json;
-        }
-
-        [HttpDelete]
-        public string Delete(int id)
-        {
-            var context = new Context();
-            var log = new SysLogModel(context: context);
-            var json = TenantUtilities.Delete(
-                context: context,
-                ss: SiteSettingsUtilities.TenantsSiteSettings(context: context),
-                tenantId: id);
-            log.Finish(context: context, responseSize: json.Length);
-            return json;
-        }
-
-        [HttpDelete]
-        public string DeleteComment(int id)
-        {
-            var context = new Context();
-            var log = new SysLogModel(context: context);
-            var json = TenantUtilities.Update(
-                context: context,
-                ss: SiteSettingsUtilities.TenantsSiteSettings(context: context),
-                tenantId: id);
-            log.Finish(context: context, responseSize: json.Length);
-            return json;
-        }
-
-        [HttpPost]
-        public string Histories(int id)
-        {
-            var context = new Context();
-            var log = new SysLogModel(context: context);
-            var json = TenantUtilities.Histories(
-                context: context,
-                ss: SiteSettingsUtilities.TenantsSiteSettings(context: context),
-                tenantId: id);
-            log.Finish(context: context, responseSize: json.Length);
-            return json;
-        }
-
-        [HttpPost]
-        public string History(int id)
-        {
-            var context = new Context();
-            var log = new SysLogModel(context: context);
-            var json = TenantUtilities.History(
                 context: context,
                 ss: SiteSettingsUtilities.TenantsSiteSettings(context: context),
                 tenantId: id);
