ec6ce3392095f76ef63144f99b596f69cad31e2e
Taiji Uchida <support@pleasanter.org>
2018-12-18 10:41:08 +0900

    SysLogsビューワの期間指定方法に日付による期間を追加。

--- Implem.SupportTools/SysLogViewer/Model/PleasanterDbClient.cs
+++ Implem.SupportTools/SysLogViewer/Model/PleasanterDbClient.cs
@@ -16,11 +16,33 @@ namespace Implem.SupportTools.SysLogViewer.Model
             
         }
 
+        public async Task<IEnumerable<SysLogModel>> GetSysLogsAsync(int count, DateTime? startDate, DateTime? endDate)
+        {
+            var where = "";
+            if (startDate != null)
+            {
+                where += $" '{startDate?.ToString("yyyy-MM-dd HH:mm:ss.fff")}' <= [CreatedTime]";
+            }
+            var endDate2 = endDate + TimeSpan.FromDays(1);
+            var and = (where == string.Empty) ? "" : " AND";
+            if(endDate2 != null)
+            {
+                where += $"{and} [CreatedTime] < '{endDate2?.ToString("yyyy-MM-dd HH:mm:ss.fff")}'";
+            }
+            if(where != string.Empty)
+            {
+                where = "WHERE" + where;
+            }
+            return await connection.QueryAsync<SysLogModel>(
+                $"SELECT * FROM [{dbName}].[dbo].[SysLogs] {where} ORDER BY [CreatedTime] DESC");
+        }
+
         public async Task<IEnumerable<SysLogModel>> GetSysLogsAsync(int count)
         {
             return await connection.QueryAsync<SysLogModel>(
                 $"SELECT Top {count} * FROM [{dbName}].[dbo].[SysLogs] ORDER BY [CreatedTime] DESC");
         }
+        
 
         public async Task<IEnumerable<SysLogModel>> GetSysLogsAsync(DateTime lastCreatedTime)
         {
