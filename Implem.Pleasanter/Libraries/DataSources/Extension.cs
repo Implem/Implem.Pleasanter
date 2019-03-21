8a9e6f810670eae3fd0f21b5e0c3128167e32632
Taiji Uchida <support@pleasanter.org>
2019-03-21 19:43:43 +0900

    Bug fixed: メソッドのパラメータを修正。

--- Implem.Pleasanter/Libraries/DataSources/Extension.cs
+++ Implem.Pleasanter/Libraries/DataSources/Extension.cs
@@ -5,7 +5,11 @@ namespace Implem.Pleasanter.Libraries.DataSources
 {
     public static class Extension
     {
-        public static User Authenticate(Context context, string loginId, string password)
+        public static bool Authenticate(
+            Context context,
+            string loginId,
+            string password,
+            UserModel userModel)
         {
             throw new NotImplementedException();
         }
