a9bd824bd1a6147975e64c2e82e3121990d82f91
Taiji Uchida <support@pleasanter.org>
2019-04-01 19:33:44 +0900

    Copyrightの修正。

--- Implem.SupportTools/MailTester/Properties/AssemblyInfo.cs
+++ Implem.SupportTools/MailTester/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Implem.SupportTools.MailTester")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright © Implem Inc 2014 - 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
