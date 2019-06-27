3b631cd45377e82a9a6e71b7490cfb6b06f5f5b1
HAYATO <support@pleasanter.org>
2019-06-27 18:04:32 +0900

    Bug fixed: 作成ボタンの二重動作を抑止。

--- Implem.Pleasanter/Scripts/_Form.js
+++ Implem.Pleasanter/Scripts/_Form.js
@@ -48,6 +48,10 @@ $p.syncSend = function ($control, formId) {
 
 $p.send = function ($control, formId, async) {
     if ($p.outsideDialog($control)) return false;
+    if ($control.hasClass('no-send')) return false;
+    if ($control.hasClass('send-once')) {
+        $control.addClass('no-send');
+    }
     $form = formId !== undefined
         ? $('#' + formId)
         : $control.closest('form');
