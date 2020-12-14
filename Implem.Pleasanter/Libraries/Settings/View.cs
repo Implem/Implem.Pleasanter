d119ef10bc94f3d4e7f1847cd797e67b84ff43d5
uchida <uchida@implem.co.jp>
2020-12-14 11:04:47 +0900

    Bug fixed:
    一覧画面で空白のみで検索を行うとアプリケーションエラーとなる事象を修正。

--- Implem.Pleasanter/Libraries/Settings/View.cs
+++ Implem.Pleasanter/Libraries/Settings/View.cs
@@ -561,7 +561,7 @@ namespace Implem.Pleasanter.Libraries.Settings
 
         private string String(Context context, string controlId)
         {
-            var data = context.Forms.Data(controlId);
+            var data = context.Forms.Data(controlId).Trim();
             if (data != string.Empty)
             {
                 return data;
