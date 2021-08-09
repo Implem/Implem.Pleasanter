361cabb96dc0ce6fb179689236fe1d769a05709b
Taiji Uchida <63937957+uchi-ta@users.noreply.github.com>
2021-08-09 10:41:30 +0900

    [assembly: AssemblyVersion("1.2.1.0")]

--- Implem.SupportTools/Common/Properties/AssemblyInfo.cs
+++ Implem.SupportTools/Common/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Imlpem.SupportTools.Common")]
-[assembly: AssemblyCopyright("Copyright © Implem Inc 2014 - 2019")]
+[assembly: AssemblyCopyright("Copyright © Implem Inc 2014 - 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
--- Implem.SupportTools/Launcher/Properties/AssemblyInfo.cs
+++ Implem.SupportTools/Launcher/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Implem.SupportTools")]
-[assembly: AssemblyCopyright("Copyright © Implem Inc 2014 - 2019")]
+[assembly: AssemblyCopyright("Copyright © Implem Inc 2014 - 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
