248a509eec066848aa9d739e4c9fb368a8b4ed8d
Taiji Uchida <63937957+uchi-ta@users.noreply.github.com>
2021-08-21 11:10:11 +0900

    X-Forwarded-ForからUserHostAddressを取得する機能を追加。

--- Implem.Pleasanter.NetCore/Libraries/Requests/Context.cs
+++ Implem.Pleasanter.NetCore/Libraries/Requests/Context.cs
@@ -136,7 +136,7 @@ namespace Implem.Pleasanter.NetCore.Libraries.Requests
             UserId = userId;
             Language = language ?? Language;
             UserHostAddress = HasRoute
-                ? GetUserHostAddress(AspNetCoreHttpContext.Current?.Connection)
+                ? GetUserHostAddress()
                 : null;
             SetTenantProperties();
             SetPublish();
@@ -194,7 +194,7 @@ namespace Implem.Pleasanter.NetCore.Libraries.Requests
                 Action = RouteData.Get("action")?.ToLower() ?? string.Empty;
                 Id = RouteData.Get("id")?.ToLong() ?? 0;
                 Guid = RouteData.Get("guid")?.ToUpper();
-                UserHostName = GetUserHostAddress(request?.HttpContext?.Connection);
+                UserHostName = GetUserHostAddress();
                 UserHostAddress = CreateUserHostAddress(AspNetCoreHttpContext.Current.Request);
                 UserAgent = CreateUserAgent(AspNetCoreHttpContext.Current.Request);
             }
@@ -326,7 +326,7 @@ namespace Implem.Pleasanter.NetCore.Libraries.Requests
                 User = SiteInfo.User(context: this, userId: UserId);
                 Language = userModel.Language;
                 Theme = Strings.CoalesceEmpty(userModel.Theme, Parameters.User.Theme, "sunny");
-                UserHostAddress = GetUserHostAddress(AspNetCoreHttpContext.Current?.Connection);
+                UserHostAddress = GetUserHostAddress();
                 Developer = userModel.Developer;
                 TimeZoneInfo = userModel.TimeZoneInfo;
                 UserSettings = userModel.UserSettings;
@@ -783,9 +783,24 @@ namespace Implem.Pleasanter.NetCore.Libraries.Requests
             return AspNetCoreHttpContext.Current.User.Identity?.GetType().Name.Contains("Windows") ?? false;
         }
 
-        private string GetUserHostAddress(ConnectionInfo request)
+        private string GetUserHostAddress()
         {
-            return request?.RemoteIpAddress?.ToString();
+            var xFoForwardedFor = new Microsoft.Extensions.Primitives.StringValues();
+            var address = AspNetCoreHttpContext.Current?.Request?.Headers?.TryGetValue("X-Forwarded-For", out xFoForwardedFor) == true
+                ? xFoForwardedFor.FirstOrDefault()
+                : null;
+            if (address == null)
+            {
+                return AspNetCoreHttpContext.Current?.Connection?.RemoteIpAddress?.ToString();
+            }
+            var sn = address.IndexOf("[");
+            var en = address.IndexOf("]");
+            if (sn >= 0 && en > sn)
+            {
+                return address.Substring(sn + 1, en - 1);
+            }
+            var n = address.IndexOf(":");
+            return (n > 0) ? address.Substring(0, n) : address;
         }
 
         private static readonly Lazy<ISqlObjectFactory> _sqlObjectFactory = new Lazy<ISqlObjectFactory>(() =>
