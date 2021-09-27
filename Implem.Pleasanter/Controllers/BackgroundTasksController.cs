ed0e3778587b74501c9ae044765794a083dd9f20
Taiji Uchida <63937957+uchi-ta@users.noreply.github.com>
2021-09-28 08:18:17 +0900

    特定テーブルの検索インデックスをバッチ処理で再構築する機能を追加。

--- Implem.Pleasanter/Controllers/BackgroundTasksController.cs
+++ Implem.Pleasanter/Controllers/BackgroundTasksController.cs
@@ -1,8 +1,8 @@
 ﻿using Implem.DefinitionAccessor;
 using Implem.Pleasanter.Libraries.Requests;
+using Implem.Pleasanter.Libraries.Search;
 using Implem.Pleasanter.Models;
 using Implem.Pleasanter.Tools;
-
 namespace Implem.Pleasanter.Controllers
 {
     public class BackgroundTasksController
@@ -27,6 +27,26 @@ namespace Implem.Pleasanter.Controllers
             {
                 return null;
             }
+        }
+
+        public string RebuildSearchIndexes(Context context)
+        {
+            if (Parameters.BackgroundTask.Enabled)
+            {
+                if (context.QueryStrings.Bool("NoLog"))
+                {
+                    Indexes.RebuildSearchIndexes(context: context);
+                }
+                else
+                {
+                    var log = new SysLogModel(context: context);
+                    Indexes.RebuildSearchIndexes(
+                        context: context,
+                        siteId: context.QueryStrings.Long("SiteId"));
+                    log.Finish(context: context, responseSize: 0);
+                }
+            }
+            return null;
         }
     }
 }
