be45ae075c6b77657e17ea687826236c388c20f4
Taiji Uchida <63937957+uchi-ta@users.noreply.github.com>
2021-12-05 20:44:56 +0900

    コードの整形。

--- Implem.Pleasanter/Libraries/HtmlParts/HtmlViewExtensions.cs
+++ Implem.Pleasanter/Libraries/HtmlParts/HtmlViewExtensions.cs
@@ -6,24 +6,33 @@ namespace Implem.Pleasanter.Libraries.HtmlParts
 {
     public static class HtmlViewExtensions
     {
-        public static HtmlBuilder ViewExtensions(this HtmlBuilder hb, Context context, SiteSettings ss, View view)
+        public static HtmlBuilder ViewExtensions(
+            this HtmlBuilder hb,
+            Context context,
+            SiteSettings ss,
+            View view)
         {
             var columns = context.ExtendedFieldColumns(
                 ss: ss,
                 extendedFieldType: "ViewExtensions");
             return columns.Count > 0
                 && ss.ReferenceType != "Sites"
-                ? hb.Div(
-                    id: "ViewExtensions",
-                    action: () => hb.ViewExtensionColumns(
-                        context: context,
-                        ss: ss,
-                        view: view,
-                        columns: columns))
-                : hb;
+                    ? hb.Div(
+                        id: "ViewExtensions",
+                        action: () => hb.ViewExtensionColumns(
+                            context: context,
+                            ss: ss,
+                            view: view,
+                            columns: columns))
+                    : hb;
         }
 
-        private static HtmlBuilder ViewExtensionColumns(this HtmlBuilder hb, Context context, SiteSettings ss, View view, IList<Column> columns)
+        private static HtmlBuilder ViewExtensionColumns(
+            this HtmlBuilder hb,
+            Context context,
+            SiteSettings ss,
+            View view,
+            IList<Column> columns)
         {
             foreach (var column in columns)
             {
