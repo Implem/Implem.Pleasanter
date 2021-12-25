c8e291c54cd52dff984377357cced41b8f9edc9a
Taiji Uchida <uchida@implem.co.jp>
2021-12-25 18:00:11 +0900

    レコードのアクセス制御の選択肢に0が表示される問題を解消。

--- Implem.Pleasanter/Libraries/DataTypes/User.cs
+++ Implem.Pleasanter/Libraries/DataTypes/User.cs
@@ -191,10 +191,14 @@ namespace Implem.Pleasanter.Libraries.DataTypes
                         value = value.Replace(match.Value, Displays.Users(context: context));
                         break;
                     case "[UserId]":
-                        value = value.Replace(match.Value, Id.ToString());
+                        value = value.Replace(match.Value, Id > 0
+                            ? Id.ToString()
+                            : string.Empty);
                         break;
                     case "[DeptId]":
-                        value = value.Replace(match.Value, DeptId.ToString());
+                        value = value.Replace(match.Value, DeptId > 0
+                            ? DeptId.ToString()
+                            : string.Empty);
                         break;
                     case "[Dept]":
                     case "[DeptName]":
