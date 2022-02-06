3e2967c9290f034c62aa981790d03bc7589ba1a4
Taiji Uchida <uchida@implem.co.jp>
2022-02-07 07:58:05 +0900

    Bug fixed:
    一覧画面のソートメニューが正しい位置に表示されない問題を解消。

--- Implem.Pleasanter/Scripts/GridEvents.js
+++ Implem.Pleasanter/Scripts/GridEvents.js
@@ -101,9 +101,9 @@ $(function () {
             $menuSort = $(".menu-sort[id='GridHeaderMenu__" + dataName + "']");
             $menuSort.css('width', '');
             $menuSort
-                .css('position', 'absolute')
-                .css('top', $control.position().top + $control.outerHeight())
-                .css('left', $control.position().left)
+                .css('position', 'fixed')
+                .css('top', $control.offset().top + $control.outerHeight())
+                .css('left', $control.offset().left)
                 .outerWidth($control.outerWidth() > $menuSort.outerWidth()
                     ? $control.outerWidth()
                     : $menuSort.outerWidth())
