5a4ad990adc748dd5c2bfe9f82cc2cce3cb885d5
野田 幸士朗 <noda@implem.co.jp>
2022-02-09 15:16:38 +0900

    サイトパッケージのエクスポートとインポートを実行するAPIを追加。

---
+++ Implem.Pleasanter/Models/Sites/SitePackageApiModel.cs
@@ -0,0 +1,20 @@
+﻿using Implem.Pleasanter.Libraries.SitePackages;
+using Implem.Pleasanter.Models.Shared;
+using System;
+using System.Collections.Generic;
+
+namespace Implem.Pleasanter.Models.Sites
+{
+    [Serializable]
+    public class SitePackageApiModel : _BaseApiModel
+    {
+        public long? TargetSiteId { get; set; }
+        public string SiteTitle { get; set; } = null;
+        public List<SelectedSite> SelectedSites { get; set; } = new List<SelectedSite>();
+        public bool IncludeSitePermission { get; set; } = false;
+        public bool IncludeRecordPermission { get; set; } = false;
+        public bool IncludeColumnPermission { get; set; } = false;
+        public bool IncludeNotifications { get; set; } = false;
+        public bool IncludeReminders { get; set; } = false;
+    }
+}
