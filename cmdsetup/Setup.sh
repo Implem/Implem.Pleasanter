6627f5ba39a15b89042cf206e36fd4e56756eb76
Taiji Uchida <uchida@implem.co.jp>
2022-02-27 15:03:20 +0900

    使用していないファイルを削除。

--- cmdsetup/Setup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-mkdir -p /var/www/pleasanter/
-cp -r Pleasanter.NetCore/* /var/www/pleasanter/
-dotnet CodeDefiner.NetCore/Implem.CodeDefiner.NetCore.dll _rds /p \\var\\www\\pleasanter /s /r
