95107490c7ac596a9ebf6fa2b825ac74b25299ac
Taiji Uchida <uchida@implem.co.jp>
2022-05-15 11:54:37 +0900

    Linux環境でディレクトリの区切り文字を正しく識別できない問題を解消。

--- Implem.Libraries/Classes/XlsIo.cs
+++ Implem.Libraries/Classes/XlsIo.cs
@@ -49,7 +49,8 @@ namespace Implem.Libraries.Classes
                         var data = Files.Read(file.FullName);
                         hash[file.Name.Replace("_Body.txt", ".json")]["Body"] = data;
                     }
-                    else if (file.FullName.Contains("\\Definition_Demo\\") && file.Name.EndsWith("_Body.json"))
+                    else if (file.FullName.Contains($"{System.IO.Path.DirectorySeparatorChar}Definition_Demo{System.IO.Path.DirectorySeparatorChar}")
+                        && file.Name.EndsWith("_Body.json"))
                     {
                         var data = Files.Read(file.FullName);
                         hash[file.Name.Replace("_Body.json", ".json")]["Body"] = data;
