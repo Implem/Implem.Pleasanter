871f2d9a141efc20ea91db3ae670e6f038c36eaa
Taiji Uchida <uchida@implem.co.jp>
2022-05-28 12:59:20 +0900

    ColumnFilterHashに所属グループによってフィルタする機能を追加。

--- Implem.Libraries/DataSources/SqlServer/SqlExists.cs
+++ Implem.Libraries/DataSources/SqlServer/SqlExists.cs
@@ -1,10 +1,14 @@
 ﻿using Implem.IRds;
 using Implem.Libraries.Utilities;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 namespace Implem.Libraries.DataSources.SqlServer
 {
     public class SqlExists : SqlStatement
     {
+        public List<SqlStatement> SqlStatements { get; set; }
+
         public SqlExists()
         {
         }
@@ -18,26 +22,43 @@ namespace Implem.Libraries.DataSources.SqlServer
         {
             if (!Using) return;
             if (Not) commandText.Append("not ");
-            commandText.Append("exists(select * from ", TableBracket, " ");
-            SqlJoinCollection?.BuildCommandText(commandText);
-            SqlWhereCollection?.BuildCommandText(
-                factory: factory,
-                sqlContainer: sqlContainer,
-                sqlCommand: sqlCommand,
-                commandText: commandText,
-                commandCount: commandCount,
-                select: true);
-            commandText.Append(")");
-            AddTermination(commandText);
-            AddParams_Where(factory: factory, sqlCommand: sqlCommand, commandCount: commandCount);
-            switch (TableType)
+            if (SqlStatements?.Any() == true)
             {
-                case Sqls.TableTypes.History:
-                    commandText = commandText.Replace(TableBracket, HistoryTableBracket);
-                    break;
-                case Sqls.TableTypes.Deleted:
-                    commandText = commandText.Replace(TableBracket, DeletedTableBracket);
-                    break;
+                commandText.Append("exists(");
+                SqlStatements.ForEach(statement =>
+                    commandText.Append(statement.GetCommandText(
+                        factory: factory,
+                        sqlContainer: sqlContainer,
+                        sqlCommand: sqlCommand,
+                        commandCount: commandCount)));
+                commandText.Append(")");
+            }
+            else
+            {
+                commandText.Append("exists(select * from ", TableBracket, " ");
+                SqlJoinCollection?.BuildCommandText(commandText);
+                SqlWhereCollection?.BuildCommandText(
+                    factory: factory,
+                    sqlContainer: sqlContainer,
+                    sqlCommand: sqlCommand,
+                    commandText: commandText,
+                    commandCount: commandCount,
+                    select: true);
+                commandText.Append(")");
+                AddTermination(commandText);
+                AddParams_Where(
+                    factory: factory,
+                    sqlCommand: sqlCommand,
+                    commandCount: commandCount);
+                switch (TableType)
+                {
+                    case Sqls.TableTypes.History:
+                        commandText = commandText.Replace(TableBracket, HistoryTableBracket);
+                        break;
+                    case Sqls.TableTypes.Deleted:
+                        commandText = commandText.Replace(TableBracket, DeletedTableBracket);
+                        break;
+                }
             }
         }
     }
