9870600ad5968e92d4df9a480e64079cb4efd6a2
野田 幸士朗 <noda@implem.co.jp>
2022-06-09 11:23:35 +0900

    ユーザ情報を表示する複数選択可能な分類項目について、画面上で選択した値が正常に表示されない問題の解消。

--- Implem.Pleasanter/Libraries/HtmlParts/HtmlFields.cs
+++ Implem.Pleasanter/Libraries/HtmlParts/HtmlFields.cs
@@ -55,6 +55,7 @@ namespace Implem.Pleasanter.Libraries.HtmlParts
             bool _using = true)
         {
             if (column.Type == Column.Types.User
+                && (!column.MultipleSelections ?? false)
                 && SiteInfo.User(
                     context: context,
                     userId: value.ToInt()).Anonymous())
