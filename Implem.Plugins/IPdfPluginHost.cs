94ade21cc6453c5abf173cd7bd1385f51ada6e85
Hirotada Kobayashi <pierre3@outlook.com>
2022-07-15 17:06:33 +0900

    PdfPluginHostからdefaultViewを取得できるように修正

--- Implem.Plugins/IPdfPluginHost.cs
+++ Implem.Plugins/IPdfPluginHost.cs
@@ -4,6 +4,8 @@
     {
         string SiteTitle { get; }
         int ReportId { get; }
+        string DefaultView { get; }
+
         List<Dictionary<string, object>> GetGridData(string viewJson);
     }
 }
