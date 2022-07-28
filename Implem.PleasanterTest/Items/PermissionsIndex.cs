16057a65f89b2a2afd84d221d2bd67804970e871
Taiji Uchida <uchida@implem.co.jp>
2022-07-28 15:44:01 +0900

    テストコードにコメントを追加。

--- Implem.PleasanterTest/Items/PermissionsIndex.cs
+++ Implem.PleasanterTest/Items/PermissionsIndex.cs
@@ -86,63 +86,92 @@ namespace Implem.PleasanterTest.Items
             PermissionData.PermissionIdTypes permissionIdType,
             PermissionData.PermissionInitTypes permissionInitType)
         {
+            // テーブルの種類でスイッチ
             switch (referenceType)
             {
+                // Wikiのレコードのindexは存在しないためNotFound
                 case ItemData.ReferenceTypes.Wikis:
                     return HtmlTestNotFoundMessage(title: title);
                 default:
+                    // 権限の種類でスイッチ
                     switch (permissionInitType)
                     {
+                        // アクセス権無しの場合にはNotFound
+                        // レコードのアクセス権のみの場合にはサイトにアクセスできないためNotFound
+                        // 特権ユーザはアクセス権に関係なくアクセスできるためExists
                         case PermissionData.PermissionInitTypes.Nothing:
                         case PermissionData.PermissionInitTypes.RecordAllow:
                             return userType == UserData.UserTypes.Privileged
                                 ? HtmlTestExistsOne(title: title)
                                 : HtmlTestNotFoundMessage(title: title);
+                        // サイトへのエントリー権限(PermissionType:0)が存在する場合
                         case PermissionData.PermissionInitTypes.SiteEntry:
+                            // ユーザの種類でスイッチ
                             switch (userType)
                             {
+                                // 無効化された組織のユーザの場合
                                 case UserData.UserTypes.DisabledDept:
+                                    // アクセス付与対象でスイッチ
                                     switch (permissionIdType)
                                     {
+                                        // 無効化された組織またはそれを含むグループではアクセスできないためNotFound
                                         case PermissionData.PermissionIdTypes.Dept:
                                         case PermissionData.PermissionIdTypes.DeptGroup:
                                             return HtmlTestNotFoundMessage(title: title);
+                                        // ユーザのアクセス権またはグループのアクセス権では一覧は表示できるが対象の行が表示されないためEntryAndNoRow
                                         default:
                                             return HtmlTestEntryAndNoRow(title: title);
                                     }
+                                // 無効化されたグループのユーザの場合アクセス付与対象がグループの場合はNotFound
+                                // 上記グループ以外の場合、一覧は表示できるが対象の行が表示されないためEntryAndNoRow
                                 case UserData.UserTypes.DisabledGroup:
                                     return permissionIdType == PermissionData.PermissionIdTypes.Group
                                         ? HtmlTestNotFoundMessage(title: title)
                                         : HtmlTestEntryAndNoRow(title: title);
+                                // 無効化またはロックされたユーザの場合はアクセスできないためNotFound
                                 case UserData.UserTypes.Disabled:
                                 case UserData.UserTypes.Lockout:
                                     return HtmlTestNotFoundMessage(title: title);
+                                // 特権ユーザはアクセス権に関係なくアクセスできるためExists
                                 case UserData.UserTypes.Privileged:
                                     return HtmlTestExistsOne(title: title);
+                                // 一般ユーザでは一覧は表示できるが対象の行が表示されないためEntryAndNoRow
                                 default:
                                     return HtmlTestEntryAndNoRow(title: title);
                             }
+                        // サイトのアクセス権または継承されたサイトのアクセス権がある場合
+                        // またはサイトのエントリー権限とレコードのアクセス権がある場合
                         default:
+                            // ユーザの種類でスイッチ
                             switch (userType)
                             {
+                                // 無効化された組織のユーザの場合
                                 case UserData.UserTypes.DisabledDept:
+                                    // アクセス付与対象でスイッチ
                                     switch (permissionIdType)
                                     {
+                                        // 無効化された組織またはそれを含むグループではアクセスできないためNotFound
                                         case PermissionData.PermissionIdTypes.Dept:
                                         case PermissionData.PermissionIdTypes.DeptGroup:
                                             return HtmlTestNotFoundMessage(title: title);
+                                        // ユーザのアクセス権またはグループのアクセス権ではアクセスできるためExists
                                         default:
                                             return HtmlTestExistsOne(title: title);
                                     }
+                                // 無効化されたグループのユーザの場合アクセス付与対象がグループの場合はNotFound
+                                // 上記グループ以外の場合アクセスできるためExists
                                 case UserData.UserTypes.DisabledGroup:
                                     return permissionIdType == PermissionData.PermissionIdTypes.Group
                                         ? HtmlTestNotFoundMessage(title: title)
                                         : HtmlTestExistsOne(title: title);
+                                // 無効化またはロックされたユーザの場合はアクセスできないためNotFound
                                 case UserData.UserTypes.Disabled:
                                 case UserData.UserTypes.Lockout:
                                     return HtmlTestNotFoundMessage(title: title);
+                                // 特権ユーザはアクセス権に関係なくアクセスできるためExists
                                 case UserData.UserTypes.Privileged:
                                     return HtmlTestExistsOne(title: title);
+                                // 一般ユーザはアクセスできるためExists
                                 default:
                                     return HtmlTestExistsOne(title: title);
                             }
