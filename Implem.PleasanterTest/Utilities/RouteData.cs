f48f128069019a6052ae20aec7bec557061792b2
Taiji Uchida <uchida@implem.co.jp>
2022-07-31 13:30:04 +0900

    xUnitテストケース追加（ItemsHistories）

--- Implem.PleasanterTest/Utilities/RouteData.cs
+++ Implem.PleasanterTest/Utilities/RouteData.cs
@@ -464,6 +464,16 @@ namespace Implem.PleasanterTest.Utilities
             };
         }
 
+        public static Dictionary<string, string> ItemsHistories(long id)
+        {
+            return new Dictionary<string, string>()
+            {
+                { "controller", "items" },
+                { "action", "histories" },
+                { "id", id.ToString() },
+            };
+        }
+
         public static Dictionary<string, string> ItemsApiGet(long id)
         {
             return new Dictionary<string, string>()
