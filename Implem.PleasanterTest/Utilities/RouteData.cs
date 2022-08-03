f80fbeb184cf46ca39cbe60922edd8bb6e42760c
Taiji Uchida <uchida@implem.co.jp>
2022-08-03 21:14:05 +0900

    xUnitテストケース追加（ItemsOpenExportSelectorDialog）

--- Implem.PleasanterTest/Utilities/RouteData.cs
+++ Implem.PleasanterTest/Utilities/RouteData.cs
@@ -574,6 +574,16 @@ namespace Implem.PleasanterTest.Utilities
             };
         }
 
+        public static Dictionary<string, string> ItemsOpenExportSelectorDialog(long id)
+        {
+            return new Dictionary<string, string>()
+            {
+                { "controller", "items" },
+                { "action", "openexportselectordialog" },
+                { "id", id.ToString() },
+            };
+        }
+
         public static Dictionary<string, string> ItemsApiGet(long id)
         {
             return new Dictionary<string, string>()
