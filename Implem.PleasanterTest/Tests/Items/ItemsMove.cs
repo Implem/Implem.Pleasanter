6bcb7c2fa8914ee90d928bf9f36aff16e6e249ee
Taiji Uchida <uchida@implem.co.jp>
2022-08-09 16:34:04 +0900

    xUnitテストケース追加（ItemsMove）

---
+++ Implem.PleasanterTest/Tests/Items/ItemsMove.cs
@@ -0,0 +1,108 @@
+﻿using Implem.Libraries.Utilities;
+using Implem.Pleasanter.Libraries.Requests;
+using Implem.Pleasanter.Libraries.Settings;
+using Implem.Pleasanter.Models;
+using Implem.PleasanterTest.Models;
+using Implem.PleasanterTest.Utilities;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Implem.PleasanterTest.Tests.Items
+{
+    public class ItemsMove
+    {
+        [Theory]
+        [MemberData(nameof(GetData))]
+        public void Test(
+            string title,
+            Forms forms,
+            UserModel userModel,
+            List<JsonTest> jsonTests)
+        {
+            var id = Initializer.Titles.Get(title);
+            var context = ContextData.Get(
+                userId: userModel.UserId,
+                routeData: RouteData.ItemsMove(id: id),
+                httpMethod: "POST",
+                forms: forms);
+            var json = Results(context: context);
+            Assert.True(Compare.Json(
+                context: context,
+                json: json,
+                jsonTests: jsonTests));
+        }
+
+        public static IEnumerable<object[]> GetData()
+        {
+            var forms = FormsUtilities.Get(
+                new KeyValue("MoveTargets", Initializer.Titles.Get("WBS").ToString()));
+            var jsonTests = JsonData.Tests(
+                JsonData.ExistsOne(method: "Log"),
+                JsonData.ExistsOne(
+                    method: "Response",
+                    target: "id"),
+                JsonData.ExistsOne(
+                    method: "Invoke",
+                    target: "clearDialogs"),
+                JsonData.ExistsOne(
+                    method: "ReplaceAll",
+                    target: "#MainContainer"),
+                JsonData.ExistsOne(
+                    method: "SetValue",
+                    target: "#Id"),
+                JsonData.ExistsOne(
+                    method: "SetValue",
+                    target: "#SwitchTargets"),
+                JsonData.ExistsOne(
+                    method: "SetMemory",
+                    target: "formChanged"),
+                JsonData.ExistsOne(
+                    method: "Invoke",
+                    target: "setCurrentIndex"),
+                JsonData.ExistsOne(
+                    method: "Invoke",
+                    target: "initRelatingColumnEditor"),
+                JsonData.ExistsOne(method: "Message"),
+                JsonData.ExistsOne(method: "ClearFormData"),
+                JsonData.ExistsOne(
+                    method: "Events",
+                    target: "on_editor_load"));
+            var testParts = new List<TestPart>()
+            {
+                new TestPart(
+                    title: "RecordToMove1",
+                    forms: forms,
+                    jsonTests: jsonTests)
+            };
+            foreach (var testPart in testParts)
+            {
+                yield return TestData(
+                    title: testPart.Title,
+                    forms: testPart.Forms,
+                    userModel: testPart.UserModel,
+                    jsonTests: testPart.JsonTests);
+            }
+        }
+
+        private static object[] TestData(
+            string title,
+            Forms forms,
+            UserModel userModel,
+            List<JsonTest> jsonTests)
+        {
+            return new object[]
+            {
+                title,
+                forms,
+                userModel,
+                jsonTests
+            };
+        }
+
+        private static string Results(Context context)
+        {
+            var itemModel = Initializer.ItemIds.Get(context.Id) ?? new ItemModel();
+            return itemModel.Move(context: context);
+        }
+    }
+}
