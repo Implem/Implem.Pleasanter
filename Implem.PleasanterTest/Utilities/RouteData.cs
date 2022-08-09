1722508dd616b3a92b7b6fed2f94aaecb3ed1c1c
Taiji Uchida <uchida@implem.co.jp>
2022-08-09 21:26:21 +0900

    xUnitテストケース追加（ItemsBulkMove）

--- Implem.PleasanterTest/Utilities/RouteData.cs
+++ Implem.PleasanterTest/Utilities/RouteData.cs
@@ -794,6 +794,16 @@ namespace Implem.PleasanterTest.Utilities
             };
         }
 
+        public static Dictionary<string, string> ItemsBulkMove(long id)
+        {
+            return new Dictionary<string, string>()
+            {
+                { "controller", "items" },
+                { "action", "bulkmove" },
+                { "id", id.ToString() }
+            };
+        }
+
         public static Dictionary<string, string> ItemsApiGet(long id)
         {
             return new Dictionary<string, string>()
