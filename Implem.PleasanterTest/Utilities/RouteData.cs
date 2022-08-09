3a08a504fc4a509f0e09fb19f995dd3dda3fd28e
Taiji Uchida <uchida@implem.co.jp>
2022-08-09 21:35:03 +0900

    xUnitテストケース追加（ItemsBulkDelete）

--- Implem.PleasanterTest/Utilities/RouteData.cs
+++ Implem.PleasanterTest/Utilities/RouteData.cs
@@ -804,6 +804,16 @@ namespace Implem.PleasanterTest.Utilities
             };
         }
 
+        public static Dictionary<string, string> ItemsBulkDelete(long id)
+        {
+            return new Dictionary<string, string>()
+            {
+                { "controller", "items" },
+                { "action", "bulkdelete" },
+                { "id", id.ToString() }
+            };
+        }
+
         public static Dictionary<string, string> ItemsApiGet(long id)
         {
             return new Dictionary<string, string>()
