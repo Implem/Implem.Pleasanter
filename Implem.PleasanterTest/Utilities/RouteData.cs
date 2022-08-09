ebf0b7e4576aaee77ef1c21322a24367242067c6
Taiji Uchida <uchida@implem.co.jp>
2022-08-10 07:29:32 +0900

    xUnitテストケース追加（ItemsPhysicalDelete）

--- Implem.PleasanterTest/Utilities/RouteData.cs
+++ Implem.PleasanterTest/Utilities/RouteData.cs
@@ -824,6 +824,16 @@ namespace Implem.PleasanterTest.Utilities
             };
         }
 
+        public static Dictionary<string, string> ItemsPhysicalDelete(long id)
+        {
+            return new Dictionary<string, string>()
+            {
+                { "controller", "items" },
+                { "action", "physicaldelete" },
+                { "id", id.ToString() }
+            };
+        }
+
         public static Dictionary<string, string> ItemsApiGet(long id)
         {
             return new Dictionary<string, string>()
