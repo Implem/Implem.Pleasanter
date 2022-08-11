daad61f6656a96e240ec3f77887beff56b5e37ae
Taiji Uchida <uchida@implem.co.jp>
2022-08-11 12:58:51 +0900

    xUnitテストケース追加（ItemsExportSitePackage）

--- Implem.PleasanterTest/Utilities/RouteData.cs
+++ Implem.PleasanterTest/Utilities/RouteData.cs
@@ -914,6 +914,16 @@ namespace Implem.PleasanterTest.Utilities
             };
         }
 
+        public static Dictionary<string, string> ItemsExportSitePackage(long id)
+        {
+            return new Dictionary<string, string>()
+            {
+                { "controller", "items" },
+                { "action", "exportsitepackage" },
+                { "id", id.ToString() }
+            };
+        }
+
         public static Dictionary<string, string> ItemsApiGet(long id)
         {
             return new Dictionary<string, string>()
