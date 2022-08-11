590a0f04e6f9b5bd80eb89f86e2dea7a93cf4a51
Taiji Uchida <uchida@implem.co.jp>
2022-08-11 13:33:28 +0900

    xUnitテストケース追加（ItemsSearchPermissionElements）

---
+++ Implem.PleasanterTest/Tests/Items/ItemsSearchPermissionElements.cs
@@ -0,0 +1,87 @@
+﻿using Implem.Libraries.Utilities;
+using Implem.Pleasanter.Libraries.Requests;
+using Implem.Pleasanter.Libraries.Settings;
+using Implem.Pleasanter.Models;
+using Implem.PleasanterTest.Models;
+using Implem.PleasanterTest.Utilities;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Implem.PleasanterTest.Tests.Items
+{
+    public class ItemsSearchPermissionElements
+    {
+        [Theory]
+        [MemberData(nameof(GetData))]
+        public void Test(
+            string title,
+            Forms forms,
+            UserModel userModel,
+            List<JsonTest> jsonTests)
+        {
+            var id = Initializer.Titles.Get(title);
+            var context = ContextData.Get(
+                userId: userModel.UserId,
+                routeData: RouteData.ItemsSearchPermissionElements(id: id),
+                httpMethod: "POST",
+                forms: forms);
+            var results = Results(context: context);
+            Assert.True(Compare.Json(
+                context: context,
+                results: results,
+                jsonTests: jsonTests));
+        }
+
+        public static IEnumerable<object[]> GetData()
+        {
+            var forms = FormsUtilities.Get(
+                new KeyValue("ControlId", "SearchPermissionElements"),
+                new KeyValue("SearchPermissionElements", "高橋"));
+            var jsonTests = JsonData.Tests(
+                JsonData.ExistsOne(
+                    method: "Html",
+                    target: "#SourcePermissions"),
+                JsonData.ExistsOne(
+                    method: "SetValue",
+                    target: "#SourcePermissionsOffset"));
+            var testParts = new List<TestPart>()
+            {
+                new TestPart(
+                    title: "ネットワークの構築",
+                    forms: forms,
+                    jsonTests: jsonTests,
+                    userType: UserData.UserTypes.TenantManager1)
+            };
+            foreach (var testPart in testParts)
+            {
+                yield return TestData(
+                    title: testPart.Title,
+                    forms: testPart.Forms,
+                    userModel: testPart.UserModel,
+                    jsonTests: testPart.JsonTests);
+            }
+        }
+
+        private static object[] TestData(
+            string title,
+            Forms forms,
+            UserModel userModel,
+            List<JsonTest> jsonTests)
+        {
+            return new object[]
+            {
+                title,
+                forms,
+                userModel,
+                jsonTests
+            };
+        }
+
+        private static string Results(Context context)
+        {
+            return PermissionUtilities.SearchPermissionElements(
+                context: context,
+                referenceId: context.Id);
+        }
+    }
+}
--- Implem.PleasanterTest/Utilities/RouteData.cs
+++ Implem.PleasanterTest/Utilities/RouteData.cs
@@ -944,6 +944,16 @@ namespace Implem.PleasanterTest.Utilities
             };
         }
 
+        public static Dictionary<string, string> ItemsSearchPermissionElements(long id)
+        {
+            return new Dictionary<string, string>()
+            {
+                { "controller", "items" },
+                { "action", "searchpermissionelements" },
+                { "id", id.ToString() }
+            };
+        }
+
         public static Dictionary<string, string> ItemsApiGet(long id)
         {
             return new Dictionary<string, string>()
