0f1edd447d17bf9abbf1195b93b5518db7a21538
Taiji Uchida <uchida@implem.co.jp>
2022-08-11 21:33:54 +0900

    xUnitテストケース追加（ItemsForceUnlockTable）

--- Implem.PleasanterTest/Utilities/RouteData.cs
+++ Implem.PleasanterTest/Utilities/RouteData.cs
@@ -1054,6 +1054,16 @@ namespace Implem.PleasanterTest.Utilities
             };
         }
 
+        public static Dictionary<string, string> ItemsForceUnlockTable(long id)
+        {
+            return new Dictionary<string, string>()
+            {
+                { "controller", "items" },
+                { "action", "forceunlocktable" },
+                { "id", id.ToString() }
+            };
+        }
+
         public static Dictionary<string, string> ItemsApiGet(long id)
         {
             return new Dictionary<string, string>()
