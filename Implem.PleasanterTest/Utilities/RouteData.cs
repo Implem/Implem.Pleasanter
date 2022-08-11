a3df21def3c2beba67f85ea04c0843112f01f002
Taiji Uchida <uchida@implem.co.jp>
2022-08-11 14:06:38 +0900

    xUnitテストケース追加（ItemsOpenPermissionsDialog）

--- Implem.PleasanterTest/Utilities/RouteData.cs
+++ Implem.PleasanterTest/Utilities/RouteData.cs
@@ -964,6 +964,16 @@ namespace Implem.PleasanterTest.Utilities
             };
         }
 
+        public static Dictionary<string, string> ItemsOpenPermissionsDialog(long id)
+        {
+            return new Dictionary<string, string>()
+            {
+                { "controller", "items" },
+                { "action", "openpermissionsdialog" },
+                { "id", id.ToString() }
+            };
+        }
+
         public static Dictionary<string, string> ItemsApiGet(long id)
         {
             return new Dictionary<string, string>()
