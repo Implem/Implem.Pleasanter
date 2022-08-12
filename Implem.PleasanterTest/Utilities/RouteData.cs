82787bce1616a3f4e0db4e3b40e1063877df5716
Taiji Uchida <uchida@implem.co.jp>
2022-08-12 12:52:05 +0900

    xUnitテストケース追加（ItemsSearchColumnAccessControl）

--- Implem.PleasanterTest/Utilities/RouteData.cs
+++ Implem.PleasanterTest/Utilities/RouteData.cs
@@ -1044,6 +1044,16 @@ namespace Implem.PleasanterTest.Utilities
             };
         }
 
+        public static Dictionary<string, string> ItemsSearchColumnAccessControl(long id)
+        {
+            return new Dictionary<string, string>()
+            {
+                { "controller", "items" },
+                { "action", "searchcolumnaccesscontrol" },
+                { "id", id.ToString() }
+            };
+        }
+
         public static Dictionary<string, string> ItemsBurnDownRecordDetails(long id)
         {
             return new Dictionary<string, string>()
