a446c253b5b10acd88b780b257821ad31a99be89
Kotanigawa <kotanigawa@implem.co.jp>
2022-08-12 18:43:09 +0900

    TimerBackgroundService (WIP)

--- Implem.ParameterAccessor/Parts/BackgroundService.cs
+++ Implem.ParameterAccessor/Parts/BackgroundService.cs
@@ -1,8 +1,18 @@
-﻿namespace Implem.ParameterAccessor.Parts
+﻿using System.Collections.Generic;
+
+namespace Implem.ParameterAccessor.Parts
 {
     public class BackgroundService
     {
         public bool Reminder;
         public int ReminderIgnoreConsecutiveExceptionCount;
+        public bool SyncByLdapTimer;
+        public List<string> SyncByLdapTimerTime;
+
+        public bool TimerEnabled()
+        {
+            //TimerBackgroundServiceを使うものをここの条件に追加
+            return SyncByLdapTimer;
+        }
     }
 }
