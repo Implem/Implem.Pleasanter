d6002d3cfce1afe35e3f2a43e481a7e5f6852ce1
Taiji Uchida <uchida@implem.co.jp>
2022-08-21 14:05:20 +0900

    使用していないBinariesController/MultiUploadメソッドを削除。

--- Implem.Pleasanter/Controllers/BinariesController.cs
+++ Implem.Pleasanter/Controllers/BinariesController.cs
@@ -144,18 +144,6 @@ namespace Implem.Pleasanter.Controllers
             return json;
         }
 
-        [HttpPost]
-        public string MultiUpload(string reference, long id, ICollection<IFormFile> file)
-        {
-            var context = new Context(files: file);
-            var log = new SysLogModel(context: context);
-            var json = BinaryUtilities.MultiUpload(
-                context: context,
-                id: id);
-            log.Finish(context: context, responseSize: json.Length);
-            return json;
-        }
-
         [HttpGet]
         public ActionResult Download(string reference, string guid)
         {
