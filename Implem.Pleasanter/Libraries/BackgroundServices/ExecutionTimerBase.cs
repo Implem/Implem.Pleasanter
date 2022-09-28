2f7b01fc2b87266297fa6e8604d38ad0968849e9
Kotanigawa <kotanigawa@implem.co.jp>
2022-09-28 17:54:20 +0900

    SysLogDeleteTimer追加 引数名変更

--- Implem.Pleasanter/Libraries/BackgroundServices/ExecutionTimerBase.cs
+++ Implem.Pleasanter/Libraries/BackgroundServices/ExecutionTimerBase.cs
@@ -1,21 +1,49 @@
-﻿using System.Collections.Generic;
+﻿using Implem.Pleasanter.Libraries.Requests;
+using Implem.Pleasanter.Models;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace Implem.Pleasanter.Libraries.BackgroundServices
 {
     /// <summary>
-    /// 毎日定時に呼び出されるクラス。
-    /// ExecuteAsync()が毎日指定した時間に実行される。
-    /// メソッドGetTimerList()で実行される時間を返す。
+    /// 毎日定時に呼び出されるクラスのベースクラス。
+    /// 各種タイマー毎にこのクラスのサブクラスとして実装する。
+    /// TimerBackgroundServiceクラスによってこのクラスのメソッドExecuteAsync()が毎日指定された時間に実行される。
     /// </summary>
     abstract public class ExecutionTimerBase
     {
         virtual public async Task ExecuteAsync(CancellationToken stoppingToken) { await Task.CompletedTask; }
 
         /// <summary>
-        /// ExecuteAsync()を呼び出す時間を返す。時間を"HH:MM"形式のリストで返す。
+        /// ExecuteAsync()を呼び出す時間を"HH:MM"形式の文字列リストで返す。BackgroundService.jsonの設定を読み込んで返す。
         /// </summary>
         abstract public IList<string> GetTimeList();
+
+        /// <summary>
+        /// このタイマーのExecuteAsync()を呼び出すかを返す。BackgroundService.jsonの設定でこのタイマーが有効化されているかどうか。
+        /// </summary>
+        abstract public bool Enabled();
+
+        protected Context CreateContext()
+        {
+            return new Context(
+                request: false,
+                sessionStatus: false,
+                sessionData: false,
+                user: false,
+                item: false);
+        }
+
+        protected SysLogModel CreateSysLogModel(Context context, string message, [CallerMemberName] string callerMemberName = "")
+        {
+            return new SysLogModel(
+                context: context,
+                method: nameof(CreateSysLogModel),
+                message: $"{MethodBase.GetCurrentMethod().DeclaringType.FullName}.{callerMemberName} , {message}",
+                sysLogType: SysLogModel.SysLogTypes.Info);
+        }
     }
 }
---
+++ Implem.Pleasanter/Libraries/BackgroundServices/SysLogDeleteTimer.cs
@@ -0,0 +1,47 @@
+﻿using Implem.DefinitionAccessor;
+using Implem.Pleasanter.Libraries.DataSources;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Implem.Pleasanter.Libraries.BackgroundServices
+{
+    /// <summary>
+    /// SysLog削除を毎日定時に呼び出すクラス
+    /// </summary>
+    public class SysLogDeleteTimer : ExecutionTimerBase
+    {
+        override public async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            await Task.Run(() =>
+            {
+                var context = CreateContext();
+                var log = CreateSysLogModel(
+                    context: context,
+                    message: "delete SysLog.");
+                if (Parameters.SysLog.RetentionPeriod > 0)
+                {
+                    Repository.ExecuteNonQuery(
+                        context: context,
+                        statements: Rds.PhysicalDeleteSysLogs(
+                            where: Rds.SysLogsWhere().CreatedTime(
+                                DateTime.Now.Date.AddDays(
+                                    Parameters.SysLog.RetentionPeriod * -1),
+                                _operator: "<")));
+                }
+                log.Finish(context: context);
+            }, stoppingToken);
+        }
+
+        override public IList<string> GetTimeList()
+        {
+            return Parameters.BackgroundService.DeleteSysLogTime;
+        }
+
+        public override bool Enabled()
+        {
+            return Parameters.BackgroundService.DeleteSysLog;
+        }
+    }
+}
