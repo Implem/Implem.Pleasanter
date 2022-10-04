499910f24314b01d7773b92e2ffa65187e2d15af
Kotanigawa <kotanigawa@implem.co.jp>
2022-10-04 16:46:01 +0900

    TempファイルをBackgroundServiceで削除する機能を追加

--- Implem.ParameterAccessor/Parts/BackgroundService.cs
+++ Implem.ParameterAccessor/Parts/BackgroundService.cs
@@ -10,11 +10,13 @@ namespace Implem.ParameterAccessor.Parts
         public List<string> SyncByLdapTime;
         public bool DeleteSysLog;
         public List<string> DeleteSysLogTime;
+        public bool DeleteTemporaryFiles;
+        public List<string> DeleteTemporaryFilesTime;
 
         public bool TimerEnabled()
         {
             //TimerBackgroundServiceを使うものをここの条件に追加
-            return SyncByLdap || DeleteSysLog;
+            return SyncByLdap || DeleteSysLog || DeleteTemporaryFiles;
         }
     }
 }
