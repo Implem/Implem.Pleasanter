070c4be5eebbde9f1f994d61ed1c208a5c754526
大石 悠輔 <oishi@implem.co.jp>
2022-10-12 15:30:23 +0900

    折れ線チャートの凡例のレイアウト調整とプロット追加

--- Implem.Pleasanter/wwwroot/scripts/timeseries.js
+++ Implem.Pleasanter/wwwroot/scripts/timeseries.js
@@ -15,11 +15,14 @@
     }
     $svg.show();
     var svg = d3.select('#TimeSeries');
-    //var svgLegend = d3.select('#TimeSeries');
     var padding = 40;
     var axisPaddingX = 130;
     var axisPaddingY = 50;
     var width = parseInt(svg.style('width'));
+    //凡例用のスペースを確保
+    if (chartType === 'LineChart') {
+        width = width * 0.9;
+    } 
     var height = parseInt(svg.style('height'));
     var bodyWidth = width - axisPaddingX - (padding);
     var bodyHeight = height - axisPaddingY - (padding);
@@ -39,23 +42,7 @@
         .tickFormat(d3.timeFormat('%m/%d'))
         .ticks(10);
     var yAxis = d3.axisLeft(yScale);
-    var colorScale = d3.scaleOrdinal(d3.schemeSet1); //色のセット
-    //var color = d3.scale.category10();
-    //凡例の領域
-    /*var legend = svgLegned.selectAll('.legends')　// 凡例の領域作成
-        .data(legendVals)
-        .enter()
-        .append('g')
-        .attr('class', "legends")
-        .attr("transform", function (d, i) {
-            {
-                return "translate(0," + i * 20 + ")" // 各凡例をy方向に20px間隔で移動
-            }
-        });*/
-    
-  
-    
-
+    var colorScale = d3.scaleSequential(d3.interpolateRainbow).domain([0, 20]); 
     svg.append('g')
         .attr('class', 'axis')
         .attr('transform', 'translate(' + axisPaddingX + ', ' + (height - axisPaddingY) + ')')
@@ -72,30 +59,41 @@
         .attr('x', -20);
     indexes.forEach(function (index) {
         var ds = elements.filter(function (d) { return d.Index === index.Id; });
-        (chartType === 'LineChart') ? drawLine(ds, index.Id) : drawArea(ds);
+        if (chartType === 'LineChart') {
+            drawLine(ds, index.Id)
+        } else {
+            drawArea(ds);
+        }
     });
+	if (chartType === 'LineChart') {
+       	 indexes.sort((a, b) => (b.Text).replace(/[^\d+(\.\d+)?]/g, '') - (a.Text).replace(/[^\d+(\.\d+)?]/g, ''));
+    }
+    var lineCount = 0;
     indexes.forEach(function (index) {
         var ds = elements.filter(function (d) { return d.Index === index.Id; });
         if (ds.length !== 0) {
             var last = ds[ds.length - 1];
             var g = svg.append('g');
-            if (chartType === 'LineChart') {
+            if (chartType === 'LineChart' ) {
+                var str = indexes.filter(function (d) { return d.Id === last.Index })[0].Text;
+                var cutText = str.substr(0, str.indexOf(':'));
                 g.append('text')
                     .attr('class', 'index')
-                    .attr('transform', 'translate(' + 10 + ', ' + 20 + ')')//後で試すtranslate(index.Id * 10, height-5)
-                    .attr('x', 300)
-                    .attr('y', index.Id * 20)
+                    .attr('transform', 'translate(' + 10 + ', ' + padding + ')')
+                    .attr('x', width * 1.05)
+                    .attr('y', lineCount * 20)
                     .attr('text-anchor', 'end')
                     .attr('dominant-baseline', 'middle')
-                    .text(indexes.filter(function (d) { return d.Id === last.Index })[0].Text)
+                    .text(cutText)  
                 g.append("line")
-                    .attr('transform', 'translate(' + 10 + ', ' + 20 + ')')
-                    .attr("x1", 310)
-                    .attr("x2", 330)
-                    .attr("y1", index.Id * 20)
-                    .attr("y2", index.Id * 20)
+                    .attr('transform', 'translate(' + 10 + ', ' + padding + ')')
+                    .attr("x1", width * 1.07)
+                    .attr("x2", width * 1.06)
+                    .attr("y1", lineCount * 20)
+                    .attr("y2", lineCount * 20)
                     .attr("stroke-width", 4)
                     .attr("stroke", colorScale(index.Id));
+                lineCount++;
             } else {
                 g.append('text')
                     .attr('class', 'index')
@@ -105,18 +103,10 @@
                     .attr('text-anchor', 'end')
                     .attr('dominant-baseline', 'middle')
                     .text(indexes.filter(function (d) { return d.Id === last.Index })[0].Text);
-            }
-            
-            //g.append('rect')
-            //    .attr('transform', 'translate(' + 10 + ', ' + 20 + ')')
-            //    .attr('x', 300)
-            //    .attr('y', index.Id * 20)
-            //    .attr("width", 10)
-            //    .attr("height", 10)
-            //    .style("fill", function (d, i) { return color(i); }) // 色付け
+            }            
         }
     });
-
+       
     function drawArea(ds) {
         var area = d3.area()
             .x(function (d) {
@@ -141,24 +131,29 @@
             })
             .y(function (d) {
                 return yScale(d.Value);
-            });
+            });        
         var g = svg.append('g').attr('class', 'surface');
         g.append('path')
             .attr('d', line(ds))
             .attr('fill', 'none')
             .attr('stroke', colorScale(index))
             .attr('stroke-width', 1.5);
-        //g.selectAll('circle')
-        //    .data(ds)
-        //    .enter()
-        //    .append('circle')
-        //    .attr('cx', function (d, i) { return i * dayWidth + axisPadding + padding })
-        //    .attr('cy', function (d) { return yScale(prop(d)); })
-        //    .attr('r', 4)
-        //    .append('title')
-        //    .text(function (d) { return prop(d); });
+        g.selectAll('circle')
+            .data(ds)
+            .enter()
+            .append('circle')
+            .attr('cx', function (d) {
+                return ($p.dateDiff('d', new Date(d.Day), minDate) * dayWidth)
+                    + axisPaddingX + padding
+            })
+            .attr('cy', function (d) {
+                return yScale(d.Value);
+            })
+            .attr('r', 4)
+            .attr('stroke', colorScale(index))
+            .attr('fill', colorScale(index));            
     }
-
+    
     function color() {
         var c = Math.floor(Math.random() * 50 + 180);
         return '#' + part(c) + part(c) + part(c);
