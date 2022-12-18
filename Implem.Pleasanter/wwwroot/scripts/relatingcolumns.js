0274e18bee804287fa5f803dc00d6b07674429f3
Taiji Uchida <uchida@implem.co.jp>
2022-12-18 12:39:53 +0900

    自動ポストバック後に項目連携の値がクリアされる問題を解消。

--- Implem.Pleasanter/wwwroot/scripts/relatingcolumns.js
+++ Implem.Pleasanter/wwwroot/scripts/relatingcolumns.js
@@ -82,7 +82,7 @@
                 parentIds.push($parentElement.attr('data-value'));
             }
             var childIds = [];
-            var $child = $('#' + tablename + '_' + chld + ' option:selected');
+            var $child = $('#' + tablename + '_' + chld + ' option[selected="selected"]');
             $child.each(function (index, element) {
                 childIds.push($(element).val());
             });
