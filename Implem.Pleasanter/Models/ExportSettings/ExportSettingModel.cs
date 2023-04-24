51479439ef59a86d11d7dc164549156f8a07dd3e
浦田哲平 <urata@implem.co.jp>
2023-04-24 17:36:06 +0900

    履歴に存在するファイルは削除しないの不具合を修正

--- Implem.Pleasanter/Models/ExportSettings/ExportSettingModel.cs
+++ Implem.Pleasanter/Models/ExportSettings/ExportSettingModel.cs
@@ -294,20 +294,25 @@ namespace Implem.Pleasanter.Models
             bool otherInitValue = false,
             bool setBySession = true,
             bool get = true,
-            bool checkConflict = true) 
+            bool checkConflict = true)
         {
             if (setBySession)
             {
                 SetBySession(context: context);
             }
             var statements = new List<SqlStatement>();
+            var verUp = Versions.VerUp(
+                context: context,
+                ss: ss,
+                verUp: VerUp);
             statements.AddRange(UpdateStatements(
                 context: context,
                 ss: ss,
                 param: param,
                 otherInitValue: otherInitValue,
                 additionalStatements: additionalStatements,
-                checkConflict: checkConflict));
+                checkConflict: checkConflict,
+                verUp: verUp));
             var response = Repository.ExecuteScalar_response(
                 context: context,
                 transactional: true,
@@ -332,7 +337,8 @@ namespace Implem.Pleasanter.Models
             SqlParamCollection param = null,
             bool otherInitValue = false,
             List<SqlStatement> additionalStatements = null,
-            bool checkConflict = true)
+            bool checkConflict = true,
+            bool verUp = false)
         {
             var timestamp = Timestamp.ToDateTime();
             var statements = new List<SqlStatement>();
@@ -340,10 +346,7 @@ namespace Implem.Pleasanter.Models
                 context: context,
                 exportSettingModel: this)
                     .UpdatedTime(timestamp, _using: timestamp.InRange() && checkConflict);
-            if (Versions.VerUp(
-                context: context,
-                ss: ss,
-                verUp: VerUp))
+            if (verUp)
             {
                 statements.Add(Rds.ExportSettingsCopyToStatement(
                     where: where,
