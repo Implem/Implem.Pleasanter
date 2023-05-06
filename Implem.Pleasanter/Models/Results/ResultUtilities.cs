e985bb8a99cb8012c2dee9710558c7b7581dd4f4
Taiji Uchida <uchida@implem.co.jp>
2023-05-06 16:46:25 +0900

    ResultUtilities内でDisplays.Issues_Updatorが使用されている問題を解消。

--- Implem.Pleasanter/Models/Results/ResultUtilities.cs
+++ Implem.Pleasanter/Models/Results/ResultUtilities.cs
@@ -5420,7 +5420,7 @@ namespace Implem.Pleasanter.Models
                             context: context,
                             ss.SiteId) + "\n");
                         body.Append(
-                            $"{Displays.Issues_Updator(context: context)}: ",
+                            $"{Displays.Results_Updator(context: context)}: ",
                             $"{context.User.Name}\n");
                         if (notification.AfterBulkDelete != false)
                         {
