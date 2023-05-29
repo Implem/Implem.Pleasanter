83d6ecad8fa25e8a15404d3bd64df09be86e80a9
Hirotada Kobayashi <pierre3@outlook.com>
2023-05-29 19:51:38 +0900

    SAML認証のMailAddress連携パターンを追加

--- Implem.Pleasanter/Libraries/DataSources/Saml.cs
+++ Implem.Pleasanter/Libraries/DataSources/Saml.cs
@@ -32,12 +32,39 @@ namespace Implem.Pleasanter.Libraries.DataSources
         public static SamlAttributes MapAttributes(IEnumerable<Claim> claims, string nameId)
         {
             var attributes = new SamlAttributes();
-            var setMailAddress = false;
-            if (Parameters.Authentication?.SamlParameters?
-                    .Attributes?["MailAddress"] == "{NameId}")
+            //メールアドレスは下記フォーマットで設定可能
+            //"MailAddress": "AttributeName"　(通常の属性指定)
+            //"MailAddress" : "{NameId}" (Name ID をメールアドレスとする)
+            //"MailAddress" : "AttributeName|{NameId}" (指定した属性がない場合はName IDをメールアドレスとする)
+            var attributeMailAddress = Parameters.Authentication?.SamlParameters?
+                    .Attributes?["MailAddress"];
+            //Name ID をメールアドレスとする場合
+            if (attributeMailAddress == "{NameId}")
             {
-                setMailAddress = true;
                 attributes.Add("MailAddress", nameId);
+            }
+            //指定した属性がない場合はName IDをメールアドレスとする場合
+            else if (attributeMailAddress.EndsWith("|{NameId}"))
+            {
+                var attributeName = attributeMailAddress.Split_1st('|');
+                var claimMailAddress = claims.FirstOrDefault(claim => claim.Type == attributeName);
+                if (claimMailAddress != null)
+                {
+                    attributes.Add("MailAddress", claimMailAddress.Value);
+                }
+                else
+                {
+                    attributes.Add("MailAddress", nameId);
+                }
+            }
+            //通常の属性指定の場合
+            else
+            {
+                var claimMailAddress = claims.FirstOrDefault(claim => claim.Type == attributeMailAddress);
+                if (claimMailAddress != null)
+                {
+                    attributes.Add("MailAddress", claimMailAddress.Value);
+                }
             }
             foreach (var claim in claims)
             {
@@ -46,13 +73,12 @@ namespace Implem.Pleasanter.Libraries.DataSources
                     .SamlParameters?
                     .Attributes?
                     .FirstOrDefault(kvp => kvp.Value == claim.Type) ?? default(KeyValuePair<string, string>);
-                if (attribute.Key == null)
+                if (attribute.Key == null || attribute.Key == "MailAddress")
                 {
                     continue;
                 }
-                if ((typeof(UserModel).GetField(attribute.Key) != null
-                    || attribute.Key == "Dept" || attribute.Key == "DeptCode"
-                    || (setMailAddress == false && attribute.Key == "MailAddress")))
+                if (typeof(UserModel).GetField(attribute.Key) != null
+                    || attribute.Key == "Dept" || attribute.Key == "DeptCode")
                 {
                     attributes.Add(attribute.Key, claim.Value);
                 }
