d73ecf6abe0ceab57be1a3d820e2cc4512de8446
Taiji Uchida <63937957+uchi-ta@users.noreply.github.com>
2023-06-26 12:38:06 +0900

    レコード更新時の許可を設定しているテーブルで履歴の復元を行うとサイトのアクセス制御の設定が削除される問題を解消。

--- Implem.Pleasanter/Models/Wikis/WikiModel.cs
+++ Implem.Pleasanter/Models/Wikis/WikiModel.cs
@@ -902,29 +902,6 @@ namespace Implem.Pleasanter.Models
                 where: where,
                 param: param,
                 otherInitValue: otherInitValue));
-            if (ss.PermissionForUpdating?.Any() == true)
-            {
-                statements.AddRange(PermissionUtilities.UpdateStatements(
-                    context: context,
-                    ss: ss,
-                    referenceId: WikiId,
-                    columns: ss.Columns
-                        .Where(o => o.Type != Column.Types.Normal)
-                        .ToDictionary(
-                            o => $"{o.ColumnName},{o.Type}",
-                            o => (o.MultipleSelections == true
-                                ? PropertyValue(
-                                    context: context,
-                                    column: o)?.Deserialize<List<int>>()
-                                : PropertyValue(
-                                    context: context,
-                                    column: o)?.ToInt().ToSingleList()) ?? new List<int>()),
-                    permissions: ss.PermissionForUpdating));
-            }
-            else if (RecordPermissions != null)
-            {
-                statements.UpdatePermissions(context, ss, WikiId, RecordPermissions);
-            }
             if (additionalStatements?.Any() == true)
             {
                 statements.AddRange(additionalStatements);
