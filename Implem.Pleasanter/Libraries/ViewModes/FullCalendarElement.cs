dbc0b17915eb7748482db9b85f5b4fd9df21de3e
聡慧 史 <shiconghui@global-asp.co.jp>
2023-08-22 09:42:09 +0900

    begin,end仮出し

--- Implem.Pleasanter/Libraries/ViewModes/FullCalendarElement.cs
+++ Implem.Pleasanter/Libraries/ViewModes/FullCalendarElement.cs
@@ -6,36 +6,36 @@ using System.Collections.Generic;
 namespace Implem.Pleasanter.Libraries.ViewModes
 {
     [Serializable]
-    public class CalendarElement
+    public class FullCalendarElement
     {
-        public long Id;
-        public string Title;
-        public string Time;
-        public DateTime From;
-        public DateTime? To;
+        public long id;
+        public string title;
+        public string time;
+        public DateTime start;
+        public DateTime? end;
         public bool? Changed;
         public string StatusHtml;
         [NonSerialized]
         public DateTime UpdatedTime;
 
-        public CalendarElement(
-            long id,
-            string title,
-            string time,
+        public FullCalendarElement(
+            long Id,
+            string Title,
+            string Time,
             DateTime from,
             DateTime to,
             long changedItemId,
             DateTime updatedTime,
             string statusHtml)
         {
-            Id = id;
-            Title = title;
-            Time = time;
-            From = from;
-            if (to.InRange()) To = to;
+            id = Id;
+            title = Title;
+            time = Time;
+            start = from;
+            if (to.InRange()) end = to;
             if (id == changedItemId) Changed = true;
             UpdatedTime = updatedTime;
             StatusHtml = statusHtml;
         }
-    }    
+    }
 }
