1309f0dcdd87f5e92e9a12e2a87bd2098e32fd8e
Hirotada Kobayashi <pierre3@outlook.com>
2023-09-21 11:52:46 +0900

    Revert "ユーザー、グループ、組織のAPI実装"
    This reverts commit 4253cc9d4d8d07894b739fd07dd49290d2881f6c.

--- Implem.Pleasanter/Libraries/Requests/ImportApi.cs
+++ Implem.Pleasanter/Libraries/Requests/ImportApi.cs
@@ -5,7 +5,6 @@ namespace Implem.Pleasanter.Libraries.Requests
     [Serializable]
     public class ImportApi : Api
     {
-        public bool ReplaceAllGroupMembers { get; set; }
         public bool UpdatableImport { get; set; }
         public string Encoding { get; set; }
         public string Key {get; set;}
