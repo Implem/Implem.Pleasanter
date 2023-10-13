e5d79497036c84c341f8d2fc4112c3bc5fb276aa
優希 藤原 <fujihara@implem.co.jp>
2023-10-13 17:29:10 +0900

    分析チャート機能の追加（取得したデータを円グラフで描画する）

--- Implem.Pleasanter/wwwroot/scripts/analy.js
+++ Implem.Pleasanter/wwwroot/scripts/analy.js
@@ -1,180 +1,69 @@
 ﻿$p.drawAnaly = function () {
-    $('#TimeSeriesValueField').toggle($('#TimeSeriesAggregateType').val() !== 'Count');
-    var chartType = $('#TimeSeriesChartType').val();
-    var $svg = $('#TimeSeries');
-    if ($svg.length !== 1) {
-        return;
-    }
-    $svg.empty();
-    var json = JSON.parse($('#TimeSeriesJson').val());
-    var indexes = json.Indexes;
-    if (chartType === 'LineChart') {
-        indexes.sort(function (a, b) {
-            return b.LegendValue - a.LegendValue;
-        });
-    }
-    var elements = json.Elements;
-    if (elements.length === 0) {
-        $svg.hide();
-        return;
-    }
-    $svg.show();
-    var svg = d3.select('#TimeSeries');
-    var padding = 40;
-    var axisPaddingX = 130;
-    var axisPaddingY = 50;
-    var width = parseInt(svg.style('width'));
-    //折れ線チャートの場合は凡例を表示するためにグラフ右側の領域を確保する
-    if (chartType === 'LineChart') {
-        width = width - axisPaddingX;
-    }
-    var height = parseInt(svg.style('height'));
-    var bodyWidth = width - axisPaddingX - (padding);
-    var bodyHeight = height - axisPaddingY - (padding);
-    var minDate = new Date(d3.min(elements, function (d) { return d.Day; }));
-    var maxDate = new Date(d3.max(elements, function (d) { return d.Day; }));
-    var dayWidth = (bodyWidth - padding) / $p.dateDiff('d', maxDate, minDate);
-    var xScale = d3.scaleTime()
-        .domain([minDate, maxDate])
-        .range([padding, bodyWidth]);
-    var yScale = d3.scaleLinear()
-        .domain([d3.max(elements, function (d) {
-            return (chartType === 'LineChart') ? d.Value : d.Y;
-        }), 0])
-        .range([padding, bodyHeight])
-        .nice();
-    var xAxis = d3.axisBottom(xScale)
-        .tickFormat(d3.timeFormat('%m/%d'))
-        .ticks(10);
-    var yAxis = d3.axisLeft(yScale);
-    //チャート数が10以下の場合は色の識別がしやすい10色のカラーセット、
-    //そうでない場合はグラデーションの20色のカラーセットを利用する
-    var colorScale = (indexes.length <= 10)
-        ? d3.scaleOrdinal(d3.schemeCategory10)
-        : d3.scaleSequential(d3.interpolateRainbow).domain([0, 20]);
-    svg.append('g')
-        .attr('class', 'axis')
-        .attr('transform', 'translate(' + axisPaddingX + ', ' + (height - axisPaddingY) + ')')
-        .call(xAxis)
-        .selectAll('text')
-        .attr('x', -20)
-        .attr('y', 20)
-        .style('text-anchor', 'start');
-    svg.append('g')
-        .attr('class', 'axis')
-        .attr('transform', 'translate(' + axisPaddingX + ', 0)')
-        .call(yAxis)
-        .selectAll('text')
-        .attr('x', -20);
-    indexes.forEach(function (index) {
-        var ds = elements.filter(function (d) { return d.Index === index.Id; });
-        if (chartType === 'LineChart') {
-            drawLine(ds, index.Id);
-        } else {
-            drawArea(ds);
-        }
-    });
-    var lineCount = 0;
-    indexes.forEach(function (index) {
-        var ds = elements.filter(function (d) { return d.Index === index.Id; });
-        if (ds.length !== 0) {
-            var last = ds[ds.length - 1];
-            if (chartType === 'LineChart') {
-                drawLineLegend(index, lineCount);
-                lineCount++;
+    var json = JSON.parse($('#AnalyJson').val());
+    // データが空でない場合にグラフを描画
+    var pieChartWidth = 400;
+    var pieChartHeight = 400;
+    var count = 0;  // 処理の回数
+    var newX = 200;
+    var newY = pieChartHeight;
+    var screenWidth = window.innerWidth;  // 画面の幅
+
+    json.forEach(function (pieChart) {
+        var pieChartElements = pieChart.Elements;
+        count = count + 1;
+        var radius = Math.min(pieChartWidth, pieChartHeight) / 2 - 10;
+        var svg = d3.select("#Analy")
+            .attr("width", pieChartWidth * 4)
+            .attr("height", pieChartHeight * 4);
+
+        // 2回目以降の処理から+400ずつ
+        if (count !== 1) {
+            newX = newX + 400;
+            if (screenWidth >= newX) {
             } else {
-                drawAreaLegend(last);
+                newX = 200;
+                newY = newY + 900;
             }
         }
-    });
 
-    function drawArea(ds) {
-        var area = d3.area()
-            .x(function (d) {
-                return ($p.dateDiff('d', new Date(d.Day), minDate) * dayWidth)
-                    + axisPaddingX + padding;
-            })
-            .y0(function (d) {
-                return yScale(0);
-            })
-            .y1(function (d) {
-                return yScale(d.Y);
-            });
-        var g = svg.append('g').attr('class', 'surface');
-        g.append('path').attr('d', area(ds)).attr('fill', color());
-    }
+        // X軸とY軸を決めている
+        var g = svg.append("g")
+            .attr("transform", "translate(" + newX + "," + newY / 2 + ")");
+        var color = d3.scaleOrdinal()
+            .range(["#DC3912", "#3366CC", "#109618", "#FF9900", "#990099"]);
 
-    function drawLine(ds, index) {
-        var line = d3.line()
-            .x(function (d) {
-                return ($p.dateDiff('d', new Date(d.Day), minDate) * dayWidth)
-                    + axisPaddingX + padding;
-            })
-            .y(function (d) {
-                return yScale(d.Value);
-            });
-        var g = svg.append('g').attr('class', 'surface');
-        g.append('path')
-            .attr('d', line(ds))
-            .attr('fill', 'none')
-            .attr('stroke', colorScale(index))
-            .attr('stroke-width', 1.5);
-        g.selectAll('circle')
-            .data(ds)
+        var pie = d3.pie()
+            .value(function (d) { return d.Value; })
+            .sort(null);
+
+        var pieGroup = g.selectAll(".pie")  // gのすべてのpie要素を選択
+            .data(pie(pieChartElements))
             .enter()
-            .append('circle')
-            .attr('cx', function (d) {
-                return ($p.dateDiff('d', new Date(d.Day), minDate) * dayWidth)
-                    + axisPaddingX + padding
-            })
-            .attr('cy', function (d) {
-                return yScale(d.Value);
-            })
-            .attr('r', 4)
-            .attr('stroke', colorScale(index))
-            .attr('fill', colorScale(index));
-    }
+            .append("g")
+            .attr("class", "pie");  // idをpieに変更
 
-    function drawLineLegend(index, lineCount) {
-        var g = svg.append('g');
-        g.append('text')
-            .attr('class', 'index')
-            .attr('transform', 'translate(' + (width + padding) + ', ' + axisPaddingY + ')')
-            .attr('x', padding)
-            .attr('y', lineCount * 20)
-            .attr('text-anchor', 'end')
-            .attr('dominant-baseline', 'central')
-            .text(index.LegendText);
-        g.append('line')
-            .attr('transform', 'translate(' + (width + padding) + ', ' + axisPaddingY + ')')
-            .attr('x1', padding + 10)
-            .attr('x2', padding + 20)
-            .attr('y1', lineCount * 20)
-            .attr('y2', lineCount * 20)
-            .attr('stroke-width', 4)
-            .attr('stroke', colorScale(index.Id));
-    }
+        var arc = d3.arc()
+            .outerRadius(radius)
+            .innerRadius(90);
 
-    function drawAreaLegend(last) {
-        var g = svg.append('g');
-        g.append('text')
-            .attr('class', 'index')
-            .attr('x', ($p.dateDiff('d', new Date(last.Day), minDate) * dayWidth)
-                + axisPaddingX + padding - 10)
-            .attr('y', yScale(last.Y - (last.Value / 2)))
-            .attr('text-anchor', 'end')
-            .attr('dominant-baseline', 'middle')
-            .text(indexes.filter(function (d) { return d.Id === last.Index })[0].Text);
-    }
-    
-    function color() {
-        var c = Math.floor(Math.random() * 50 + 180);
-        return '#' + part(c) + part(c) + part(c);
-    }
+        pieGroup.append("path")  // pieGroupにpathを追加
+            .attr("d", arc)
+            .attr("fill", function (d) { return color(d.index) })
+            .attr("opacity", 0.85)
+            .attr("stroke", "white");
 
-    function part(c) {
-        return (c + Math.floor(Math.random() * 10 - 5)).toString(16);
-    }
+        var text = d3.arc()
+            .outerRadius(radius - 30)
+            .innerRadius(radius - 50);
+
+        pieGroup.append("text")  // pieGroupにtextを追加
+            .attr("fill", "black")
+            .attr("transform", function (d) { return "translate(" + text.centroid(d) + ")"; })
+            .attr("dy", "5px")
+            .attr("font-size", "15px")
+            .attr("text-anchor", "middle")
+            .text(function (d) { return d.data.GroupTitle; });
+    });
 }
 
 $p.openAnalyPartDialog = function ($control) {
