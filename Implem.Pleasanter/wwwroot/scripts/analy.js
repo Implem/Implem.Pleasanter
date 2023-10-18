6f3481de7c0396dfb20c286d95077687a612e985
優希 藤原 <fujihara@implem.co.jp>
2023-10-18 18:22:35 +0900

    分析チャート機能追加（円グラフを複数表示する）

--- Implem.Pleasanter/wwwroot/scripts/analy.js
+++ Implem.Pleasanter/wwwroot/scripts/analy.js
@@ -1,34 +1,66 @@
 ﻿$p.drawAnaly = function () {
     var json = JSON.parse($('#AnalyJson').val());
     // データが空でない場合にグラフを描画
-    var pieChartWidth = 400;
-    var pieChartHeight = 400;
+    var pieChartWidth = 300;
+    var pieChartHeight = 300;
     var count = 0;  // 処理の回数
     var newX = 200;
     var newY = pieChartHeight;
-    var screenWidth = window.innerWidth;  // 画面の幅
+    var screenWidth = window.innerWidth;
+    var screenHeight = window.innerHeight;// 画面の幅
 
     json.forEach(function (pieChart) {
         var pieChartElements = pieChart.Elements;
+        var pieChartSetting = pieChart.Setting;
         count = count + 1;
         var radius = Math.min(pieChartWidth, pieChartHeight) / 2 - 10;
         var svg = d3.select("#Analy")
-            .attr("width", pieChartWidth * 4)
-            .attr("height", pieChartHeight * 4);
+            .attr("width", screenWidth )
+            .attr("height", screenHeight);
 
         // 2回目以降の処理から+400ずつ
         if (count !== 1) {
-            newX = newX + 400;
+            newX = newX + 300;
             if (screenWidth >= newX) {
             } else {
                 newX = 200;
-                newY = newY + 900;
+                newY = newY + 600;
+            }
+            if (screenHeight >= newY) {
+            } else {
+                alert('データがいっぱいです。ログインしなおしてください。')
+                return;
             }
         }
 
         // X軸とY軸を決めている
         var g = svg.append("g")
             .attr("transform", "translate(" + newX + "," + newY / 2 + ")");
+        g.append("text")  
+            .attr("fill", "black")
+            .attr("font-size", "25px")
+            .attr("text-anchor", "middle")
+            .attr("dy", -20)
+            .text(pieChartSetting.AggregationType);
+        g.append("text")
+            .attr("fill", "black")
+            .attr("font-size", "15px")
+            .attr("text-anchor", "middle")
+            .attr("dy", 0)
+            .text(pieChartSetting.TimePeriodValue + "" + pieChartSetting.TimePeriod);
+        g.append("text")
+            .attr("fill", "black")
+            .attr("font-size", "15px")
+            .attr("text-anchor", "middle")
+            .attr("dy", 20)
+            .text(pieChartSetting.AggregationTarget);
+        g.append("text")
+            .attr("fill", "black")
+            .attr("font-size", "15px")
+            .attr("text-anchor", "middle")
+            .attr("dy", 40)
+            .text(pieChartSetting.GroupBy);
+
         var color = d3.scaleOrdinal()
             .range(["#DC3912", "#3366CC", "#109618", "#FF9900", "#990099"]);
 
@@ -44,7 +76,7 @@
 
         var arc = d3.arc()
             .outerRadius(radius)
-            .innerRadius(90);
+            .innerRadius(75);
 
         pieGroup.append("path")  // pieGroupにpathを追加
             .attr("d", arc)
@@ -54,15 +86,15 @@
 
         var text = d3.arc()
             .outerRadius(radius - 30)
-            .innerRadius(radius - 50);
+            .innerRadius(radius - 40);
 
         pieGroup.append("text")  // pieGroupにtextを追加
             .attr("fill", "black")
             .attr("transform", function (d) { return "translate(" + text.centroid(d) + ")"; })
             .attr("dy", "5px")
-            .attr("font-size", "15px")
+            .attr("font-size", "13px")
             .attr("text-anchor", "middle")
-            .text(function (d) { return d.data.GroupTitle; });
+            .text(function (d) { return d.data.GroupTitle + "," + d.data.Value; });
     });
 }
 
