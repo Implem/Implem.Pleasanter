28cc598d42b6c63c8995056afc5883257a3866fb
TakutoOsugi_PMC <oosugi-t@vehiclevision.jp>
2023-11-02 17:49:43 +0900

    Splitフォーム削除時の挙動を一部最適化

--- Implem.Pleasanter/wwwroot/scripts/splittotpform.js
+++ Implem.Pleasanter/wwwroot/scripts/splittotpform.js
@@ -3,6 +3,8 @@
         var totpForm = $(".totp-authentication-code");
         var columnNum = totpForm.index(this);
         var selectedForm = totpForm.eq(columnNum).get(0);
+        var totpCode = $("#SecondaryAuthenticationCode").val();
+        var isDeleted = false;
         if (
             e.code === "ArrowLeft" &&
             selectedForm.selectionStart === 0 &&
@@ -25,8 +27,10 @@
             selectedForm.selectionStart === 0 &&
             columnNum !== 0
         ) {
-            totpForm.eq(columnNum - 1).val("");
-            totpForm.eq(columnNum - 1).focus();
+            columnNum = columnNum - 1;
+            totpForm.eq(columnNum).val("");
+            totpForm.eq(columnNum).focus();
+            isDeleted = true;
         } else if (
             e.code === "Delete" &&
             (
@@ -34,7 +38,17 @@
                 totpForm.eq(columnNum).val() === ""
             ) &&
             columnNum !== 5) {
-            totpForm.eq(columnNum + 1).val("");
+            columnNum = columnNum + 1;
+            totpForm.eq(columnNum).val("");
+            isDeleted = true;
+        }
+
+        if (isDeleted) {
+            totpCode = totpCode.slice(0, columnNum) + totpCode.slice(columnNum + 1);
+
+            $("#SecondaryAuthenticationCode").val(totpCode);
+
+            setCodeSepalateForm(totpForm, totpCode);
         }
     });
 
