ff5d235f37b977f2768ff517823c1c86cf9daf49
HuyHQ <huyhq@codluck.com>
2023-11-27 12:07:51 +0700

    Feature: Add 25 Formula caculation functions Update Display Error: Apply control to type of Script only.

--- Implem.Pleasanter/wwwroot/scripts/formula.js
+++ Implem.Pleasanter/wwwroot/scripts/formula.js
@@ -1,5 +1,10 @@
 ﻿$p.changeCalculationMethod = function ($control) {
     var action = $("option:selected", $control).attr('data-action');
+    if ($("option:selected", $control).val() === 'Default') {
+        $(".formula-display-error-check").addClass('hidden')
+    } else {
+        $(".formula-display-error-check").removeClass('hidden')
+    }
     var url = action
         ? $('.main-form').attr('action').replace('_action_', action.toLowerCase())
         : location.href;
