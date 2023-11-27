1e760fb2ef86bca09121573ab13bda9463222014
trungbuitri <trungbt@codluck.com>
2023-11-27 13:46:56 +0700

    Feature: No #10861658 Add 9 Formula caculation functions Add TEXT function

--- Implem.Pleasanter/Libraries/ServerScripts/FormulaServerScriptUtilities.cs
+++ Implem.Pleasanter/Libraries/ServerScripts/FormulaServerScriptUtilities.cs
@@ -8,7 +8,6 @@ using System;
 using System.Collections.Generic;
 using System.Dynamic;
 using System.Linq;
-using System.Text.RegularExpressions;
 
 namespace Implem.Pleasanter.Libraries.ServerScripts
 {
