6d02950eaf44e3f6b57b78f52f4b6697690383f5
trungbuitri <trungbt@codluck.com>
2023-12-08 11:18:58 +0700

    Update column display text before execute

--- Implem.Pleasanter/Models/Issues/IssueModel.cs
+++ Implem.Pleasanter/Models/Issues/IssueModel.cs
@@ -3628,6 +3628,9 @@ namespace Implem.Pleasanter.Models
                         ss: ss,
                         formulaScript: formulaSet.FormulaScript,
                         calculationMethod: formulaSet.CalculationMethod);
+                    formulaSet = FormulaBuilder.UpdateColumnDisplayText(
+                        ss: ss,
+                        formulaSet: formulaSet);
                     formulaSet.FormulaScript = FormulaBuilder.ParseFormulaScript(
                         ss: ss,
                         formulaScript: formulaSet.FormulaScript,
