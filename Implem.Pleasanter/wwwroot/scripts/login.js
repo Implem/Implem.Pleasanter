7b34fdf6bc53542996bf21fd3c3018a0a140dec3
TakutoOsugi_PMC <oosugi-t@vehiclevision.jp>
2023-12-08 20:46:47 +0900

    不要なファイルを削除

--- Implem.Pleasanter/wwwroot/scripts/login.js
+++
@@ -1,9 +0,0 @@
-﻿$p.showPassword = function () {
-    if ($('#Users_Password').attr('type') === 'password') {
-        $('#Users_Password').attr('type', 'text');
-        $('#show_password').removeClass('fa-eye').addClass('fa-eye-slash');
-    } else {
-        $('#Users_Password').attr('type', 'password');
-        $('#show_password').removeClass('fa-eye-slash').addClass('fa-eye');
-    }
-}
