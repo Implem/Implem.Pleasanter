6c8487c0425052c93b4af1d7a83c05dd00c8893b
Naoya Misawa <nwtba1plt2610@gmail.com>
2023-12-13 11:27:14 +0900

    メッセージ送信を判定する処理を修正。

--- Implem.Pleasanter/Libraries/Settings/Reminder.cs
+++ Implem.Pleasanter/Libraries/Settings/Reminder.cs
@@ -197,11 +197,15 @@ namespace Implem.Pleasanter.Libraries.Settings
                         dataRows: dataTable.AsEnumerable().ToList(),
                         test: test)
                     : null;
+                var dataRows = dataTable.AsEnumerable().ToList();
+                var sendFlag = dataRows.Count == 0
+                    ? NotSendIfNotApplicable != true
+                    : true;
                 var body = reminderType != ReminderTypes.Mail
                     ? GetBody(
                         context: context,
                         ss: ss,
-                        dataRows: dataTable.AsEnumerable().ToList())
+                        dataRows: dataRows)
                     : null;
                 switch (reminderType)
                 {
@@ -241,7 +245,7 @@ namespace Implem.Pleasanter.Libraries.Settings
                         }
                         break;
                     case ReminderTypes.Slack:
-                        if (Parameters.Reminder.Slack && !body.IsNullOrEmpty())
+                        if (Parameters.Reminder.Slack && sendFlag)
                         {
                             new Slack(
                                 _context: context,
@@ -251,7 +255,7 @@ namespace Implem.Pleasanter.Libraries.Settings
                         }
                         break;
                     case ReminderTypes.ChatWork:
-                        if (Parameters.Reminder.ChatWork && !body.IsNullOrEmpty())
+                        if (Parameters.Reminder.ChatWork && sendFlag)
                         {
                             new ChatWork(
                                 _context: context,
@@ -263,7 +267,7 @@ namespace Implem.Pleasanter.Libraries.Settings
                         break;
                     case ReminderTypes.Line:
                     case ReminderTypes.LineGroup:
-                        if (Parameters.Reminder.Line && !body.IsNullOrEmpty())
+                        if (Parameters.Reminder.Line && sendFlag)
                         {
                             new Line(
                                 _context: context,
@@ -274,7 +278,7 @@ namespace Implem.Pleasanter.Libraries.Settings
                         }
                         break;
                     case ReminderTypes.Teams:
-                        if (Parameters.Reminder.Teams && !body.IsNullOrEmpty())
+                        if (Parameters.Reminder.Teams && sendFlag)
                         {
                             new Teams(
                                 _context: context,
@@ -283,7 +287,7 @@ namespace Implem.Pleasanter.Libraries.Settings
                         }
                         break;
                     case ReminderTypes.RocketChat:
-                        if (Parameters.Reminder.RocketChat && !body.IsNullOrEmpty())
+                        if (Parameters.Reminder.RocketChat && sendFlag)
                         {
                             new RocketChat(
                                 _context: context,
@@ -293,7 +297,7 @@ namespace Implem.Pleasanter.Libraries.Settings
                         }
                         break;
                     case ReminderTypes.InCircle:
-                        if (Parameters.Reminder.InCircle && !body.IsNullOrEmpty())
+                        if (Parameters.Reminder.InCircle && sendFlag)
                         {
                             new InCircle(
                                 _context: context,
@@ -493,7 +497,7 @@ namespace Implem.Pleasanter.Libraries.Settings
                     });
                 sb.Append("\n");
             });
-            if (!timeGroups.Any() && NotSendIfNotApplicable != true)
+            if (!timeGroups.Any())
             {
                 sb.Append(Displays.NoTargetRecord(context: context), "\r\n");
             }
