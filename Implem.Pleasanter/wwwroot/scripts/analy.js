c0dcda5592c243d7b778cc915d2fa0d57b1cfb75
浦田哲平 <urata@implem.co.jp>
2023-12-27 18:01:52 +0900

    [fix]cssのfloatプロパティを実装。

--- Implem.Pleasanter/wwwroot/scripts/analy.js
+++ Implem.Pleasanter/wwwroot/scripts/analy.js
@@ -1,35 +1,25 @@
 ﻿$p.drawAnaly = function () {
     // データが空でない場合にグラフを描画
-    var pieChartWidth = 300;
-    var pieChartHeight = 300;
-    var newX = 200;
-    var newY = pieChartHeight;
-    var screenWidth = window.innerWidth;
-    var screenHeight = window.innerHeight; // 画面の幅
+    const pieChartWidth = 300;
+    const pieChartHeight = 300;
+    const newX = 200;
+    const newY = pieChartHeight;
     var conditionIllegalFlag = false;
+    const radius = Math.min(pieChartWidth, pieChartHeight) / 2 - 10;
 
-    var svg = d3
-        .select('#Analy')
-        .attr('width', screenWidth)
-        .attr('height', screenHeight);
+    // サーバから返却されたデータをもとに反復処理
     for (let pieChart of JSON.parse($('#AnalyJson').val())) {
-        var radius = Math.min(pieChartWidth, pieChartHeight) / 2 - 10;
 
-        // 2回目以降の処理から+400ずつ
-        // if (pieChart.Setting.Id !== 1) {
-        //     newX = newX + 300;
-        //     if (screenWidth >= newX) {
-        //     } else {
-        //         newX = 200;
-        //         newY = newY + 600;
-        //     }
-        //     if (screenHeight >= newY) {
-        //     } else {
-        //         alert('データがいっぱいです。ログインしなおしてください。');
-        //         return;
-        //     }
-        // }
+        // SVGタグを設定
+        var svg = d3
+            .select('#TimeSeriesBody')
+            .append('svg')
+            .attr('id', 'Chart_' + pieChart.Setting.Id)
+            .style('width', newX * 2)
+            .style('height', newY)
+            .style("float", "left");
 
+        // Gタグを設定
         var g = svg
             .append('g')
             .attr('id', 'DeleteAnalyPart_' + pieChart.Setting.Id)
@@ -40,6 +30,8 @@
             .style('width', 100)
             .style('height', 100)
             .style('float', 'left');
+
+        // Gタグに対して削除ボタンを追加
         g
             .append('text')
             .attr('fill', 'black')
@@ -49,41 +41,57 @@
             .attr('dy', -130)
             .attr('class', 'delete-analy')
             .attr('onclick', '$p.send($(\'#DeleteAnalyPart_' + pieChart.Setting.Id + '\'));')
-            .text('delete');
+            .text($p.display('Delete'));
 
+        // データが抽出できない場合
         if (pieChart.Elements.length === 0) {
-            // X軸とY軸を決めている
+            // データがない旨画面に表示
             g.append('text')
                 .attr('fill', 'black')
                 .attr('font-size', '15px')
                 .attr('text-anchor', 'middle')
                 .attr('dy', 0)
-                .text($p.display('NoData'));
+                .text($p.display('There is no applicable data.'));
+
+            // データが抽出できた場合
         } else {
+
+            // valueに「0」が設定されているかどうかの判定処理
             for (let element of pieChart.Elements) {
+
+                // 要求が不正だった場合
                 if (element.Value === 0) {
                     conditionIllegalFlag = true;
+                    // 要求が正しかった場合
                 } else {
                     conditionIllegalFlag = false;
                     break;
                 }
             }
+
+            // 要求が不正だった場合
             if (conditionIllegalFlag === true) {
-                // X軸とY軸を決めている
+
+                // 要求が不正だった旨画面に表示
                 g.append('text')
                     .attr('fill', 'black')
                     .attr('font-size', '15px')
                     .attr('text-anchor', 'middle')
                     .attr('dy', 0)
-                    .text($p.display('InvalidRequest'));
+                    .text($p.display('Invalid request has been sent.'));
+
+                // 要求が正しかった場合
             } else {
-                // X軸とY軸を決めている
+
+                // 条件「集計種別」を画面に表示
                 g.append('text')
                     .attr('fill', 'black')
                     .attr('font-size', '25px')
                     .attr('text-anchor', 'middle')
                     .attr('dy', -20)
                     .text($p.display(pieChart.Setting.AggregationType));
+
+                // 条件「値」と「期間」を画面に表示
                 g.append('text')
                     .attr('fill', 'black')
                     .attr('font-size', '15px')
@@ -94,6 +102,8 @@
                         '' +
                         $p.display(pieChart.Setting.TimePeriod)
                     );
+
+                // 条件「集計対象」を画面に表示
                 g.append('text')
                     .attr('fill', 'black')
                     .attr('font-size', '15px')
@@ -107,6 +117,8 @@
                             }
                         }
                     });
+
+                // 条件「項目」を画面に表示
                 g.append('text')
                     .attr('fill', 'black')
                     .attr('font-size', '15px')
@@ -125,6 +137,7 @@
                         }
                     })
 
+                // 円グラフの各要素に対して色を設定
                 var color = d3
                     .scaleOrdinal()
                     .range([
@@ -135,6 +148,7 @@
                         '#990099',
                     ]);
 
+                // 円グラフの各要素に対してラベルを設定
                 var pie = d3
                     .pie()
                     .value(function (d) {
@@ -142,6 +156,7 @@
                     })
                     .sort(null);
 
+                // 円グラフの各要素に対してデータを設定
                 var pieGroup = g
                     // gのすべてのpie要素を選択
                     .selectAll('.pie')
@@ -151,10 +166,11 @@
                     // idをpieに変更
                     .attr('class', 'pie');
 
+                // 円グラフの半径を設定
                 var arc = d3.arc().outerRadius(radius).innerRadius(75);
 
+                // 円グラフの各要素の間にある余白を設定
                 pieGroup
-                    // pieGroupにpathを追加
                     .append('path')
                     .attr('d', arc)
                     .attr('fill', function (d) {
@@ -163,13 +179,14 @@
                     .attr('opacity', 0.85)
                     .attr('stroke', 'white');
 
+                // 円グラフの各要素に設定するラベル位置を設定
                 var text = d3
                     .arc()
                     .outerRadius(radius - 30)
                     .innerRadius(radius - 40);
 
+                // 円グラフの各要素に対して条件「項目」に設定された値と条件「集計種別」で修正されたデータを設定
                 pieGroup
-                    // pieGroupにtextを追加
                     .append('text')
                     .attr('fill', 'black')
                     .attr('transform', function (d) {
@@ -181,7 +198,7 @@
                     .text(function (d) {
                         if (d.data.Value === 0) return;
                         if (d.data.GroupTitle === '? ')
-                            return $p.display('NotSet') + ',' + d.data.Value;
+                            return $p.display('(Not set)') + ',' + d.data.Value;
                         return d.data.GroupTitle + ',' + d.data.Value;
                     });
             }
