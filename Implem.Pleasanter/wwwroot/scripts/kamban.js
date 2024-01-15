4fec013573110927013104f8a18c48bae701a08c
Hirotada Kobayashi <pierre3@outlook.com>
2024-01-15 13:48:15 +0900

    Revert "フォーマット修正"
    This reverts commit 93da7b1e008ed6e1c97cfbb32b13a809af564692.

--- Implem.Pleasanter/wwwroot/scripts/kamban.js
+++ Implem.Pleasanter/wwwroot/scripts/kamban.js
@@ -50,75 +50,76 @@
     });
     $(suffixElArr).each(function (index, value) {
         $(this).find('.kamban-item').each(function () {
-            let offsetX, offsetY, kambanSuffix, siteId;
-            $(this).on('touchstart', function (e) {
-                kambanSuffix = $(this).parents('[id^=Kamban]').attr('id').substring($(this).parents('[id^=Kamban]').attr('id').indexOf('_'));
-                kambanSuffix = kambanSuffix.indexOf('_') === -1 ? '' : kambanSuffix;
-                const touch = e.touches[0];
-                offsetX = touch.clientX;
-                offsetY = touch.clientY;
-                siteId = $(this).attr('data-siteid');
-            });
-            $(this).on('touchmove', function (e) {
-                e.preventDefault();
-                const touch = e.touches[0];
-                const x = touch.clientX - offsetX;
-                const y = touch.clientY - offsetY;
-                const rectDraggable = this.getBoundingClientRect();
-                $(this).css('z-index', 2);
-                // Change background color when moving
-                $('.kambanbody .kamban-container').each(function () {
-                    const rectDroppable = this.getBoundingClientRect();
-                    if (
-                        rectDraggable.left >= rectDroppable.left &&
-                        rectDraggable.left <= rectDroppable.right &&
-                        rectDraggable.top >= rectDroppable.top &&
-                        rectDraggable.top <= rectDroppable.bottom
-                    ) {
-                        $(this).css('background-color', '#f5f5f5');
-                    } else {
-                        $(this).css('background-color', 'unset');
-                    }
-                });
-                $(this).css('transform', `translate(${x}px, ${y}px)`);
-            });
-            $(this).on('touchend', function () {
-                const rectDraggable = this.getBoundingClientRect();
-                const id = $(this).attr('data-id');
-                let isDroppableIntoKambanContainer = false;
-                $('#KambanBody' + kambanSuffix + ' .kamban-container').each(function () {
-                    const rectDroppable = this.getBoundingClientRect();
-                    if (
-                        rectDraggable.left >= rectDroppable.left &&
-                        rectDraggable.left <= rectDroppable.right &&
-                        rectDraggable.top >= rectDroppable.top &&
-                        rectDraggable.top <= rectDroppable.bottom
-                    ) {
-                        var data = $p.getData($('.main-form'));
-                        var tableNamePrefix = $('#KambanReferenceType' + kambanSuffix).val() + '_';
-                        var dataX = $(this).attr('data-x');
-                        var dataY = $(this).attr('data-y');
-                        data["KambanId"] = id;
-                        if (dataX !== undefined) {
-                            data[tableNamePrefix + $('#KambanGroupByX' + kambanSuffix).val()] = dataX;
-                        }
-                        if (dataY !== undefined) {
-                            data[tableNamePrefix + $('#KambanGroupByY' + kambanSuffix).val()] = dataY;
-                        }
-                        if (kambanSuffix !== '') {
-                            data.SiteId = siteId;
-                        }
-                        $p.set($('#KambanSuffix' + kambanSuffix), $('#KambanSuffix' + kambanSuffix).val());
-                        $p.send($('#KambanBody' + kambanSuffix));
-                        $p.clearData('KambanSuffix' + kambanSuffix);
-                        isDroppableIntoKambanContainer = true;
-                    }
-                });
-                // Moves back to original position if not dropped into KambanContainer
-                if (!isDroppableIntoKambanContainer) {
-                    $(this).css('transform', 'unset');
+        let offsetX, offsetY,kambanSuffix,siteId;
+        $(this).on('touchstart', function (e) {
+            kambanSuffix = $(this).parents('[id^=Kamban]').attr('id').substring($(this).parents('[id^=Kamban]').attr('id').indexOf('_'));
+            kambanSuffix = kambanSuffix.indexOf('_') === -1 ? '' : kambanSuffix;
+            const touch = e.touches[0];
+            offsetX = touch.clientX;
+            offsetY = touch.clientY;
+            siteId = $(this).attr('data-siteid');
+        });
+        $(this).on('touchmove', function (e) {
+            e.preventDefault();
+            const touch = e.touches[0];
+            const x = touch.clientX - offsetX;
+            const y = touch.clientY - offsetY;
+            const rectDraggable = this.getBoundingClientRect();
+            $(this).css('z-index', 2);
+            // Change background color when moving
+            $('.kambanbody .kamban-container').each(function () {
+                const rectDroppable = this.getBoundingClientRect();
+                if (
+                    rectDraggable.left >= rectDroppable.left &&
+                    rectDraggable.left <= rectDroppable.right &&
+                    rectDraggable.top >= rectDroppable.top &&
+                    rectDraggable.top <= rectDroppable.bottom
+                ) {
+                    $(this).css('background-color', '#f5f5f5');
+                } else {
+                    $(this).css('background-color', 'unset');
                 }
             });
+            $(this).css('transform', `translate(${x}px, ${y}px)`);
         });
+        $(this).on('touchend', function () {
+            const rectDraggable = this.getBoundingClientRect();
+            const id = $(this).attr('data-id');
+            let isDroppableIntoKambanContainer = false;
+            $('#KambanBody' + kambanSuffix + ' .kamban-container').each(function () {
+                const rectDroppable = this.getBoundingClientRect();
+                if (
+                    rectDraggable.left >= rectDroppable.left &&
+                    rectDraggable.left <= rectDroppable.right &&
+                    rectDraggable.top >= rectDroppable.top &&
+                    rectDraggable.top <= rectDroppable.bottom
+                ) {
+                    var data = $p.getData($('.main-form'));
+                    var tableNamePrefix = $('#KambanReferenceType' + kambanSuffix).val() + '_';
+                    var dataX = $(this).attr('data-x');
+                    var dataY = $(this).attr('data-y');
+                    data["KambanId"] = id;
+                    if (dataX !== undefined) {
+                        data[tableNamePrefix + $('#KambanGroupByX' + kambanSuffix).val()] = dataX;
+                    }
+                    if (dataY !== undefined) {
+                        data[tableNamePrefix + $('#KambanGroupByY' + kambanSuffix).val()] = dataY;
+                    }
+                    if (kambanSuffix !== '') {
+                        data.SiteId = siteId;
+                    }
+                    $p.set($('#KambanSuffix' + kambanSuffix), $('#KambanSuffix' + kambanSuffix).val());
+                    $p.send($('#KambanBody' + kambanSuffix));
+                    $p.clearData('KambanSuffix' + kambanSuffix);
+                    isDroppableIntoKambanContainer = true;
+                }
+            });
+            // Moves back to original position if not dropped into KambanContainer
+            if (!isDroppableIntoKambanContainer) {
+                $(this).css('transform', 'unset');
+            }
+        });
+    });
+
     })
 }
