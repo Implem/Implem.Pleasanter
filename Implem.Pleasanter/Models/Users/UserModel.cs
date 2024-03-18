d6857c8c46ad379d7260d87289669a36af32e149
TakutoOsugi_PMC <oosugi-t@vehiclevision.jp>
2024-03-18 16:35:05 +0900

    Mailでの二段階認証時、Letterでの入力ができない問題を修正

--- Implem.Pleasanter/Models/Users/UserModel.cs
+++ Implem.Pleasanter/Models/Users/UserModel.cs
@@ -4562,8 +4562,7 @@ namespace Implem.Pleasanter.Models
                             controlId: "SecondaryAuthenticationCode",
                             controlCss: " focus always-send",
                             labelText: Displays.AuthenticationCode(context: context),
-                            validateRequired: true,
-                            validateNumber: true)
+                            validateRequired: true)
                     : () => hb = TotpAuthenticationCodeSeparate(hb, context));
             hb
                 .Div(
