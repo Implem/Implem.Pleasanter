2b72d6f891daa58de251b362160382743932f040
はるか 峯 <mine@implem.co.jp>
2024-07-26 15:17:37 +0900

    MySQLでエラーになる記法等の修正 [skip ci]

--- Rds/Implem.MySql/MySqlResult.cs
+++ Rds/Implem.MySql/MySqlResult.cs
@@ -1,17 +1,19 @@
 ﻿using Implem.IRds;
+using Implem.Libraries.Utilities;
 using System.Data;
+using System.Linq;
 namespace Implem.MySql
 {
     internal class MySqlResult : ISqlResult
     {
         public int DeleteCount(DataTable data)
         {
-            return data?.Rows.Count ?? 0;
+            return data?.AsEnumerable().FirstOrDefault()?.Field<long>(0).ToInt() ?? 0;
         }
 
         public int RestoreCount(DataTable data)
         {
-            return data?.Rows.Count ?? 0;
+            return data?.AsEnumerable().FirstOrDefault()?.Field<long>(0).ToInt() ?? 0;
         }
     }
 }
