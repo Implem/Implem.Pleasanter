f20f7a53bca7fead70adc2a0985bd20898d2fc5b
はるか 峯 <mine@implem.co.jp>
2024-07-26 16:09:26 +0900

    MySQLでテーブル更新時にwhereを副問い合わせで指定した際、エラーになる場合がある問題への対応。 論理削除とリストアに水平展開。 [skip ci]

--- Implem.Libraries/DataSources/SqlServer/SqlDelete.cs
+++ Implem.Libraries/DataSources/SqlServer/SqlDelete.cs
@@ -19,6 +19,7 @@ namespace Implem.Libraries.DataSources.SqlServer
         {
             if (!Using) return;
             Build_If(commandText: commandText);
+            SetMainQueryInfoForSub();
             Build_DeleteStatement(
                 factory: factory,
                 sqlContainer: sqlContainer,
@@ -51,6 +52,30 @@ namespace Implem.Libraries.DataSources.SqlServer
                     sqlContainer: sqlContainer,
                     sqlCommand: sqlCommand,
                     commandCount: commandCount))));
+        }
+
+        private string GetTableBracketText()
+        {
+            switch (TableType)
+            {
+                case Sqls.TableTypes.History:
+                    return HistoryTableBracket;
+                case Sqls.TableTypes.Deleted:
+                    return DeletedTableBracket;
+                default:
+                    return TableBracket;
+            }
+        }
+
+        private void SetMainQueryInfoForSub()
+        {
+            //サブクエリのselect文生成を行う際に、メイン（本クラスのこと）のクエリの情報を取得できるように、
+            //あらかじめ情報をセットする処理
+            SqlWhereCollection
+                .Where(o => o.Sub != null)
+                .ForEach(o => o.Sub.SetMainQueryInfo(
+                    sqlClass: GetType().ToString(),
+                    tableBracket: GetTableBracketText()));
         }
     }
 }
