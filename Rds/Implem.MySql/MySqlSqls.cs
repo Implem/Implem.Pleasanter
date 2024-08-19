91af0a6b3e412a1724ea9c1649e533f97642b7b3
はるか 峯 <mine@implem.co.jp>
2024-08-19 17:52:36 +0900

    CodeDefiner2回目以降に不要なMigrateが実施される問題を解消

--- Rds/Implem.MySql/MySqlSqls.cs
+++ Rds/Implem.MySql/MySqlSqls.cs
@@ -14,7 +14,8 @@ namespace Implem.MySql
 
         public string IsNotTrue { get; } = " <> 1 ";
 
-        public string CurrentDateTime { get; } = " current_timestamp(3) ";
+        //CURRENT_TIMESTAMPは、デフォルト値の設定をselectする際に大文字が取得されるため、ここでも大文字で定義する必要がある
+        public string CurrentDateTime { get; } = " CURRENT_TIMESTAMP(3) ";
 
         public string Like { get; } = " like ";
 
