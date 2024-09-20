fe70a68e3888e7fd58181a432f831e90532d8db7
nagamune <nagamune@implem.co.jp>
2024-09-20 12:55:50 +0900

    サイトパッケージインポート時にスクリプト・サーバスクリプトのSiteID/recodeID置換を行う機能を追加。 #1686

--- Implem.Pleasanter/Libraries/SitePackages/Utilities.cs
+++ Implem.Pleasanter/Libraries/SitePackages/Utilities.cs
@@ -261,11 +261,11 @@ namespace Implem.Pleasanter.Libraries.SitePackages
                     statements: statements.ToArray());
             }
             var idHash = sitePackage.GetIdHashFromConverters();
-            foreach (long savedSiteId in sitePackage.HeaderInfo.Convertors.Select(e => e.SavedSiteId))
+            foreach (var conv in sitePackage.HeaderInfo.Convertors)
             {
                 var siteModel = new SiteModel(
                     context: context,
-                    siteId: savedSiteId);
+                    siteId: conv.SavedSiteId.ToLong());
                 switch (siteModel.ReferenceType)
                 {
                     case "Wikis":
@@ -287,7 +287,8 @@ namespace Implem.Pleasanter.Libraries.SitePackages
                                 top: 1,
                                 column: Rds.WikisColumn().WikiId(),
                                 where: Rds.WikisWhere().SiteId(siteModel.SiteId)));
-                        idHash.Add(savedSiteId, wikiId);
+                        var srcWikiId = sitePackage.Sites?.FirstOrDefault(o => o.SiteId == conv.SiteId)?.WikiId;
+                        if (srcWikiId != null) idHash.Add(srcWikiId.ToLong(), wikiId);
                         break;
                     default:
                         Search.Indexes.Create(
@@ -342,6 +343,9 @@ namespace Implem.Pleasanter.Libraries.SitePackages
                             }
                         }
                     }
+                    conv.Updated |= ConvertScriptDataId(
+                        ss: conv.SiteSettings,
+                        idHash: idHash);
                     if (conv.Updated)
                     {
                         conv.SiteSettings.Init(context: context);
@@ -460,6 +464,59 @@ namespace Implem.Pleasanter.Libraries.SitePackages
                     })
                     .ToJson();
             }
+        }
+
+        private static bool ConvertScriptDataId(
+            SiteSettings ss,
+            Dictionary<long, long> idHash)
+        {
+            var isUpdated = false;
+            ss?.Scripts?.ForEach(script =>
+            {
+                var newScript = ConvertScriptDataId(
+                    text: script.Body,
+                    idHash: idHash);
+                if (newScript != script.Body)
+                {
+                    isUpdated = true;
+                    script.Body = newScript;
+                }
+            });
+            ss?.ServerScripts?.ForEach(script =>
+            {
+                var newScript = ConvertScriptDataId(
+                    text: script.Body,
+                    idHash: idHash);
+                if (newScript != script.Body)
+                {
+                    isUpdated = true;
+                    script.Body = newScript;
+                }
+            });
+            return isUpdated;
+        }
+
+        private static string ConvertScriptDataId(
+            string text,
+            Dictionary<long, long> idHash)
+        {
+            var regexSection = new Regex(@"^(?<pre>// @siteid list start@.*\r?)$(?<code>[\s\S]*?)^(?<post>// @siteid list end@)", RegexOptions.Multiline);
+            var regexId = new Regex(@"\b(?<!\.)\d+(?!\.)\b");
+            return regexSection.Replace(
+                text,
+                new MatchEvaluator((Match matchSection) =>
+                    !matchSection.Groups.ContainsKey("code")
+                        ? matchSection.Value
+                        : matchSection.Groups["pre"].Value
+                            + regexId.Replace(
+                                matchSection.Groups["code"].Value,
+                                new MatchEvaluator((Match matchId) =>
+                                    (long.TryParse(matchId.Value, out var id)
+                                        && idHash.ContainsKey(id))
+                                        ? idHash[id].ToString()
+                                        : matchId.Value
+                                ))
+                            + matchSection.Groups["post"].Value));
         }
 
         public static SitePackage GetSitePackageFromPostedFile(Context context)
