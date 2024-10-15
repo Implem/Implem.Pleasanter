b154748f78b7ee9543da33fc869b41003d81bb72
nagamune <nagamune@implem.co.jp>
2024-10-15 15:26:12 +0900

    DevelopmentToolsで作成されたテーブルがある場合にCodeDefinerに失敗する問題を解消 #1065 ・呼び出されていないメソッドの削除

--- Implem.Pleasanter/App_Data/Definitions/Sqls/PostgreSQL/GrantPrivilegeUser.sql
+++ Implem.Pleasanter/App_Data/Definitions/Sqls/PostgreSQL/GrantPrivilegeUser.sql
@@ -1,1 +1,11 @@
-﻿grant select, insert, update, delete on all tables in schema "#SchemaName#" to "#Uid#";
+﻿do
+$$
+declare
+    r record;
+begin
+    for r in select  schemaname,tablename from pg_tables where tableowner='#Oid#' and schemaname='#SchemaName#'
+    loop
+        execute 'grant select, insert, update, delete on table "' || r.schemaname || '"."' || r.tablename || '" to "#Uid#"';
+    end loop;
+end
+$$;
