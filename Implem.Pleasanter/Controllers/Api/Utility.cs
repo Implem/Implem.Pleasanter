c0f5edf229712a57f4ce933f95f1a00ab49d49f7
implem-kato <kato@implem.co.jp>
2024-11-12 14:06:47 +0900

    ライセンスの有無およびライセンス期間を返すAPIを作成

---
+++ Implem.Pleasanter/Controllers/Api/Utility.cs
@@ -0,0 +1,50 @@
+﻿using Implem.DefinitionAccessor;
+using Implem.Libraries.Utilities;
+using Implem.ParameterAccessor.Parts;
+using Implem.Pleasanter.Libraries.Requests;
+using Implem.Pleasanter.Libraries.Responses;
+using Implem.Pleasanter.Libraries.Settings;
+using Implem.Pleasanter.Models;
+using Implem.PleasanterFilters;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Linq;
+
+namespace Implem.Pleasanter.Controllers.Api
+{
+    [CheckApiContextAttributes]
+    [AllowAnonymous]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class Utility : ControllerBase
+    {
+        [HttpPost("GetLicenseInfo")]
+        public ContentResult GetLicenseInfo()
+        {
+            var body = default(string);
+            using (var reader = new StreamReader(Request.Body)) body = reader.ReadToEnd();
+            var context = new Context(
+                sessionStatus: User?.Identity?.IsAuthenticated == true,
+                sessionData: User?.Identity?.IsAuthenticated == true,
+                apiRequestBody: body,
+                contentType: Request.ContentType,
+                api: true);
+            var log = new SysLogModel(context: context);
+            var result = context.Authenticated
+                ? ApiResults.Get(new
+                {
+                    StatusCode = 200,
+                    Response = new
+                    {
+                        CommercialLicense = Parameters.CommercialLicense(),
+                        LicenseDeadline = Parameters.LicenseDeadline(),
+                    }
+                }.ToJson())
+                : ApiResults.Unauthorized(context: context);
+            log.Finish(context: context, responseSize: result.Content.Length);
+            return result.ToHttpResponse(request: Request);
+        }
+
+    }
+}
