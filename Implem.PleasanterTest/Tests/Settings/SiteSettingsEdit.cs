87c1923f6b7921c0485c0f83de2d8d06cf18545f
浦田哲平 <urata@implem.co.jp>
2024-11-15 15:39:21 +0900

    「サイト設定 - 参照」の xUnit 実装完了。

---
+++ Implem.PleasanterTest/Tests/Settings/SiteSettingsEdit.cs
@@ -0,0 +1,57 @@
+﻿using Implem.Libraries.Utilities;
+using Implem.Pleasanter.Libraries.Settings;
+using Implem.Pleasanter.Models;
+using Implem.PleasanterTest.Models;
+using Implem.PleasanterTest.Utilities;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Implem.PleasanterTest.Tests.Settings
+{
+    [Collection(nameof(SiteSettingsEdit))]
+    public class SiteSettingsEdit
+    {
+
+        [Theory]
+        [MemberData(nameof(GetData))]
+        public void Test(
+            string title,
+            UserModel userModel)
+        {
+            var siteId = Initializer.Sites.Get(title).SiteId;
+            var context = ContextData.Get(
+                userId: userModel.UserId,
+                routeData: RouteData.ItemsEdit(id: siteId));
+            var html = new ItemModel(context: context, referenceId: siteId).Editor(context: context);
+            var result = html.Contains("EditorTabsContainer");
+            Assert.True(result);
+        }
+
+        public static IEnumerable<object[]> GetData()
+        {
+            var testParts = new List<TestPart>()
+            {
+                new TestPart(
+                    title: "サイト設定 - 参照",
+                    userType: UserData.UserTypes.Privileged),
+            };
+            foreach (var testPart in testParts)
+            {
+                yield return TestData(
+                    title: testPart.Title,
+                    userModel: testPart.UserModel);
+            }
+        }
+
+        private static object[] TestData(
+            string title,
+            UserModel userModel)
+        {
+            return new object[]
+            {
+                title,
+                userModel
+            };
+        }
+    }
+}
