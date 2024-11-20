d7ef4856d8c42763d95b6cddd7020926072e78c3
浦田哲平 <urata@implem.co.jp>
2024-11-20 20:23:40 +0900

    「サイト設定 - 更新」xUnit 実装途中。

---
+++ Implem.PleasanterTest/Tests/Settings/SiteSettingsUpdate.cs
@@ -0,0 +1,71 @@
+﻿using Implem.Libraries.Utilities;
+using Implem.Pleasanter.Libraries.Requests;
+using Implem.Pleasanter.Libraries.Settings;
+using Implem.Pleasanter.Models;
+using Implem.PleasanterTest.Models;
+using Implem.PleasanterTest.Utilities;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Implem.PleasanterTest.Tests.Settings
+{
+    [Collection(nameof(SiteSettingsUpdate))]
+    public class SiteSettingsUpdate
+    {
+
+        [Theory]
+        [MemberData(nameof(GetData))]
+        public void Test(
+            string title,
+            Forms forms,
+            UserModel userModel)
+        {
+            var siteId = Initializer.Sites.Get(title).SiteId;
+            var context = ContextData.Get(
+                userId: userModel.UserId,
+                routeData: RouteData.ItemsSetSiteSettings(id: siteId),
+                forms: forms);
+            var results = Results(context: context);
+            Initializer.SaveResults(results);
+            Assert.True(false);
+        }
+
+        public static IEnumerable<object[]> GetData()
+        {
+            var testParts = new List<TestPart>()
+            {
+                new TestPart(
+                    title: "サイト設定 - 更新",
+                    forms: FormsUtilities.Get(new KeyValue("Sites_SiteName", "サイト設定 - 更新後")),
+                    userType: UserData.UserTypes.Privileged),
+            };
+            foreach (var testPart in testParts)
+            {
+                yield return TestData(
+                    title: testPart.Title,
+                    forms: testPart.Forms,
+                    userModel: testPart.UserModel);
+            }
+        }
+
+        private static object[] TestData(
+            string title,
+            Forms forms,
+            UserModel userModel)
+        {
+            return new object[]
+            {
+                title,
+                forms,
+                userModel
+            };
+        }
+
+        private static string Results(Context context)
+        {
+            return SiteUtilities.SetSiteSettings(
+                context: context,
+                siteId: context.Id);
+        }
+    }
+}
