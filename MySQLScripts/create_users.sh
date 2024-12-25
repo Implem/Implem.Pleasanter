71f2c07bde35fb7363964abf724812a1b1641803
はるか 峯 <mine@implem.co.jp>
2024-12-25 19:56:58 +0900

    MySQL残対応_GitHubActions シェルスクリプトデバッグ

--- MySQLScripts/create_users.sh
+++ MySQLScripts/create_users.sh
@@ -1,6 +1,6 @@
 ﻿# GitHub Actionsのdb起動時に自動実行するSQL群
-#mysql -u root -p$MYSQL_ROOT_PASSWORD -e $"create user 'Implem.Pleasanter_Owner'@'%' identified by '$MYSQL_OWNER_PASSWORD';"
+#mysql -u root -p$MYSQL_ROOT_PASSWORD -e $"create user 'Implem.Pleasanter_Owner'@'%' identified by '${MYSQL_OWNER_PASSWORD}';"
 #mysql -u root -p$MYSQL_ROOT_PASSWORD -e $"grant all on `Implem.Pleasanter`.* to 'Implem.Pleasanter_Owner'@'%' with grant option;"
-#mysql -u root -p$MYSQL_ROOT_PASSWORD -e $"create user 'Implem.Pleasanter_User'@'%' identified by '$MYSQL_USER_PASSWORD';"
+#mysql -u root -p$MYSQL_ROOT_PASSWORD -e $"create user 'Implem.Pleasanter_User'@'%' identified by '${MYSQL_USER_PASSWORD}';"
 #mysql -u root -p$MYSQL_ROOT_PASSWORD -e $"grant select, insert, update, delete, create routine, alter routine on `Implem.Pleasanter`.* to 'Implem.Pleasanter_User'@'%';"
-echo "Hello, Docker"
+exit 0
