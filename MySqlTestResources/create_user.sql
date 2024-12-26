ee1f9d5500bb3f95e91df7a8cdb38913950e1792
はるか 峯 <mine@implem.co.jp>
2024-12-26 17:14:18 +0900

    MySQL残対応_GitHubActions deサービスの初期投入データをSQLからシェルスクリプトに切り替えるテスト

--- MySqlTestResources/create_user.sql
+++
@@ -1,4 +0,0 @@
-create user 'Implem.Pleasanter_Owner'@'%' identified by 'dummy';
-grant all on `Implem.Pleasanter`.* to 'Implem.Pleasanter_Owner'@'%' with grant option;
-create user 'Implem.Pleasanter_User'@'%' identified by 'dummy';
-grant select, insert, update, delete, create routine, alter routine on `Implem.Pleasanter`.* to 'Implem.Pleasanter_User'@'%';
