78285fd506a13e11f3aeeaad63ed792c2ac4ff34
はるか 峯 <mine@implem.co.jp>
2024-12-26 18:24:24 +0900

    MySQL残対応_GitHubActions deサービスの初期投入データの仕様確認

---
+++ MySqlTestResources/pleasanter.sql
@@ -0,0 +1,4 @@
+﻿create user 'Implem.Pleasanter_Owner'@'%' identified by '$MMm56@W57C&sN';
+grant all on `Implem.Pleasanter`.* to 'Implem.Pleasanter_Owner'@'%' with grant option;
+create user 'Implem.Pleasanter_User'@'%' identified by 'qasgo%v5MzTJZt';
+grant select, insert, update, delete, create routine, alter routine on `Implem.Pleasanter`.* to 'Implem.Pleasanter_User'@'%';
