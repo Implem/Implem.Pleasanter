649f33555bd8f5d0ea6633082b8444fad3079ecd
はるか 峯 <mine@implem.co.jp>
2024-12-26 17:22:43 +0900

    MySQL残対応_GitHubActions deサービスの初期投入データをSQLからシェルスクリプトに切り替えるテスト

--- MySqlTestResources/pleasanter.sh
+++ MySqlTestResources/pleasanter.sh
@@ -1,4 +1,1 @@
-﻿mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "create user 'Implem.Pleasanter_Owner'@'%' identified by 'dummy1';"
-mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "grant all on \`Implem.Pleasanter\`.* to 'Implem.Pleasanter_Owner'@'%' with grant option;"
-mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "create user 'Implem.Pleasanter_User'@'%' identified by 'dummy2';"
-mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "grant select, insert, update, delete, create routine, alter routine on \`Implem.Pleasanter\`.* to 'Implem.Pleasanter_User'@'%';"
+﻿exit 0
