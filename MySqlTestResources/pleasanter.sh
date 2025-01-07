79ae6d46009a104b6e44425d86da39dbf2daf1f7
はるか 峯 <mine@implem.co.jp>
2025-01-07 15:36:05 +0900

    MySQL残対応_GitHubActions シェルスクリプト再検証

--- MySqlTestResources/pleasanter.sh
+++ MySqlTestResources/pleasanter.sh
@@ -1,4 +1,2 @@
-mysql -u root -p${MYSQL_ROOT_PASSWORD};
-mysql -u root -p${MYSQL_ROOT_PASSWORD};
-mysql -u root -p${MYSQL_ROOT_PASSWORD};
-mysql -u root -p${MYSQL_ROOT_PASSWORD};
+mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "create user 'Implem.Pleasanter_Owner'@'%' identified by 'pw2';";
+mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "grant all on \`Implem.Pleasanter\`.* to 'Implem.Pleasanter_Owner'@'%' with grant option;";
