43d6184794c54e5df39ed6c16024df27ee95249c
はるか 峯 <mine@implem.co.jp>
2025-01-08 16:52:16 +0900

    MySQL残対応_GitHubActions シェルスクリプトをBOMなしのUTF8かつ改行コードをLFに修正

--- MySqlTestResources/pleasanter.sh
+++ MySqlTestResources/pleasanter.sh
@@ -1,1 +1,3 @@
-﻿#!/bin/bash
+#!/bin/bash
+mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "create user 'Implem.Pleasanter_Owner'@'%' identified by '${MYSQL_OWNER_PASSWORD}';";
+mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "grant all on \`Implem.Pleasanter\`.* to 'Implem.Pleasanter_Owner'@'%' with grant option;";
