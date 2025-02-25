1034e56f2d50a2256964336f28a378e649267870
nagamune <nagamune@implem.co.jp>
2025-02-25 17:45:42 +0900

    AWSでCodeDfinerエラーが発生する問題の解消。 #1570

--- Implem.Pleasanter/App_Data/Definitions/Sqls/PostgreSQL/CreateDatabaseForPostgres.sql
+++ Implem.Pleasanter/App_Data/Definitions/Sqls/PostgreSQL/CreateDatabaseForPostgres.sql
@@ -1,1 +1,11 @@
 ﻿create database "#InitialCatalog#" owner "#Uid_Owner#";
+
+do $$
+begin
+  if exists (select 'public' != '#SchemaName#') then
+     revoke all on database "#InitialCatalog#" from public;
+  end if;
+end $$ ;
+
+grant all privileges on database "#InitialCatalog#" to "#Uid_Owner#";
+grant connect on database "#InitialCatalog#" to "#Uid_User#";
