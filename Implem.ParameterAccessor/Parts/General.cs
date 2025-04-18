c8af9e782daa3314f1c6cfcf1213486acb51b383
TakutoOsugi_PMC <oosugi-t@vehiclevision.jp>
2025-04-18 16:27:51 +0900

    表示件数の文言・一部表現を修正

--- Implem.ParameterAccessor/Parts/General.cs
+++ Implem.ParameterAccessor/Parts/General.cs
@@ -123,8 +123,8 @@ namespace Implem.ParameterAccessor.Parts
         public int BulkUpsertMax { get; set; }
         public bool EnableExpandLinkPath { get; set; }
         public bool BlockSiteTaskWhileRunning { get; set; }
-        public int LinkPageSize { get; set; }
-        public bool LinkPageSizeMin { get; set; }
-        public bool LinkPageSizeMax { get; set; }
+        public int? LinkPageSize { get; set; }
+        public int LinkPageSizeMin { get; set; }
+        public int LinkPageSizeMax { get; set; }
     }
 }
