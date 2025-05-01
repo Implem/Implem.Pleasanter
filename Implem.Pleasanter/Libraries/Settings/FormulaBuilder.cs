0b44e93aabd4990f654510c6fb53e3ea633b2601
implem-oishi <114896000+implem-oishi@users.noreply.github.com>
2025-05-01 16:35:49 +0900

    不要なusingの削除

--- Implem.Pleasanter/Libraries/Settings/FormulaBuilder.cs
+++ Implem.Pleasanter/Libraries/Settings/FormulaBuilder.cs
@@ -1,6 +1,5 @@
 ﻿using Implem.Libraries.Utilities;
 using Implem.Pleasanter.Libraries.General;
-using Microsoft.Identity.Client;
 using System;
 using System.Collections.Generic;
 using System.Linq;
