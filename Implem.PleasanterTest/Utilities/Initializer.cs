24e75e8f3daf16141f3eb57339aec40c8f3eb350
はるか 峯 <mine@implem.co.jp>
2025-05-12 18:52:29 +0900

    サイトパッケージをxUnitのテストデータとする機能の追加

--- Implem.PleasanterTest/Utilities/Initializer.cs
+++ Implem.PleasanterTest/Utilities/Initializer.cs
@@ -6,6 +6,7 @@ using Implem.Pleasanter.Libraries.Requests;
 using Implem.Pleasanter.Libraries.Settings;
 using Implem.Pleasanter.Models;
 using Implem.PleasanterTest.Models;
+using Microsoft.AspNetCore.Http;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -188,8 +189,49 @@ namespace Implem.PleasanterTest.Utilities
                         .TenantId(TenantId),
                     param: Rds.UsersParam()
                         .Password("ABCDEF".Sha512Cng())));
+            //サイトパッケージをテストデータとして登録。
+            ImportSitePackageForTest(context: Context);
+
         }
-
-
+        private static void ImportSitePackageForTest(Context context)
+        {
+            context.HasPrivilege = true;
+            // IncludeData(データを含める)のみ選択状態にする。
+            // IncludeSitePermission | IncludeRecordPermission | IncludeColumnPermission | IncludeNotifications | IncludeReminders は非選択。
+            context.Forms.Add("IncludeData", "True");
+            var path = Path.Combine(Directories.PleasanterTest(), "Data_Package");
+            foreach (var filePath in Directory.GetFiles(path))
+            {
+                var fileStream = new FileStream(filePath, FileMode.Open);
+                var file = new FormFile(fileStream, 0, fileStream.Length, null, Path.GetFileName(filePath));
+                SetPostedFiles(context, file);
+                var model = new ItemModel(
+                    context: context,
+                    referenceId: 0)
+                    .ImportSitePackage(context: context);
+            }
+        }
+        public static void SetPostedFiles(Context context, IFormFile file)
+        {
+            if (context.PostedFiles.Count > 0)
+            {
+                context.PostedFiles.RemoveAt(0);
+            }
+            context.PostedFiles.Add(new PostedFile()
+            {
+                Guid = new HttpPostedFile(file).WriteToTemp(context),
+                FileName = file.FileName.Split(System.IO.Path.DirectorySeparatorChar).Last(),
+                Extension = new HttpPostedFile(file).Extension(),
+                Size = file.Length,
+                ContentType = "application/json",
+                ContentRange = file.Length > 0
+                    ? new System.Net.Http.Headers.ContentRangeHeaderValue(
+                        0,
+                        file.Length - 1,
+                        file.Length)
+                    : new System.Net.Http.Headers.ContentRangeHeaderValue(0, 0, 0),
+                InputStream = file.OpenReadStream()
+            });
+        }
     }
 }
