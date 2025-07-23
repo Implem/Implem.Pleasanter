997099ef481272554e04aea7220162269c2f45d9
TakutoOsugi_PMC <oosugi-t@vehiclevision.jp>
2025-07-23 18:34:34 +0900

    カメラ切替時、モーダルを閉じないよう修正

--- Implem.PleasanterFrontend/wwwroot/src/scripts/generals/video.js
+++ Implem.PleasanterFrontend/wwwroot/src/scripts/generals/video.js
@@ -1,40 +1,41 @@
-﻿$p.openVideo = function (controlId, deviceIdIndexNo = 0) {
+﻿$p.openVideo = function (controlId) {
     $p.getVideoDeviceList().then(function (videoDeviceList) {
         const maxDeviceIdIndexNo = videoDeviceList.length - 1;
         if (maxDeviceIdIndexNo === 0) {
             $('#ChangeCamera').hide()
         }
-        if (maxDeviceIdIndexNo - deviceIdIndexNo < 0) {
-            deviceIdIndexNo = 0;
-        }
-        let deviceId = videoDeviceList[deviceIdIndexNo].deviceId;
-        navigator.mediaDevices.getUserMedia({ video: { deviceId }, audio: false }).then(function (stream) {
-            $('#VideoTarget').val(controlId);
-            $('#VideoDialog').dialog({
-                modal: true,
-                width: '680px',
-                appendTo: '#Application',
-                resizable: false,
-                close: function () {
-                    $p.videoTracks.forEach(function (track) { track.stop() });
-                }
-            });
-            $p.video = document.getElementById('Video');
-            $p.video.srcObject = stream;
-            $p.video.onloadedmetadata = function (e) {
-                $p.videoTracks = stream.getVideoTracks();
-                $p.video.play();
-                $('#ChangeCamera').on('click', function () {
-                    $p.changeCamera($(this), controlId, deviceIdIndexNo);
-                });
-            };
-        }).catch(function (error) {
-            $p.setErrorMessage('CanNotConnectCamera');
-            return;
-        });
+        $p.playVideo(controlId, videoDeviceList, maxDeviceIdIndexNo, 0);
     }).catch(function (error) {
         $p.setErrorMessage('CanNotConnectCamera');
         return;
+    });
+}
+
+$p.playVideo = function (controlId, videoDeviceList, maxDeviceIdIndexNo, deviceIdIndexNo) {
+    const deviceId = videoDeviceList[deviceIdIndexNo].deviceId;
+    navigator.mediaDevices.getUserMedia({ video: { deviceId }, audio: false }).then(function (stream) {
+        $('#VideoTarget').val(controlId);
+        $('#VideoDialog').dialog({
+            modal: true,
+            width: '680px',
+            appendTo: '#Application',
+            resizable: false,
+            close: function () {
+                $p.videoTracks.forEach(function (track) { track.stop() });
+            }
+        });
+        $p.video = document.getElementById('Video');
+        $p.video.srcObject = stream;
+        $p.video.onloadedmetadata = function (e) {
+            $p.videoTracks = stream.getVideoTracks();
+            $p.video.play();
+            $('#ChangeCamera').on('click', function () {
+                $p.changeCamera(controlId, videoDeviceList, $p.videoTracks, maxDeviceIdIndexNo, deviceIdIndexNo);
+            });
+        };
+    }).catch(function (error) {
+        $p.setErrorMessage('CanNotGetMediaInformation');
+        reject(error);
     });
 }
 
@@ -51,12 +52,14 @@ $p.toShoot = function ($control) {
     $p.closeDialog($control);
 }
 
-$p.changeCamera = function ($control, controlId, deviceIdIndexNo) {
-// index番号はイベントを設定した時点のものが保持されているハズ(Mediaが動いたままなので)
-    const nowDeviceIdIndexNo = deviceIdIndexNo + 1;
+$p.changeCamera = function (controlId, videoDeviceList, $videoTracks, maxDeviceIdIndexNo, nowDeviceIdIndexNo) {
+    let deviceIdIndexNo = nowDeviceIdIndexNo + 1;
+    if (maxDeviceIdIndexNo - deviceIdIndexNo < 0) {
+        deviceIdIndexNo = 0;
+    }
     $('#ChangeCamera').off('click');
-    $p.closeDialog($control);
-    $p.openVideo(controlId, nowDeviceIdIndexNo);
+    $videoTracks.forEach(function (track) { track.stop() });
+    $p.playVideo(controlId, videoDeviceList, maxDeviceIdIndexNo, deviceIdIndexNo);
 }
 
 $p.getVideoDeviceList = function () {
