422c3b956b997ac23bc7bd576b968670de9e3191
Koji Tomeoku <tomeoku@implem.co.jp>
2025-08-28 13:30:06 +0900

    一覧headerのチェックボックスがデータが無い場合にチェック済みになっている事象を修正

--- Implem.Pleasanter/Models/Tenants/TenantUtilities.cs
+++ Implem.Pleasanter/Models/Tenants/TenantUtilities.cs
@@ -2157,8 +2157,9 @@ namespace Implem.Pleasanter.Models
                     .Th(action: () => hb
                         .CheckBox(
                             controlCss: "select-all",
-                            _checked: tenantModel.TenantSettings.BackgroundServerScripts?.Scripts.All(o =>
-                                selected?.Contains(o.Id) == true) == true))
+                            _checked: tenantModel.TenantSettings.BackgroundServerScripts?.Scripts.Count > 0
+                                && tenantModel.TenantSettings.BackgroundServerScripts?.Scripts.All(o =>
+                                    selected?.Contains(o.Id) == true) == true))
                     .Th(action: () => hb
                         .Text(text: Displays.Id(context: context)))
                     .Th(action: () => hb
@@ -2336,7 +2337,7 @@ namespace Implem.Pleasanter.Models
                     .Th(action: () => hb
                         .CheckBox(
                             controlCss: "select-all",
-                            _checked: schedules.All(o =>
+                            _checked: schedules.Count() > 0 && schedules?.All(o =>
                                 selected?.Contains(o.Id) == true) == true))
                     .Th(action: () => hb
                         .Text(text: Displays.Id(context: context)))
@@ -2360,8 +2361,7 @@ namespace Implem.Pleasanter.Models
                             .Td(action: () => hb
                                 .CheckBox(
                                     controlCss: "select",
-                                    _checked: selected?
-                                        .Contains(schedule.Id) == true))
+                                    _checked: selected.Contains(schedule.Id) == true))
                             .Td(action: () => hb
                                 .Text(text: schedule.Id.ToString()))
                             .Td(action: () => hb
